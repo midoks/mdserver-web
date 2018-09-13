4018c2ff95220ae8076d6bda8b75bb9fd95f5a7e
midoks <midoks@163.com>
2018-09-13 22:26:34 +0800

    up

--- install/install.sh
+++ install/install.sh
@@ -1,3 +1,13 @@
-#!/bin/sh
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+LANG=en_US.UTF-8
+is64bit=`getconf LONG_BIT`
+
+startTime=`date +%s`
 
 echo "hello"
+
+endTime=`date +%s`
+((outTime=($endTime-$startTime)/60))
+echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
