d6f88caf3d3f636bd5e9f0bedb4447496af7166c
Mr Chen <midoks@163.com>
2018-09-21 14:18:29 +0800

    up

--- main.py
+++ main.py
@@ -1,31 +1,33 @@
 from flask import Flask
 from flask import Blueprint,render_template
-import views
+from views import *
 
 app = Flask(__name__)
 app.debug = True
 
+# @app.route("/")
+# def index():
+#     return render_template('default/index.html')
 
-@app.route("/h")
-def hello():
-    return "Hello World!"
+# def setting_modules(app, modules):
+#     for module, url_prefix in modules:
+#     	print modules
+#     	print url_prefix
+#         app.register_blueprint(module, url_prefix=url_prefix)
 
+# DEFAULT_MODULES = (
+#     (views.frontend, "/"),
+# )
+# setting_modules(app,DEFAULT_MODULES)
 
-@app.route("/")
-def index():
-    return render_template('default/index.html')
+app.register_blueprint(frontend, url_prefix="")
 
-def setting_modules(app, modules):
-    for module, url_prefix in modules:
-        app.register_blueprint(module, url_prefix=url_prefix)
+# simple_page = Blueprint('simple_page', __name__, template_folder='templates')
+# @simple_page.route("/")
+# def index():
+#     return render_template('default/index.html')
 
-DEFAULT_MODULES = (
-    (views.frontend, "/"),
-)
-setting_modules(DEFAULT_MODULES, url_prefix=url_prefix)
-
-
-
+# app.register_blueprint(simple_page,url_prefix="/")
 
 if __name__ == "__main__":
     app.run()
--- views/frontend.py
+++ views/frontend.py
@@ -1,10 +1,9 @@
 from flask import Flask
-from flask import render_template
-
-app = Flask(__name__)
-app.debug = True
+from flask import Blueprint,render_template
 
 
-@app.route("/")
+frontend = Blueprint('frontend', __name__, template_folder='templates')
+
+@frontend.route("/")
 def index():
     return render_template('default/index.html')
