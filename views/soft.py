e1f67afc92ef9b7e47525e014ada89a720b826a5
Mr Chen <midoks@163.com>
2018-09-26 20:02:11 +0800

    更新

--- views/soft.py
+++ views/soft.py
@@ -1,11 +1,10 @@
 # coding:utf-8
 
-from flask import Flask
-from flask import Blueprint,render_template
-
+from flask import Blueprint, render_template
 
 soft = Blueprint('soft', __name__, template_folder='templates')
 
+
 @soft.route("/")
-def index():
+def index ():
     return render_template('default/soft.html')
