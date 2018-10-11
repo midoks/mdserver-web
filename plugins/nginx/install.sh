1d7e98004a83dbe36842b2a339ff958ad2e9c933
Mr Chen <midoks@163.com>
2018-10-11 19:26:06 +0800

    u

--- plugins/nginx/install.sh
+++ plugins/nginx/install.sh
@@ -12,24 +12,13 @@ download_Url=$NODE_URL
 
 Install_safelogin()
 {
-	mkdir -p /www/server/panel/plugin/safelogin
-	echo '正在安装脚本文件...' > $install_tmp
-	wget -O /www/server/panel/plugin/safelogin/safelogin_main.py $download_Url/install/lib/plugin/safelogin/safelogin_main.py -T 5
-	wget -O /www/server/panel/plugin/safelogin/index.html $download_Url/install/lib/plugin/safelogin/index.html -T 5
-	wget -O /www/server/panel/plugin/safelogin/info.json $download_Url/install/lib/plugin/safelogin/info.json -T 5
-	wget -O /www/server/panel/plugin/safelogin/icon.png $download_Url/install/lib/plugin/safelogin/icon.png -T 5
-	echo '安装完成' > $install_tmp
-	
+	echo "Install_safelogin"
 }
 
 Uninstall_safelogin()
 {
-	chattr -i /www/server/panel/plugin/safelogin/token.pl
-	rm -f /www/server/panel/data/limitip.conf
-	sed -i "/ALL/d" /etc/hosts.deny
-	rm -rf /www/server/panel/plugin/safelogin
+	echo "Uninstall_safelogin"
 }
-
 
 action=$1
 host=$2;
