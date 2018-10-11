056aa8f8cb8f962b2c5b4103cc40bc18f92caf92
Mr Chen <midoks@163.com>
2018-10-11 19:30:01 +0800

    Update plugins/nginx/install.sh

--- plugins/nginx/install.sh
+++ plugins/nginx/install.sh
@@ -1,14 +1,6 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-install_tmp='/tmp/bt_install.pl'
-public_file=/www/server/panel/install/public.sh
-if [ ! -f $public_file ];then
-	wget -O $public_file http://download.bt.cn/install/public.sh -T 5;
-fi
-. $public_file
-
-download_Url=$NODE_URL
 
 Install_safelogin()
 {
