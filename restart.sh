283a2c052b1a2cc26d91a454f9f9eac59b91dfa0
Mr Chen <midoks@163.com>
2018-10-29 20:19:59 +0800

    u

---
+++ restart.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+./stop.sh
+./start.sh
--- start.sh
+++ start.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
-gunicorn -b 127.0.0.1:7200 app:app
+gunicorn -b 127.0.0.1:7200 app:app &
+
+python task.py &
 
 #open "http://127.0.0.1:7200"
