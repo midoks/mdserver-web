f2df81694f0c640c6a0dce50c5d61b27b4391a93
Mr Chen <midoks@163.com>
2018-11-23 11:45:53 +0800

    Update install.sh

--- plugins/csvn/install.sh
+++ plugins/csvn/install.sh
@@ -17,7 +17,6 @@ Install_csvn()
 	wget -O $serverPath/tmp/csvn.tar.xz https://github.com/midoks/mdserver-web/releases/download/init/CollabNetSubversionEdge-5.1.4_linux-x86_64.tar.xz
 
 	cd $serverPath/tmp && tar -Jxf $serverPath/tmp/csvn.tar.xz
-	mkdir -p $serverPath/csvn
 	mv $serverPath/tmp/csvn $serverPath/csvn
 	
 	echo '安装完成' > $install_tmp
