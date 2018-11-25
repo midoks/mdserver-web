b214042d7403822fe956918ee257f86ed46abb08
midoks <midoks@163.com>
2018-11-25 17:48:48 +0800

    update

--- plugins/openresty/install.sh
+++ plugins/openresty/install.sh
@@ -7,36 +7,33 @@ rootPath=$(dirname "$curPath")
 rootPath=$(dirname "$rootPath")
 serverPath=$(dirname "$rootPath")
 
-
 install_tmp=${rootPath}/tmp/bt_install.pl
 
 
-Install_mem()
+Install_openresty()
 {
 	mkdir -p $serverPath/tmp
-
 	echo '正在安装脚本文件...' > $install_tmp
 
-	wget -O $serverPath/tmp/memcached.tar.gz http://www.memcached.org/files/memcached-1.5.12.tar.gz
-	cd $serverPath/tmp && tar -zxvf memcached.tar.gz
+	wget -O $serverPath/tmp/openresty.tar.gz https://openresty.org/download/openresty-1.13.6.2.tar.gz
+	cd $serverPath/tmp && tar -zxvf openresty.tar.gz
 
-	mkdir -p $serverPath/memcached
-	cd memcached* && ./configure --prefix=$serverPath/memcached && make && make install
-
+	mkdir -p $serverPath/openresty
+	cd openresty* && ./configure --prefix=$serverPath/openresty && make && make install
+	
 	echo '安装完成' > $install_tmp
-
 	#rm -rf $serverPath/tmp
 }
 
-Uninstall_mem()
+Uninstall_openresty()
 {
-	rm -rf mkdir -p $serverPath/memcached
+	echo "Uninstall_safelogin"
 }
 
-
 action=$1
+host=$2
 if [ "${1}" == 'install' ];then
-	Install_mem
+	Install_openresty
 else
-	Uninstall_mem
+	Uninstall_openresty
 fi
