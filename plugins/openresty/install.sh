d81a069d942117f8808be2c8bfaf44ce55919f71
midoks <midoks@163.com>
2018-11-27 23:29:28 +0800

    openresty

--- plugins/openresty/install.sh
+++ plugins/openresty/install.sh
@@ -9,27 +9,27 @@ serverPath=$(dirname "$rootPath")
 
 install_tmp=${rootPath}/tmp/bt_install.pl
 
+openrestyDir=${serverPath}/source/openresty
 
 Install_openresty()
 {
-	mkdir -p $serverPath/tmp
+	mkdir -p ${openrestyDir}
 	echo '正在安装脚本文件...' > $install_tmp
 
-	if [ ! -d $serverPath/tmp/openresty.tar.gz ];then
-		wget -O $serverPath/tmp/openresty.tar.gz https://openresty.org/download/openresty-1.13.6.2.tar.gz
+	if [ ! -f ${openrestyDir}/openresty-1.13.6.2.tar.gz ];then
+		wget -O ${openrestyDir}/openresty-1.13.6.2.tar.gz https://openresty.org/download/openresty-1.13.6.2.tar.gz
 	fi
-	cd $serverPath/tmp && tar -zxvf openresty.tar.gz
+	cd ${openrestyDir} && tar -zxvf openresty-1.13.6.2.tar.gz
 
-	mkdir -p $serverPath/openresty
-	cd $serverPath/tmp/openresty* && ./configure --prefix=$serverPath/openresty \
-	--with-openssl=$serverPath/tmp/openssl-1.0.2q && make && make install
+	cd ${openrestyDir}/openresty* && ./configure --prefix=$serverPath/openresty \
+	--with-openssl=$serverPath/source/lib/openssl-1.0.2q && make && make install
 	
 	echo '安装完成' > $install_tmp
 }
 
 Uninstall_openresty()
 {
-	rm -f $serverPath/openresty
+	rm -rf $serverPath/openresty
 	echo '卸载完成' > $install_tmp
 }
 
