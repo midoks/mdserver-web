f4dfe502cb3433e3e5ec2ae3de70ebef770bf353
Mr Chen <midoks@163.com>
2018-12-05 17:34:32 +0800

    u

--- route/site.py
+++ route/site.py
@@ -1,9 +1,16 @@
 # coding:utf-8
 
+import time
+import sys
+import os
+import json
+
 from flask import Flask
 from flask import Blueprint, render_template
 
 
+sys.path.append("class/core")
+import public
 site = Blueprint('site', __name__, template_folder='templates')
 
 
@@ -14,4 +21,15 @@ def index():
 
 @site.route('/list', methods=['POST'])
 def list():
-    return ''
+    _list = public.M('sites').where('', ()).field(
+        'id,name,path,status,ps,addtime').limit('0,5').order('id desc').select()
+    _ret = {}
+    _ret['data'] = _list
+
+    count = public.M('sites').where('', ()).count()
+    _page = {}
+    _page['count'] = count
+    _page['tojs'] = 'getWeb'
+
+    _ret['page'] = public.getPage(_page)
+    return public.getJson(_ret)
