629416760fdafa6b34628751dff9a52fd6ca6b80
Mr Chen <midoks@163.com>
2018-12-07 15:55:12 +0800

    Update install.sh

--- plugins/memcached/install.sh
+++ plugins/memcached/install.sh
@@ -13,23 +13,25 @@ install_tmp=${rootPath}/tmp/bt_install.pl
 
 Install_mem()
 {
-	mkdir -p $serverPath/tmp
+	mkdir -p $serverPath/source
 
 	echo '正在安装脚本文件...' > $install_tmp
 
-	wget -O $serverPath/tmp/memcached.tar.gz http://www.memcached.org/files/memcached-1.5.12.tar.gz
+	wget -O $serverPath/source/memcached.tar.gz http://www.memcached.org/files/memcached-1.5.12.tar.gz
 	cd $serverPath/tmp && tar -zxvf memcached.tar.gz
 
 	mkdir -p $serverPath/memcached
 	cd memcached* && ./configure --prefix=$serverPath/memcached && make && make install
 
+	echo '1.5' > $serverPath/memcached/version.pl
+
 	echo '安装完成' > $install_tmp
 
-	#rm -rf $serverPath/tmp
 }
 
 Uninstall_mem()
 {
+	$serverPath/memcached/init.d/memcached stop
 	rm -rf mkdir -p $serverPath/memcached
 }
 
