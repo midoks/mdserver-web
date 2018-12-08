829a90f03e653a4398344f4ee70a05479d26f2bb
midoks <midoks@163.com>
2018-12-08 23:02:58 +0800

    update

--- plugins/csvn/index.py
+++ plugins/csvn/index.py
@@ -106,65 +106,6 @@ def reload():
     return 'fail'
 
 
-def runInfo():
-    # 获取memcached状态
-    import telnetlib
-    import re
-
-    try:
-        tn = telnetlib.Telnet('127.0.0.1', 11211)
-        tn.write(b"stats\n")
-        tn.write(b"quit\n")
-        data = tn.read_all()
-        if type(data) == bytes:
-            data = data.decode('utf-8')
-        data = data.replace('STAT', '').replace('END', '').split("\n")
-        result = {}
-        res = ['cmd_get', 'get_hits', 'get_misses', 'limit_maxbytes', 'curr_items', 'bytes',
-               'evictions', 'limit_maxbytes', 'bytes_written', 'bytes_read', 'curr_connections']
-        for d in data:
-            if len(d) < 3:
-                continue
-            t = d.split()
-            if not t[0] in res:
-                continue
-            result[t[0]] = int(t[1])
-        result['hit'] = 1
-        if result['get_hits'] > 0 and result['cmd_get'] > 0:
-            result['hit'] = float(result['get_hits']) / \
-                float(result['cmd_get']) * 100
-
-        conf = public.readFile(getConf())
-        result['bind'] = re.search('IP=(.+)', conf).groups()[0]
-        result['port'] = int(re.search('PORT=(\d+)', conf).groups()[0])
-        result['maxconn'] = int(re.search('MAXCONN=(\d+)', conf).groups()[0])
-        result['cachesize'] = int(
-            re.search('CACHESIZE=(\d+)', conf).groups()[0])
-        return public.getJson(result)
-    except Exception, e:
-        return public.getJson({})
-
-
-def saveConf():
-     # 设置memcached缓存大小
-    import re
-    confFile = getConf()
-    try:
-        args = getArgs()
-        content = public.readFile(confFile)
-        content = re.sub('IP=.+', 'IP=' + args['ip'], content)
-        content = re.sub('PORT=\d+', 'PORT=' + args['port'], content)
-        content = re.sub('MAXCONN=\d+', 'MAXCONN=' + args['maxconn'], content)
-        content = re.sub('CACHESIZE=\d+', 'CACHESIZE=' +
-                         args['cachesize'], content)
-        public.writeFile(confFile, content)
-        reload()
-        return 'ok'
-    except Exception as e:
-        pass
-    return 'fail'
-
-
 def initdStatus():
     if not app_debug:
         os_name = public.getOs()
