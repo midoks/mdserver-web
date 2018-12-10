a509f26503c842915dd63d2d6470807efc6936a8
Mr Chen <midoks@163.com>
2018-12-10 10:44:54 +0800

    update

--- plugins/mysql/install.sh
+++ plugins/mysql/install.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-install_tmp='/tmp/bt_install.pl'
-public_file=/www/server/panel/install/public.sh
-if [ ! -f $public_file ];then
-	wget -O $public_file http://download.bt.cn/install/public.sh -T 5;
-fi
-. $public_file
 
-download_Url=$NODE_URL
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+
+install_tmp=${rootPath}/tmp/bt_install.pl
 
 Install_safelogin()
 {
@@ -32,9 +32,16 @@ Uninstall_safelogin()
 
 
 action=$1
-host=$2;
-if [ "${1}" == 'install' ];then
-	Install_safelogin
-else
-	Uninstall_safelogin
+type=$2
+
+if [ "${2}" == "" ];then
+	echo '缺少安装脚本...' > $install_tmp
+	exit 0
+fi 
+
+if [ ! -d $curPath/versions/$2 ];then
+	echo '缺少安装脚本2...' > $install_tmp
+	exit 0
 fi
+
+sh -x $curPath/versions/$2/install.sh $1
