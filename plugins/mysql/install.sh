8b3d0cbba41aacb1f2e2cbf7f9dadb242d4f14a4
Mr Chen <midoks@163.com>
2018-12-10 10:45:30 +0800

    update

--- plugins/mysql/install.sh
+++ plugins/mysql/install.sh
@@ -10,26 +10,6 @@ serverPath=$(dirname "$rootPath")
 
 install_tmp=${rootPath}/tmp/bt_install.pl
 
-Install_safelogin()
-{
-	mkdir -p /www/server/panel/plugin/safelogin
-	echo '正在安装脚本文件...' > $install_tmp
-	wget -O /www/server/panel/plugin/safelogin/safelogin_main.py $download_Url/install/lib/plugin/safelogin/safelogin_main.py -T 5
-	wget -O /www/server/panel/plugin/safelogin/index.html $download_Url/install/lib/plugin/safelogin/index.html -T 5
-	wget -O /www/server/panel/plugin/safelogin/info.json $download_Url/install/lib/plugin/safelogin/info.json -T 5
-	wget -O /www/server/panel/plugin/safelogin/icon.png $download_Url/install/lib/plugin/safelogin/icon.png -T 5
-	echo '安装完成' > $install_tmp
-	
-}
-
-Uninstall_safelogin()
-{
-	chattr -i /www/server/panel/plugin/safelogin/token.pl
-	rm -f /www/server/panel/data/limitip.conf
-	sed -i "/ALL/d" /etc/hosts.deny
-	rm -rf /www/server/panel/plugin/safelogin
-}
-
 
 action=$1
 type=$2
