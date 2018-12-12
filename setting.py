7db76547ffb197608bec579e6664283956c79054
midoks <midoks@163.com>
2018-12-13 01:14:25 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -1,8 +1,8 @@
 import os
 import time
 import sys
-
-sys.path.append(os.getcwd() + '/class/core')
+chdir = os.getcwd()
+sys.path.append(chdir + '/class/core')
 import public
 import system_api
 
@@ -17,16 +17,15 @@ workers = cpu_info[1] + 1
 threads = 1
 backlog = 512
 reload = False
-daemon = False
+daemon = True
 timeout = 7200
 keepalive = 1
-chdir = os.getcwd()
 capture_output = True
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
 loglevel = 'info'
-errorlog = os.getcwd() + '/logs/error.log'
-accesslog = os.getcwd() + '/logs/access.log'
-pidfile = os.getcwd() + '/logs/mw.pid'
+errorlog = chdir + '/logs/error.log'
+accesslog = chdir + '/logs/access.log'
+pidfile = chdir + '/logs/mw.pid'
 # if os.path.exists(chdir + '/data/ssl.pl'):
 #     certfile = 'ssl/certificate.pem'
 #     keyfile = 'ssl/privateKey.pem'
