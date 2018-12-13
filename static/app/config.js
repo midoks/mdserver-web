51951a26858403b779e5c076e75cfcc827ee6d1a
Mr Chen <midoks@163.com>
2018-12-13 14:37:31 +0800

    update

---
+++ static/app/config.js
@@ -0,0 +1,47 @@
+
+
+//设置自动更新
+function SetPanelAutoUpload(){
+	loadT = layer.msg(lan.public.config,{icon:16,time:0});
+	$.post('/config?action=AutoUpdatePanel','',function(rdata){
+		layer.close(loadT);
+		layer.msg(rdata.msg,{icon:rdata.status?1:2});
+	});
+}
+
+
+$(".set-submit").click(function(){
+	var data = $("#set-Config").serialize();
+	layer.msg(lan.config.config_save,{icon:16,time:0,shade: [0.3, '#000']});
+	$.post('/config?action=setPanel',data,function(rdata){
+		layer.closeAll();
+		layer.msg(rdata.msg,{icon:rdata.status?1:2});
+		if(rdata.status){
+			setTimeout(function(){
+				window.location.href = ((window.location.protocol.indexOf('https') != -1)?'https://':'http://') + rdata.host + window.location.pathname;
+			},1500);
+		}
+	});
+	
+});
+
+
+function syncDate(){
+	var loadT = layer.msg(lan.config.config_sync,{icon:16,time:0,shade: [0.3, '#000']});
+	$.post('/config?action=syncDate','',function(rdata){
+		layer.close(loadT);
+		layer.msg(rdata.msg,{icon:1});
+		setTimeout(function(){
+				window.location.reload();
+			},1500);
+	});
+}
+
+//PHP守护程序
+function Set502(){
+	var loadT = layer.msg(lan.public.the,{icon:16,time:0,shade: [0.3, '#000']});
+	$.post('/config?action=Set502','',function(rdata){
+		layer.close(loadT);
+		layer.msg(rdata.msg,{icon:rdata.status?1:2});
+	});	
+}
