b5fc0d31737d42a5bf13dd8c04857c4faa522fc5
midoks <midoks@163.com>
2018-12-14 21:35:55 +0800

    update

---
+++ plugins/abkill/install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+
+install_tmp=${rootPath}/tmp/bt_install.pl
+
+
+Install_abkill()
+{
+	echo '正在安装脚本文件...' > $install_tmp
+	mkdir -p $serverPath/abkill
+	echo '安装完成' > $install_tmp
+}
+Uninstall_abkill()
+{
+	rm -rf $serverPath/abkill
+	echo "Uninstall_redis" > $install_tmp
+}
+
+action=$1
+if [ "${1}" == 'install' ];then
+	Install_abkill
+else
+	Uninstall_abkill
+fi
