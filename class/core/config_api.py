c6559e690ced6075b74d5fa4318eb81befdfbda7
Mr Chen <midoks@163.com>
2018-12-29 12:42:29 +0800

    update

---
+++ class/core/config_api.py
@@ -0,0 +1,21 @@
+# coding: utf-8
+
+import psutil
+import time
+import os
+import sys
+import public
+import re
+import json
+import pwd
+
+
+class config_api:
+
+    def __init__(self):
+        pass
+
+    ##### ----- start ----- ###
+    def getApi(self):
+        print '123123'
+    ##### ----- end ----- ###
