f3c75fd00cebf86a705257ac3e0484287924be52
Mr Chen <midoks@163.com>
2019-01-02 13:58:53 +0800

    update

--- class/core/config_api.py
+++ class/core/config_api.py
@@ -21,6 +21,19 @@ class config_api:
         data = {}
 
         data['site_path'] = public.getWwwDir()
+        data['backup_path'] = public.getBackupDir()
+        data['systemdate'] = public.execShell(
+            'date +"%Y-%m-%d %H:%M:%S %Z %z"')[0].strip()
+
+        if os.path.exists('data/port.pl'):
+            data['port'] = public.readFile('data/port.pl').strip()
+        else:
+            data['port'] = '7200'
+
+        if os.path.exists('data/iplist.txt'):
+            data['ip'] = public.readFile('data/iplist.txt').strip()
+        else:
+            data['ip'] = '127.0.0.1'
 
         return data
     ##### ----- end ----- ###
