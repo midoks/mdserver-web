d900298a7b9c88f862804055ca32d4924a5fe42d
Mr Chen <midoks@163.com>
2019-01-16 13:02:05 +0800

    update

--- plugins/sphinx/install.sh
+++ plugins/sphinx/install.sh
@@ -17,7 +17,19 @@ Install_sphinx()
 	echo '正在安装脚本文件...' > $install_tmp
 	mkdir -p $serverPath/sphinx
 
-	echo '4.0' > $serverPath/sphinx/version.pl
+	SPHINX_DIR=${serverPath}/source/sphinx
+	mkdir -p $SPHINX_DIR
+	if [ ! -f ${SPHINX_DIR}/sphinx-3.1.1.tar.gz ];then
+		if [ $sysName == 'Darwin' ]; then
+			wget -O ${SPHINX_DIR}/sphinx-3.1.1.tar.gz http://sphinxsearch.com/files/sphinx-3.1.1-612d99f-darwin-amd64.tar.gz
+		else
+			wget -O ${SPHINX_DIR}/sphinx-3.1.1.tar.gz http://sphinxsearch.com/files/sphinx-3.1.1-612d99f-linux-amd64.tar.gz
+		fi
+	fi
+
+	cp -rf ${SPHINX_DIR}/sphinx-3.1.1/ $serverPath/sphinx/bin
+
+	echo '3.1.1' > $serverPath/sphinx/version.pl
 	echo '安装完成' > $install_tmp
 }
 
