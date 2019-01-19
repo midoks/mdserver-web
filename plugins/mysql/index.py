1f1d54d99ae4eae43bdd6cf965a088322b2b7d0e
Mr Chen <midoks@users.noreply.github.com>
2019-01-19 16:20:20 +0800

    Update index.py

--- plugins/mysql/index.py
+++ plugins/mysql/index.py
@@ -828,6 +828,7 @@ def getDbInfo():
         if not data:
             data = 0
         ret['data_size'] = public.toSize(data)
+        print ret
         ret['database'] = db_name
 
         ret3 = []
