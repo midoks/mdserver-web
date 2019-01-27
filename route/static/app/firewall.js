9492d47a2ad5ad60050cac8ab2a8d88c84b61a93
Mr Chen <midoks@163.com>
2019-01-27 16:15:43 +0800

    Update firewall.js

--- route/static/app/firewall.js
+++ route/static/app/firewall.js
@@ -68,7 +68,7 @@ function getSshInfo(){
 		$("#in_safe").html(SSHchecked);
 		$("#mstscPort").val(rdata.port);
 		var isPint = '';
-		if(rdata.ping){
+		if(!rdata.ping){
 			isPing = "<input class='btswitch btswitch-ios' id='noping' type='checkbox'><label class='btswitch-btn' for='noping' onclick='ping(0)'></label>";
 		}else{
 			isPing = "<input class='btswitch btswitch-ios' id='noping' type='checkbox' checked><label class='btswitch-btn' for='noping' onclick='ping(1)'></label>";
