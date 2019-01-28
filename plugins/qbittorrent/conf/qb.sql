e3c9f30a64c61ba869064cc647c2262a9bf373f8
Mr Chen <midoks@163.com>
2019-01-29 00:39:06 +0800

    Update qb.sql

--- plugins/qbittorrent/conf/qb.sql
+++ plugins/qbittorrent/conf/qb.sql
@@ -1,7 +1,19 @@
 CREATE TABLE `pl_hash_list` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `info_hash` varchar(40) NOT NULL,
+  `length` bigint(20) NOT NULL,
+  `create_time` datetime NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `info_hash` (`info_hash`),
+  KEY `create_time` (`create_time`)
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
+
+CREATE TABLE `pl_hash_file` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
+  `pid` bigint(11)  NOT NULL,
+  `name` varchar(40) NOT NULL,
+  `m3u8` varchar(40) NOT NULL,
   `length` bigint(20) NOT NULL,
   `create_time` datetime NOT NULL,
   PRIMARY KEY (`id`),
