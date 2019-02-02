c880e935d7a891a4c65715ca8a12485863c0191b
Mr Chen <midoks@163.com>
2019-02-02 18:04:42 +0800

    Update install_centos.sh

--- scripts/install_centos.sh
+++ scripts/install_centos.sh
@@ -17,7 +17,7 @@ yum install -y wget curl unzip zip
 
 
 
-if [ ! -f '/www/server/mdserver-web' ];then
+if [ ! -d '/www/server/mdserver-web' ];then
 	wget -O /tmp/master.zip https://codeload.github.com/midoks/mdserver-web/zip/master
 	cd /tmp && unzip /tmp/master.zip
 	mv /tmp/mdserver-web-master /www/server/mdserver-web
