66c9828b6834b9d91385c166386cc52179d1c80e
Mr Chen <midoks@163.com>
2019-03-13 19:24:39 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -50,8 +50,9 @@ Install_Libiconv()
 #----------------------------- libiconv end -------------------------#
 	cd ${sourcePath}
 	if [ ! -d ${libPath}/libiconv ];then
-		wget -O libiconv-1.15.tar.gz  https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz  -T 5
-		tar zxf libiconv-1.15.tar.gz
+		# wget -O libiconv-1.15.tar.gz  https://ftp.gnu.org/pub/gnu/libiconv/libiconv-1.15.tar.gz  -T 5
+		wget -O libiconv-1.15.tar.gz  https://github.com/midoks/mdserver-web/releases/download/init/libiconv-1.15.tar.gz  -T 5
+		tar zxvf libiconv-1.15.tar.gz
 		cd libiconv-1.15
 	    ./configure --prefix=${libPath}/libiconv --enable-static
 	    make && make install
