5ed1f4adb3575ebb6c2ae5b2a30df128847ceaa8
Mr Chen <midoks@163.com>
2019-03-13 17:19:33 +0800

    op

--- plugins/mysql/versions/5.5/install.sh
+++ plugins/mysql/versions/5.5/install.sh
@@ -11,7 +11,7 @@ rootPath=$(dirname "$rootPath")
 serverPath=$(dirname "$rootPath")
 
 
-install_tmp=${rootPath}/tmp/bt_install.pl
+install_tmp=${rootPath}/tmp/mw_install.pl
 mysqlDir=${serverPath}/source/mysql
 
 Install_mysql()
