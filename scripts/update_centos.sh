e290af5403c96d1b3a3dfb7c8d2e8258957f7acd
Mr Chen <midoks@163.com>
2019-03-13 19:52:10 +0800

    Update update_centos.sh

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -5,14 +5,11 @@ LANG=en_US.UTF-8
 
 wget -O /tmp/master.zip https://codeload.github.com/midoks/mdserver-web/zip/master
 cd /tmp && unzip /tmp/master.zip
-
-
 /usr/bin/cp -rf  /tmp/mdserver-web-master/* /www/server/mdserver-web
 rm -rf /tmp/master.zip
 rm -rf /tmp/mdserver-web-master
 
 cd /www/server/mdserver-web/scripts && sh lib.sh
-
 pip install -r /www/server/mdserver-web/requirements.txt
 
 
