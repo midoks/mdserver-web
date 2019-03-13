0bf9c1fe69e0cf266cacec075a69ec821cf08c54
Mr Chen <midoks@163.com>
2019-03-13 19:45:55 +0800

    Update update_centos.sh

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -18,7 +18,21 @@ cd /www/server/mdserver-web/scripts && sh lib.sh
 pip install -r /www/server/mdserver-web/requirements.txt
 
 cd /www/server/mdserver-web && sh cli.sh start
-sleep 5
-cd /www/server/mdserver-web && sh cli.sh stop
-cd /www/server/mdserver-web && sh scripts/init.d/mw default
-cd /www/server/mdserver-web && sh cli.sh start
+
+isStart=`ps -ef|grep 'gunicorn -c setting.py app:app' |grep -v grep|awk '{print $2}'`
+port=$(cat data/port.pl)
+while [[ "$isStart" == "" ]];
+do
+    echo -e ".\c"
+    sleep 0.5
+    isStart=$(lsof -n -P -i:$port|grep LISTEN|grep -v grep|awk '{print $2}'|xargs)
+    let n+=1
+    if [ $n -gt 8 ];then
+        break;
+    fi
+done
+
+cd /www/server/mdserver-web \
+&& sh cli.sh stop \
+&& sh cli.sh start \
+&& sh scripts/init.d/mw default
