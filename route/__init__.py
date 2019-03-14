1be78df66901edc67b45d88679b8d3e858eff6d5
Mr Chen <midoks@163.com>
2019-03-14 12:51:20 +0800

    Update __init__.py

--- route/__init__.py
+++ route/__init__.py
@@ -283,6 +283,7 @@ def get_input_data(data):
 
 @socketio.on('webssh')
 def webssh(msg):
+    print 'webssh ...'
     if not isLogined():
         emit('server_response', {'data': '会话丢失，请重新登陆面板!\r\n'})
         return None
