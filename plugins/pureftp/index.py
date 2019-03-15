a46afc279cbb49149783f592af116f98cdf2aeaa
Mr Chen <midoks@163.com>
2019-03-15 16:50:20 +0800

    pureftp 自启动 done

--- plugins/pureftp/index.py
+++ plugins/pureftp/index.py
@@ -166,6 +166,7 @@ def initdInstall():
     initd_bin = getInitDFile()
     shutil.copyfile(source_bin, initd_bin)
     public.execShell('chmod +x ' + initd_bin)
+    public.execShell('chkconfig --add ' + getPluginName())
     return 'ok'
 
 
@@ -176,6 +177,7 @@ def initdUinstall():
             return "Apple Computer does not support"
     initd_bin = getInitDFile()
     os.remove(initd_bin)
+    public.execShell('chkconfig --del ' + getPluginName())
     return 'ok'
 
 
