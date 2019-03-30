5124ac6599f19736746adb0bb5dc108af1320799
midoks <midoks@163.com>
2019-03-30 11:43:31 +0800

    Update index.py

--- plugins/bbr/index.py
+++ plugins/bbr/index.py
@@ -11,6 +11,11 @@ sys.path.append(os.getcwd() + "/class/core")
 import public
 
 
+app_debug = False
+if public.isAppleSystem():
+    app_debug = True
+
+
 def getPluginName():
     return 'bbr'
 
@@ -47,6 +52,9 @@ def getArgs():
 
 
 def status():
+    if not app_debug:
+        if public.isAppleSystem():
+            return "stop"
 
     data = public.execShell('sudo sysctl -n net.ipv4.tcp_congestion_control')
     r = data[0].strip()
@@ -54,7 +62,48 @@ def status():
         return 'start'
     return 'stop'
 
+
+def start():
+    if not app_debug:
+        if public.isAppleSystem():
+            return "Apple Computer does not support"
+
+    public.execShell('echo "net.core.default_qdisc=fq" >> /etc/sysctl.conf')
+    cmd = 'echo "net.ipv4.tcp_congestion_control=bbr" >> /etc/sysctl.conf'
+    public.execShell(cmd)
+    public.execShell('sysctl -p')
+    return 'ok'
+
+
+def stop():
+    if not app_debug:
+        if public.isAppleSystem():
+            return "Apple Computer does not support"
+
+    cmd1 = "sed -i '/net\.core\.default_qdisc=fq/d' /etc/sysctl.conf"
+    public.execShell(cmd1)
+    cmd2 = "sed -i '/net\.ipv4\.tcp_congestion_control=bbr/d' /etc/sysctl.conf"
+    public.execShell(cmd2)
+    public.execShell("sysctl -p")
+    return 'ok'
+
+
+def restart():
+    return '无效'
+
+
+def reload():
+    return '无效'
+
 if __name__ == "__main__":
     func = sys.argv[1]
     if func == 'status':
         print status()
+    elif func == 'start':
+        print start()
+    elif func == 'stop':
+        print stop()
+    elif func == 'restart':
+        print restart()
+    elif func == 'reload':
+        print reload()
