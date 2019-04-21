954df1b522a202e0b1aa12940ec72c66337a3706
midoks <midoks@163.com>
2019-04-21 18:22:31 +0800

    up

---
+++ plugins/op_firewall/install.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+
+action=$1
+type=$2
+
+if id www &> /dev/null ;then 
+    echo "www UID is `id -u www`"
+    echo "www Shell is `grep "^www:" /etc/passwd |cut -d':' -f7 `"
+else
+    groupadd www
+	useradd -g www -s /sbin/nologin www
+fi
+
+if [ "${2}" == "" ];then
+	echo '缺少安装脚本...' > $install_tmp
+	exit 0
+fi 
+
+if [ ! -d $curPath/versions/$2 ];then
+	echo '缺少安装脚本2...' > $install_tmp
+	exit 0
+fi
+
+sh -x $curPath/versions/$2/install.sh $1
