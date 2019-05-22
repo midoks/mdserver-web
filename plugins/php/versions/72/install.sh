b9f2974dded90b81e42d4700860ef84c8fe3559b
Mr Chen <midoks@163.com>
2019-05-22 13:07:25 +0800

    更新PHP版本

--- plugins/php/versions/72/install.sh
+++ plugins/php/versions/72/install.sh
@@ -11,7 +11,7 @@ sysName=`uname`
 install_tmp=${rootPath}/tmp/mw_install.pl
 
 
-version=7.2.16
+version=7.2.18
 Install_php()
 {
 #------------------------ install start ------------------------------------#
--- plugins/php/versions/73/install.sh
+++ plugins/php/versions/73/install.sh
@@ -11,7 +11,7 @@ sysName=`uname`
 install_tmp=${rootPath}/tmp/mw_install.pl
 
 
-version=7.3.3
+version=7.3.5
 Install_php()
 {
 #------------------------ install start ------------------------------------#
