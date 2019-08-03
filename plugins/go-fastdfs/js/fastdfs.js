abd7542888b118a2dba82c02e0b5ed43df2acce0
midoks <midoks@163.com>
2019-08-03 11:00:59 +0800

    up

--- plugins/go-fastdfs/js/fastdfs.js
+++ plugins/go-fastdfs/js/fastdfs.js
@@ -37,6 +37,57 @@ function pPost(method,args,callback, title){
 }
 
 
+
+function gfConfigSet(){
+    pPost('gf_conf_set', '', function(data){
+        var rdata = $.parseJSON(data.data);
+
+        var mlist = '';
+        for (var i = 0; i < rdata.length; i++) {
+            var w = '140';
+            if (rdata[i].name == 'error_reporting') w = '250';
+            var ibody = '<input style="width: ' + w + 'px;" class="bt-input-text mr5" name="' + rdata[i].name + '" value="' + rdata[i].value + '" type="text" >';
+            switch (rdata[i].type) {
+                case 0:
+                    var selected_1 = (rdata[i].value == 1) ? 'selected' : '';
+                    var selected_0 = (rdata[i].value == 0) ? 'selected' : '';
+                    ibody = '<select class="bt-input-text mr5" name="' + rdata[i].name + '" style="width: ' + w + 'px;">\
+                        <option value="1" ' + selected_1 + '>开启</option>\
+                        <option value="0" ' + selected_0 + '>关闭</option>\
+                        </select>';
+                    break;
+                case 1:
+                    var selected_1 = (rdata[i].value == 'On') ? 'selected' : '';
+                    var selected_0 = (rdata[i].value == 'Off') ? 'selected' : '';
+                    ibody = '<select class="bt-input-text mr5" name="' + rdata[i].name + '" style="width: ' + w + 'px;">\
+                        <option value="On" ' + selected_1 + '>开启</option>\
+                        <option value="Off" ' + selected_0 + '>关闭</option></select>'
+                    break;
+                case 2:
+                    var selected_1 = (rdata[i].value == 'true') ? 'selected' : '';
+                    var selected_0 = (rdata[i].value == 'false') ? 'selected' : '';
+                    ibody = '<select class="bt-input-text mr5" name="' + rdata[i].name + '" style="width: ' + w + 'px;">\
+                        <option value="true" ' + selected_1 + '>开启</option>\
+                        <option value="false" ' + selected_0 + '>关闭</option></select>'
+                    break;
+            }
+            mlist += '<p><span>' + rdata[i].name + '</span>' + ibody + ', <font>' + rdata[i].ps + '</font></p>'
+        }
+        var html = '<style>.conf_p p{margin-bottom: 2px}</style><div class="conf_p" style="margin-bottom:0">\
+                        ' + mlist + '\
+                        <div style="margin-top:10px; padding-right:15px" class="text-right">\
+                        <button class="btn btn-success btn-sm mr5" onclick="gogsSetConfig()">刷新</button>\
+                        <button class="btn btn-success btn-sm" onclick="submitGogsConf()">保存</button></div>\
+                    </div>';
+        $(".soft-man-con").html(html);
+    });
+}
+
+
+
+
+
+
 function pRead(){
 	var readme = '<ul class="help-info-text c7">';
     readme += '<li>使用默认solr端口,如有需要自行修改</li>';
