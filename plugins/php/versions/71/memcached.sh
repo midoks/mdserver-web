21656a87e110d046d15bbd1a5f39a9421297448a
midoks <midoks@163.com>
2019-09-06 21:03:35 +0800

    Update memcached.sh

--- plugins/php/versions/71/memcached.sh
+++ plugins/php/versions/71/memcached.sh
@@ -38,7 +38,8 @@ Install_lib()
 		$serverPath/php/$version/bin/phpize
 		./configure --with-php-config=$serverPath/php/$version/bin/php-config \
 		--enable-memcache --with-zlib-dir=$serverPath/lib/zlib \
-		--with-libmemcached-dir=$serverPath/lib/libmemcached
+		--with-libmemcached-dir=$serverPath/lib/libmemcached \
+		--disable-memcached-sasl && \
 		make && make install
 
 		cd $php_lib
