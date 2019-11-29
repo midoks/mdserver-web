99ef4b52f0ab793682fc4af3d5dafe752c08c460
midoks <midoks@163.com>
2019-11-29 19:27:08 +0800

    up

--- plugins/php/all_test.sh
+++ plugins/php/all_test.sh
@@ -18,3 +18,125 @@ done
 echo "php${PHP_VER} -- end"
 
 
+
+PHP_VER=54
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=55
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=56
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+
+PHP_VER=70
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=71
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=72
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=73
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
+PHP_VER=74
+echo "php${PHP_VER} -- start"
+cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
+cd $DIR/versions/$PHP_VER && sh install.sh
+for ii in $cmd_ext
+do
+	if [ "install.sh" == "$ii" ];then
+		echo ''
+	else
+		cd $DIR/versions/$PHP_VER &&  /bin/bash $ii install $PHP_VER
+	fi 
+done
+echo "php${PHP_VER} -- end"
+
+
