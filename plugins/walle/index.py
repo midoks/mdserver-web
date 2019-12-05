590a13256d5e575dc39462759fb815785e81360f
midoks <midoks@163.com>
2019-12-05 23:52:04 +0800

    up

---
+++ plugins/walle/index.py
@@ -0,0 +1,190 @@
+# coding: utf-8
+
+import time
+import random
+import os
+import json
+import re
+import sys
+import subprocess
+
+sys.path.append(os.getcwd() + "/class/core")
+import public
+
+app_debug = False
+if public.isAppleSystem():
+    app_debug = True
+
+
+def getPluginName():
+    return 'walle'
+
+
+def getPluginDir():
+    return public.getPluginDir() + '/' + getPluginName()
+
+
+def getServerDir():
+    return public.getServerDir() + '/' + getPluginName()
+
+
+def getInitDFile():
+    if app_debug:
+        return '/tmp/' + getPluginName()
+    return '/etc/init.d/' + getPluginName()
+
+
+def getInitDTpl():
+    return getPluginDir() + "/init.d/" + getPluginName() + ".tpl"
+
+
+def getLog():
+    return getServerDir() + "/code/logs/error.log"
+
+
+def getArgs():
+    args = sys.argv[2:]
+    tmp = {}
+    args_len = len(args)
+
+    if args_len == 1:
+        t = args[0].strip('{').strip('}')
+        t = t.split(':')
+        tmp[t[0]] = t[1]
+    elif args_len > 1:
+        for i in range(len(args)):
+            t = args[i].split(':')
+            tmp[t[0]] = t[1]
+
+    return tmp
+
+
+def checkArgs(data, ck=[]):
+    for i in range(len(ck)):
+        if not ck[i] in data:
+            return (False, public.returnJson(False, '参数:(' + ck[i] + ')没有!'))
+    return (True, public.returnJson(True, 'ok'))
+
+
+def status():
+    pn = getPluginName()
+    cmd = "ps -ef|grep 'waller.py' | grep -v grep | awk '{print $2}'"
+    data = public.execShell(cmd)
+    if data[0] == '':
+        return 'stop'
+    return 'start'
+
+
+def initDreplace():
+
+    file_tpl = getInitDTpl()
+    service_path = os.path.dirname(os.getcwd())
+
+    initD_path = getServerDir() + '/init.d'
+    if not os.path.exists(initD_path):
+        os.mkdir(initD_path)
+
+    file_bin = initD_path + '/' + getPluginName()
+    if not os.path.exists(file_bin):
+        content = public.readFile(file_tpl)
+        content = content.replace('{$SERVER_PATH}', service_path)
+        public.writeFile(file_bin, content)
+        public.execShell('chmod +x ' + file_bin)
+
+    return file_bin
+
+
+def runShell(shell):
+    data = public.execShell(shell)
+    return data
+
+
+def start():
+    file = initDreplace()
+    cmd = file + ' start'
+    data = subprocess.Popen(
+        cmd, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    return 'ok'
+
+
+def stop():
+    file = initDreplace()
+    data = runShell(file + ' stop')
+    if data[1] == '':
+        return 'ok'
+    return 'fail'
+
+
+def restart():
+    file = initDreplace()
+    data = runShell(file + ' restart')
+    if data[1] == '':
+        return 'ok'
+    return 'fail'
+
+
+def reload():
+    file = initDreplace()
+    data = runShell(file + ' reload')
+
+    solr_log = getServerDir() + "/code/logs/walle.log"
+    public.writeFile(solr_log, "")
+
+    if data[1] == '':
+        return 'ok'
+    return 'fail'
+
+
+def initdStatus():
+    initd_bin = getInitDFile()
+    if os.path.exists(initd_bin):
+        return 'ok'
+    return 'fail'
+
+
+def initdInstall():
+    import shutil
+
+    source_bin = initDreplace()
+    initd_bin = getInitDFile()
+    shutil.copyfile(source_bin, initd_bin)
+    public.execShell('chmod +x ' + initd_bin)
+
+    if not app_debug:
+        public.execShell('chkconfig --add ' + getPluginName())
+    return 'ok'
+
+
+def initdUinstall():
+    if not app_debug:
+        public.execShell('chkconfig --del ' + getPluginName())
+
+    initd_bin = getInitDFile()
+
+    if os.path.exists(initd_bin):
+        os.remove(initd_bin)
+    return 'ok'
+
+# rsyncdReceive
+if __name__ == "__main__":
+    func = sys.argv[1]
+    if func == 'status':
+        print status()
+    elif func == 'start':
+        print start()
+    elif func == 'stop':
+        print stop()
+    elif func == 'restart':
+        print restart()
+    elif func == 'reload':
+        print reload()
+    elif func == 'initd_status':
+        print initdStatus()
+    elif func == 'initd_install':
+        print initdInstall()
+    elif func == 'initd_uninstall':
+        print initdUinstall()
+    elif func == 'run_log':
+        print getLog()
+    else:
+        print 'error'
---
+++ plugins/walle/js/walle.js
@@ -0,0 +1,71 @@
+function str2Obj(str){
+    var data = {};
+    kv = str.split('&');
+    for(i in kv){
+        v = kv[i].split('=');
+        data[v[0]] = v[1];
+    }
+    return data;
+}
+
+function pPost(method,args,callback, title){
+    var _args = null; 
+    if (typeof(args) == 'string'){
+        _args = JSON.stringify(str2Obj(args));
+    } else {
+        _args = JSON.stringify(args);
+    }
+
+    var _title = '正在获取...';
+    if (typeof(title) != 'undefined'){
+        _title = title;
+    }
+
+    var loadT = layer.msg(_title, { icon: 16, time: 0, shade: 0.3 });
+    $.post('/plugins/run', {name:'walle', func:method, args:_args}, function(data) {
+        layer.close(loadT);
+        if (!data.status){
+            layer.msg(data.msg,{icon:0,time:2000,shade: [0.3, '#000']});
+            return;
+        }
+        if(typeof(callback) == 'function'){
+            callback(data);
+        }
+    },'json'); 
+}
+
+function pPostCallbak(method, version, args,callback){
+    var loadT = layer.msg('正在获取...', { icon: 16, time: 0, shade: 0.3 });
+
+    var req_data = {};
+    req_data['name'] = 'walle';
+    req_data['func'] = method;
+    args['version'] = version;
+ 
+    if (typeof(args) == 'string'){
+        req_data['args'] = JSON.stringify(str2Obj(args));
+    } else {
+        req_data['args'] = JSON.stringify(args);
+    }
+
+    $.post('/plugins/callback', req_data, function(data) {
+        layer.close(loadT);
+        if (!data.status){
+            layer.msg(data.msg,{icon:0,time:2000,shade: [0.3, '#000']});
+            return;
+        }
+
+        if(typeof(callback) == 'function'){
+            callback(data);
+        }
+    },'json'); 
+}
+
+
+function pRead(){
+	var readme = '<ul class="help-info-text c7">';
+    readme += '<li>使用默认walle端口5000,如有需要自行修改</li>';
+    readme += '</ul>';
+
+    $('.soft-man-con').html(readme);   
+}
