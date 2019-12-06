94ac06b35c0703752309f5edd5d364abc0ce3a0a
midoks <midoks@163.com>
2019-12-07 00:48:18 +0800

    up

--- plugins/sys-opt/index.py
+++ plugins/sys-opt/index.py
@@ -14,7 +14,13 @@ import public
 def status():
     return 'start'
 
+
+def sysConf():
+    return '/etc/sysctl.conf'
+
 if __name__ == "__main__":
     func = sys.argv[1]
     if func == 'status':
         print status()
+    elif func == 'conf':
+        print sysConf()
