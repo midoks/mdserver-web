a4ce338088158b847f7d3fd4e0a121ead8fdb590
midoks <midoks@163.com>
2019-12-13 02:54:48 +0800

    up

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -3,6 +3,11 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+
+if [ -f /etc/motd ];then
+    echo "Welcome to mdserver-web panel" > /etc/motd
+fi
+
 sed -i 's#SELINUX=enforcing#SELINUX=disabled#g' /etc/selinux/config
 
 wget -O /tmp/master.zip https://codeload.github.com/midoks/mdserver-web/zip/master
