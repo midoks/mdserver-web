779b9bcf5109562ec2afd61752251174b3a0353e
midoks <midoks@163.com>
2020-07-11 23:29:49 +0800

    Update memcached.sh

--- plugins/php/versions/56/memcached.sh
+++ plugins/php/versions/56/memcached.sh
@@ -81,8 +81,8 @@ Uninstall_lib()
 		return
 	fi
 	
-	sed -i BAK "/${LIBNAME}.so/d" $serverPath/php/$version/etc/php.ini
-	sed -i BAK "/${LIBNAME}/d" $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/${LIBNAME}.so/d" $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/${LIBNAME}/d" $serverPath/php/$version/etc/php.ini
 		
 	rm -f $extFile
 	$serverPath/php/init.d/php$version reload
