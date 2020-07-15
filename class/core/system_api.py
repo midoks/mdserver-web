13f6ee18f99c5a539fdd71e63082eed318cb7f03
midoks <midoks@163.com>
2020-07-15 12:38:32 +0800

    Update system_api.py

--- class/core/system_api.py
+++ class/core/system_api.py
@@ -676,8 +676,7 @@ class system_api:
                 mw.execShell('unzip -o ' + toPath + '/mw.zip' + ' -d ./')
                 mw.execShell('unzip -o mw.zip -d ./')
                 mw.execShell('rm -f mw.zip')
-                self.restartApi()
-                return mw.returnJson(True, '安装更新成功!')
+                return mw.returnJson(True, '安装更新成功,需自己重启!')
 
             return mw.returnJson(False, '已经是最新,无需更新!')
         except Exception as ex:
