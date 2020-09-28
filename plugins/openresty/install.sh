1ce6cbef51f229ca26611714cbaa599402b785d2
midoks <midoks@163.com>
2020-09-28 12:52:11 +0800

    Update install.sh

--- plugins/openresty/install.sh
+++ plugins/openresty/install.sh
@@ -20,7 +20,8 @@ if id www &> /dev/null ;then
     echo "www Shell is `grep "^www:" /etc/passwd |cut -d':' -f7 `"
 else
     groupadd www
-	useradd -g www -s /sbin/nologin www
+	# useradd -g www -s /sbin/nologin www
+	useradd -g www -s /bin/bash www
 fi
 
 Install_openresty()
