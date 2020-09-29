d988286eba996760e6335976c14e82178f29caac
midoks <midoks@163.com>
2020-09-30 02:06:04 +0800

    xhporf 52

---
+++ plugins/php/versions/52/xhprof.sh
@@ -0,0 +1,101 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sourcePath=${serverPath}/source/php
+
+LIBNAME=xhprof
+LIBV=0.9.4
+sysName=`uname`
+actionType=$1
+version=$2
+extFile=$serverPath/php/${version}/lib/php/extensions/no-debug-non-zts-20060613/${LIBNAME}.so
+
+if [ "$sysName" == "Darwin" ];then
+	BAK='_bak'
+else
+	BAK=''
+fi
+
+Install_lib()
+{
+	isInstall=`cat $serverPath/php/$version/etc/php.ini|grep "${LIBNAME}.so"`
+	if [ "${isInstall}" != "" ];then
+		echo "php-$version 已安装${LIBNAME},请选择其它版本!"
+		return
+	fi
+	
+	if [ ! -f "$extFile" ];then
+		php_lib=$sourcePath/php_lib
+		mkdir -p $php_lib
+		if [ ! -d $php_lib/${LIBNAME}-${LIBV} ];then
+			wget -O $php_lib/${LIBNAME}-${LIBV}.tgz http://pecl.php.net/get/${LIBNAME}-${LIBV}.tgz
+			cd $php_lib && tar xvf ${LIBNAME}-${LIBV}.tgz
+		fi 
+		cd $php_lib/${LIBNAME}-${LIBV}/extension
+
+		$serverPath/php/$version/bin/phpize
+		./configure --enable-xhprof \
+		--with-php-config=$serverPath/php/$version/bin/php-config
+		make && make install && make clean
+
+	fi
+	
+	if [ ! -f "$extFile" ];then
+		echo "ERROR!"
+		return
+	fi
+
+	if [ ! -f /tmp/xhprof ];then
+		mkdir -p /tmp/xhprof
+		chown -R www:www /tmp/xhprof
+	fi
+
+	echo "" >> $serverPath/php/$version/etc/php.ini
+	echo "[${LIBNAME}]" >> $serverPath/php/$version/etc/php.ini
+	echo "extension=${LIBNAME}.so" >> $serverPath/php/$version/etc/php.ini
+	echo "${LIBNAME}.output_dir=/tmp/xhprof" >> $serverPath/php/$version/etc/php.ini
+
+
+	$serverPath/php/init.d/php$version reload
+	echo '==========================================================='
+	echo 'successful!'
+}
+
+
+Uninstall_lib()
+{
+	if [ ! -f "$serverPath/php/$version/bin/php-config" ];then
+		echo "php-$version 未安装,请选择其它版本!"
+		return
+	fi
+	
+	if [ ! -f "$extFile" ];then
+		echo "php-$version 未安装${LIBNAME},请选择其它版本!"
+		echo "php-$version not install ${LIBNAME}, Plese select other version!"
+		return
+	fi
+	
+	sed -i $BAK "/${LIBNAME}.so/d" $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/${LIBNAME}/d" $serverPath/php/$version/etc/php.ini
+		
+	rm -f $extFile
+	
+	$serverPath/php/init.d/php$version reload
+	echo '==============================================='
+	echo 'successful!'
+}
+
+
+if [ "$actionType" == 'install' ];then
+	Install_lib
+elif [ "$actionType" == 'uninstall' ];then
+	Uninstall_lib
+fi
