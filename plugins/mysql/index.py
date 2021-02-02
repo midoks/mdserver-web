2573a8c94278d5f8741b79d0ac5636cd4cab56e1
midoks <midoks@163.com>
2021-02-02 22:06:49 +0800

    Update index.py

--- plugins/mysql/index.py
+++ plugins/mysql/index.py
@@ -555,6 +555,8 @@ def isSqlError(mysqlMsg):
         return mw.returnJson(False, 'MySQLdb组件缺失! <br>进入SSH命令行输入： pip install mysql-python')
     if "2002," in mysqlMsg:
         return mw.returnJson(False, '数据库连接失败,请检查数据库服务是否启动!')
+    if "2003," in mysqlMsg:
+        return mw.returnJson(False, "Can't connect to MySQL server on '127.0.0.1' (61)")
     if "using password:" in mysqlMsg:
         return mw.returnJson(False, '数据库管理密码错误!')
     if "Connection refused" in mysqlMsg:
