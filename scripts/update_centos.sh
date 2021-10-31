6eaed3c13b8530de706d0de128584b7118fc22ec
midoks <midoks@163.com>
2021-11-01 00:36:22 +0800

    Update update_centos.sh

--- scripts/update_centos.sh
+++ scripts/update_centos.sh
@@ -21,8 +21,11 @@ yum install -y curl-devel libmcrypt libmcrypt-devel python36-devel
 
 cd /www/server/mdserver-web/scripts && sh lib.sh
 
-pip3 install -r /www/server/mdserver-web/requirements.txt
+chmod 755 /www/server/mdserver-web/data
 
+#venv
+cd /www/server/mdserver-web && python3 -m venv .
+cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate && pip3 install -r /www/server/mdserver-web/requirements.txt
 
 sh /etc/init.d/mw stop && rm -rf  /www/server/mdserver-web/scripts/init.d/mw && rm -rf  /etc/init.d/mw
 
