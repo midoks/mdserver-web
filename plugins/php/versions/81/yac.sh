56cb58d433286884f875279e420422c55577bfc7
midoks <midoks@163.com>
2021-12-03 17:01:24 +0800

    up

---
+++ plugins/php/versions/81/yac.sh
@@ -0,0 +1,99 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sourcePath=${serverPath}/source/php
+
+actionType=$1
+version=$2
+
+LIBNAME=yac
+LIBV=2.2.1
+
+NON_ZTS_FILENAME=`ls $serverPath/php/${version}/lib/php/extensions | grep no-debug-non-zts`
+extFile=$serverPath/php/${version}/lib/php/extensions/${NON_ZTS_FILENAME}/${LIBNAME}.so
+
+sysName=`uname`
+if [ "$sysName" == "Darwin" ];then
+	BAK='_bak'
+else
+	BAK=''
+fi
+
+
+Install_lib()
+{
+	isInstall=`cat $serverPath/php/$version/etc/php.ini|grep "${LIBNAME}.so"`
+	if [ "$isInstall" != "" ];then
+		echo "php-$version 已安装${LIBNAME},请选择其它版本!"
+		return
+	fi
+	
+	if [ ! -f "$extFile" ];then
+
+		php_lib=$sourcePath/php_lib
+		mkdir -p $php_lib
+
+		if [ ! -f $php_lib/${LIBNAME}-${LIBV} ];then
+			wget -O $php_lib/${LIBNAME}-${LIBV}.tgz http://pecl.php.net/get/${LIBNAME}-${LIBV}.tgz
+			cd $php_lib
+			tar xvf ${LIBNAME}-${LIBV}.tgz
+		fi 
+		cd $php_lib/${LIBNAME}-${LIBV}
+
+		$serverPath/php/$version/bin/phpize
+		./configure --with-php-config=$serverPath/php/$version/bin/php-config
+		make && make install && make clean
+		cd ..
+	fi
+	
+	if [ ! -f "$extFile" ];then
+		echo "ERROR!"
+		return;
+	fi
+	
+	echo  "" >> $serverPath/php/$version/etc/php.ini
+	echo  "[${LIBNAME}]" >> $serverPath/php/$version/etc/php.ini
+	echo  "extension=${LIBNAME}.so" >> $serverPath/php/$version/etc/php.ini
+	echo  "${LIBNAME}.use_namespace=1" >> $serverPath/php/$version/etc/php.ini
+	
+	$serverPath/php/init.d/php$version reload
+	echo '==========================================================='
+	echo 'successful!'
+}
+
+Uninstall_lib()
+{
+	if [ ! -f "$serverPath/php/$version/bin/php-config" ];then
+		echo "php$version 未安装,请选择其它版本!"
+		return
+	fi
+
+	if [ ! -f "$extFile" ];then
+		echo "php$version 未安装yaf,请选择其它版本!"
+		return
+	fi
+	
+	echo $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/${LIBNAME}.so/d" $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/\[${LIBNAME}\]/d"  $serverPath/php/$version/etc/php.ini
+		
+	rm -f $extFile
+	$serverPath/php/init.d/php$version reload
+	echo '==============================================='
+	echo 'successful!'
+}
+
+
+if [ "$actionType" == 'install' ];then
+	Install_lib
+elif [ "$actionType" == 'uninstall' ];then
+	Uninstall_lib
+fi
