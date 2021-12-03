31b1f88a02fc998893ba3229b4a40c0bdf4a6677
midoks <midoks@163.com>
2021-12-03 20:57:22 +0800

    up

--- plugins/php/versions/55/memcached.sh
+++ plugins/php/versions/55/memcached.sh
@@ -56,6 +56,9 @@ Install_lib()
 		echo "ERROR!"
 		return
 	fi
+
+	echo "" >> $serverPath/php/$version/etc/php.ini
+	echo "[${LIBNAME}]" >> $serverPath/php/$version/etc/php.ini
 	echo "extension=${LIBNAME}.so" >> $serverPath/php/$version/etc/php.ini
 
 	$serverPath/php/init.d/php$version reload
