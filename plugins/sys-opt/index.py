b9b020428cf3b9d86adece76b18cab5e177ffd86
midoks <midoks@163.com>
2021-12-07 18:10:26 +0800

    Update index.py

--- plugins/sys-opt/index.py
+++ plugins/sys-opt/index.py
@@ -23,6 +23,29 @@ def getServerDir():
     return mw.getServerDir() + '/' + getPluginName()
 
 
+def getArgs():
+    args = sys.argv[2:]
+    tmp = {}
+    args_len = len(args)
+
+    if args_len == 1:
+        t = args[0].strip('{').strip('}')
+        t = t.split(':')
+        tmp[t[0]] = t[1]
+    elif args_len > 1:
+        for i in range(len(args)):
+            t = args[i].split(':')
+            tmp[t[0]] = t[1]
+    return tmp
+
+
+def checkArgs(data, ck=[]):
+    for i in range(len(ck)):
+        if not ck[i] in data:
+            return (False, mw.returnJson(False, '参数:(' + ck[i] + ')没有!'))
+    return (True, mw.returnJson(True, 'ok'))
+
+
 def status():
     return 'start'
 
