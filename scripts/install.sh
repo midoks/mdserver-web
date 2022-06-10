43b878120362e9d58a5154149b279e8ad3e27182
midoks <midoks@163.com>
2022-06-10 15:46:52 +0800

    up

--- scripts/install.sh
+++ scripts/install.sh
@@ -13,39 +13,31 @@ startTime=`date +%s`
 _os=`uname`
 echo "use system: ${_os}"
 if [ "${_os}" == "Darwin" ]; then
-	OSNAME='MAC'
+	OSNAME='macos'
 elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
-	OSNAME='CentOS'
+	OSNAME='centos'
 elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
-	OSNAME='RHEL'
+	OSNAME='rhel'
 elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
-	OSNAME='Aliyun'
+	OSNAME='aliyun'
 elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
-	OSNAME='Fedora'
+	OSNAME='fedora'
 elif grep -Eqi "Amazon Linux AMI" /etc/issue || grep -Eq "Amazon Linux AMI" /etc/*-release; then
-	OSNAME='Amazon'
+	OSNAME='amazon'
 elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
-	OSNAME='Debian'
+	OSNAME='debian'
 elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
-	OSNAME='Ubuntu'
+	OSNAME='ubuntu'
 elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
-	OSNAME='Raspbian'
+	OSNAME='raspbian'
 elif grep -Eqi "Deepin" /etc/issue || grep -Eq "Deepin" /etc/*-release; then
-	OSNAME='Deepin'
+	OSNAME='deepin'
 else
 	OSNAME='unknow'
 fi
 
 echo "use system version: ${OSNAME}"
-
-if [ "$OSNAME" == 'MAC' ]; then
-	curl -fsSL  https://raw.githubusercontent.com/midoks/mdserver-web/master/scripts/install_macos.sh | sh
-elif [ "$OSNAME" == 'Ubuntu' ]; then
-	curl -fsSL  https://raw.githubusercontent.com/midoks/mdserver-web/master/scripts/install_ubuntu.sh | sh
-else
-	curl -fsSL  https://raw.githubusercontent.com/midoks/mdserver-web/master/scripts/install_centos.sh | sh
-fi 
-
+curl -fsSL  https://raw.githubusercontent.com/midoks/mdserver-web/master/scripts/install/${$OSNAME}.sh | sh
 
 
 endTime=`date +%s`
