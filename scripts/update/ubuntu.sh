1bfed830f2b911425141cf5b02ab3be722789bf0
midoks <midoks@163.com>
2022-06-10 21:48:51 +0800

    Update ubuntu.sh

--- scripts/update/ubuntu.sh
+++ scripts/update/ubuntu.sh
@@ -6,7 +6,8 @@ LANG=en_US.UTF-8
 
 
 wget -O /tmp/master.zip https://codeload.github.com/midoks/mdserver-web/zip/master
-cd /tmp && unzip /tmp/master.zip
+rm -rf /tmp/mdserver-web-master
+cd /tmp && unzip  /tmp/master.zip
 cp -rf /tmp/mdserver-web-master/* /www/server/mdserver-web
 rm -rf /tmp/master.zip
 rm -rf /tmp/mdserver-web-master
