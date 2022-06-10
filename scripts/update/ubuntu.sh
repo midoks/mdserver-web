462a6e3b0c85764bbe2523225a4ca8ddbf63ad97
midoks <midoks@163.com>
2022-06-10 16:37:06 +0800

    Create ubuntu.sh

---
+++ scripts/update/ubuntu.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+LANG=en_US.UTF-8
+
+
+
+cd /www/server/mdserver-web/scripts && ./lib.sh
+chmod 755 /www/server/mdserver-web/data
+
+
+if [ -f /www/server/mdserver-web/bin/activate ];then
+    cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate && pip3 install -r /www/server/mdserver-web/requirements.txt
+else
+    cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
+fi
+
+cd /www/server/mdserver-web && ./cli.sh start
+sleep 5
+
+cd /www/server/mdserver-web && ./cli.sh stop
+cd /www/server/mdserver-web && ./scripts/init.d/mw default
+cd /www/server/mdserver-web && ./cli.sh start
+
+
+
+
