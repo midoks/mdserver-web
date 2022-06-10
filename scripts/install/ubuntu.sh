264f41b0e2bd87043fc0a18035b8dd3047a6bdc3
midoks <midoks@163.com>
2022-06-10 16:11:35 +0800

    up

--- scripts/install/ubuntu.sh
+++ scripts/install/ubuntu.sh
@@ -3,4 +3,11 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+
+mkdir -p /www/server
+mkdir -p /www/wwwroot
+mkdir -p /www/wwwlogs
+mkdir -p /www/backup/database
+mkdir -p /www/backup/site
+
 echo "ubuntu dev ..."
