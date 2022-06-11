e9d0e471b268649342fb5f244e72c62750a7fa37
midoks <midoks@163.com>
2022-06-11 21:40:01 +0800

    Update icu.sh

--- plugins/php/lib/icu.sh
+++ plugins/php/lib/icu.sh
@@ -8,23 +8,22 @@ rootPath=$(dirname "$rootPath")
 rootPath=$(dirname "$rootPath")
 rootPath=$(dirname "$rootPath")
 
-echo $rootPath
+# echo $rootPath
 
+SERVER_ROOT=$rootPath/lib
+SOURCE_ROOT=$rootPath/source/lib
 
-SERVER_ROOT=/www/server/lib
-SOURCE_ROOT=/www/server/source/lib
+if [ ! -d ${SERVER_ROOT}/icu ];then
 
-if [ ! -d ${SERVER_ROOT}/icu/52.2 ];then
-
-	if [ ! -f ${SERVER_ROOT}/icu4c-52_2-src.tgz ];then
-		wget -O ${SERVER_ROOT}/icu4c-52_2-src.tgz https://github.com/unicode-org/icu/releases/download/release-52-2/icu4c-52_2-src.tgz
+	if [ ! -f ${SOURCE_ROOT}/icu4c-52_2-src.tgz ];then
+		wget -O ${SOURCE_ROOT}/icu4c-52_2-src.tgz https://github.com/unicode-org/icu/releases/download/release-52-2/icu4c-52_2-src.tgz
 	fi
 
 	if [ ! -d ${SERVER_ROOT}/icu/52.2 ];then
-		cd ${SERVER_ROOT} && tar -zxvf icu4c-52_2-src.tgz
+		cd ${SOURCE_ROOT} && tar -zxvf icu4c-52_2-src.tgz
 
-		cd $MDIR/source/cmd/icu/source
-		./runConfigureICU MacOSX  && make  CXXFLAGS="-g -O2 -std=c++11" && make install
+		cd ${SOURCE_ROOT}/icu/source
+		./runConfigureICU Linux --prefix=${SERVER_ROOT}/icu && make  CXXFLAGS="-g -O2 -std=c++11" && make install
 	fi	
 
 fi
