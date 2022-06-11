256ac747f69eb1d585a48632cca3fa49d3f64aee
midoks <midoks@163.com>
2022-06-11 16:23:07 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -277,6 +277,7 @@ elif [ "$OSNAME" == "ubuntu"  ] || [ "$OSNAME" == "debian" ]; then
     
 
     aptitude install -y libcurl4-gnutls-dev
+    apt-get install -y devscripts
 
     apt install -y php-common webp
 
