58a0d6b8f62b69f2f01c92739e4fb70e25e88711
midoks <midoks@163.com>
2022-06-13 11:39:26 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -300,7 +300,8 @@ elif [ "$OSNAME" == "ubuntu"  ] || [ "$OSNAME" == "debian" ]; then
     aptitude install -y libcurl4-gnutls-dev
     apt-get install -y devscripts
 
-    apt install -y php-common webp lzma
+    apt install -y php-common webp
+    apt install -y lzma lzma-dev
 
     apt install -y libpcre3 libpcre3-dev 
     apt install -y openssl libssl-dev
