b26e529610fec1117daf569a0e996d73320a0c6c
midoks <midoks@163.com>
2022-06-15 02:12:21 +0800

    up

--- plugins/php/versions/73/install.sh
+++ plugins/php/versions/73/install.sh
@@ -22,6 +22,8 @@ mkdir -p $serverPath/php
 
 cd $serverPath/mdserver-web/plugins/php/lib && /bin/bash freetype_old.sh
 cd $serverPath/mdserver-web/plugins/php/lib && /bin/bash libiconv.sh
+cd $serverPath/mdserver-web/plugins/php/lib && /bin/bash zlib.sh
+cd $serverPath/mdserver-web/plugins/php/lib && /bin/bash libzip.sh
 
 if [ ! -d $sourcePath/php/php${PHP_VER} ];then
 	
