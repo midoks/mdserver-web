49a3d5830c954757ee33ee979197f2b45dd77f2d
midoks <midoks@163.com>
2022-06-14 12:51:53 +0800

    up

--- scripts/update/debian.sh
+++ scripts/update/debian.sh
@@ -38,7 +38,7 @@ pip3 install gevent==21.1.2
 pip3 install gevent-websocket==0.10.1
 pip3 install requests==2.20.0
 pip3 install flask-caching>=1.10.1
-pip3 install python-socketio==5.2.0
+pip3 install flask-socketio==5.2.0
 pip3 install psutil==5.9.1 
 pip3 install pymongo
 
