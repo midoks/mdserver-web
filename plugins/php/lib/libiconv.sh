96f088f292d6535b8d673975a6a712e2d92f5e8d
midoks <midoks@163.com>
2022-06-15 02:23:59 +0800

    Update libiconv.sh

--- plugins/php/lib/libiconv.sh
+++ plugins/php/lib/libiconv.sh
@@ -18,7 +18,7 @@ if [ ! -d ${SERVER_ROOT}/libiconv ];then
 	wget -O libiconv-1.15.tar.gz  https://github.com/midoks/mdserver-web/releases/download/init/libiconv-1.15.tar.gz  -T 5
     tar zxvf libiconv-1.15.tar.gz
     cd libiconv-1.15
-    ./configure --prefix=${SERVER_ROOT}/libiconv --enable-static
+    ./configure --prefix=${SERVER_ROOT}/libiconv --enable-static && make && make install
     cd $SOURCE_ROOT
     #rm -rf libiconv-1.15
     #rm -rf libiconv-1.15.tar.gz
