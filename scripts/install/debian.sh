6b5b3f3b2f63ed7ad593a9b87dae22bf6d2b6def
midoks <midoks@163.com>
2022-06-14 11:56:47 +0800

    up

--- scripts/install/debian.sh
+++ scripts/install/debian.sh
@@ -95,6 +95,15 @@ else
     cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
 fi
 
+pip install --upgrade pip
+pip3 install gunicorn==20.1.0
+pip3 install gevent==21.1.2
+pip3 install gevent-websocket==0.10.1
+pip3 install requests==2.20.0
+pip3 install flask-caching>=1.10.1
+pip3 install python-socketio==4.2.0
+pip3 install psutil==5.9.1 
+pip3 install pymongo
 
 
 cd /www/server/mdserver-web && ./cli.sh start
