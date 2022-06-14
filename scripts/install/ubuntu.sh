df1ca9af4d2786a3ffe02ca84b1491ba4b69920c
midoks <midoks@163.com>
2022-06-14 21:56:58 +0800

    Update ubuntu.sh

--- scripts/install/ubuntu.sh
+++ scripts/install/ubuntu.sh
@@ -74,6 +74,8 @@ chmod 755 /www/server/mdserver-web/data
 
 cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
 
+pip install --upgrade pip
+pip install --upgrade setuptools
 pip3 install gunicorn==20.1.0
 pip3 install gevent==21.1.2
 pip3 install gevent-websocket==0.10.1
@@ -90,22 +92,11 @@ if [ ! -f /www/server/mdserver-web/bin/activate ];then
 fi
 
 if [ -f /www/server/mdserver-web/bin/activate ];then
-	pip install --upgrade pip
-    pip install --upgrade setuptools
     cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate && pip3 install -r /www/server/mdserver-web/requirements.txt
 else
     cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
 fi
 
-pip3 install gunicorn==20.1.0
-pip3 install gevent==21.1.2
-pip3 install gevent-websocket==0.10.1
-pip3 install requests==2.20.0
-pip3 install flask-caching==1.10.1
-pip3 install flask-socketio==5.2.0
-pip3 install flask-session==0.3.2
-pip3 install pymongo
-pip3 install psutil
 
 cd /www/server/mdserver-web && ./cli.sh start
 sleep 5
