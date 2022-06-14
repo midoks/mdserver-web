719378f2776dced0927f81c7a2fbea89565852c5
midoks <midoks@163.com>
2022-06-14 18:59:10 +0800

    up

--- scripts/update/ubuntu.sh
+++ scripts/update/ubuntu.sh
@@ -41,6 +41,7 @@ pip3 install gevent-websocket==0.10.1
 pip3 install requests==2.20.0
 pip3 install flask-caching==1.10.1
 pip3 install flask-socketio==5.2.0
+pip3 install flask-session==0.3.2
 pip3 install pymongo
 pip3 install psutil
 
