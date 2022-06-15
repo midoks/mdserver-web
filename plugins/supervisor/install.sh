ed8b2e0433179e2ddc5e7cde6bb0ff7d1b7da682
midoks <midoks@163.com>
2022-06-15 22:54:44 +0800

    supervisor dev

---
+++ plugins/supervisor/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+install_tmp=${rootPath}/tmp/mw_install.pl
+VERSION=$2
+
+
+sysName=`uname`
+echo "use system: ${sysName}"
+
+if [ ${sysName} == "Darwin" ]; then
+	OSNAME='macos'
+elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+	OSNAME='centos'
+elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+	OSNAME='fedora'
+elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+	OSNAME='debian'
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+	OSNAME='ubuntu'
+elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+	OSNAME='raspbian'
+else
+	OSNAME='unknow'
+fi
+
+Install_app()
+{
+	echo '正在安装脚本文件...' > $install_tmp
+	mkdir -p $serverPath/source
+	mkdir -p $serverPath/supervisor
+
+	echo 'supervisor install...'
+	if [ "centos" == "$OSNAME" ] || [ "fedora" == "$OSNAME" ];then
+    	yum install supervisor -y 
+    elif [ "ubuntu" == "$OSNAME" ] || [ "debian" == "$OSNAME" ] ;then
+    	apt install supervisor -y 
+	else
+    	brew install supervisor
+	fi
+
+	echo '安装完成' > $install_tmp
+}
+
+Uninstall_app()
+{
+	rm -rf $serverPath/supervisor
+	echo "Uninstall_redis" > $install_tmp
+}
+
+action=$1
+if [ "${1}" == 'install' ];then
+	Install_app
+else
+	Uninstall_app
+fi
---
+++ plugins/supervisor/js/supervisor.js
@@ -0,0 +1,29 @@
+
+
+function myPost(method,args,callback, title){
+
+    var _args = null; 
+    if (typeof(args) == 'string'){
+        _args = JSON.stringify(str2Obj(args));
+    } else {
+        _args = JSON.stringify(args);
+    }
+
+    var _title = '正在获取...';
+    if (typeof(title) != 'undefined'){
+        _title = title;
+    }
+
+    var loadT = layer.msg(_title, { icon: 16, time: 0, shade: 0.3 });
+    $.post('/plugins/run', {name:'supervisor', func:method, args:_args}, function(data) {
+        layer.close(loadT);
+        if (!data.status){
+            layer.msg(data.msg,{icon:0,time:2000,shade: [0.3, '#000']});
+            return;
+        }
+
+        if(typeof(callback) == 'function'){
+            callback(data);
+        }
+    },'json'); 
+}
