44396d92528c09ecdca4457c4975fe9acb899924
midoks <midoks@163.com>
2022-06-20 20:29:29 +0800

    up

--- scripts/install/alma.sh
+++ scripts/install/alma.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-
+LANG=C.UTF-8
 
 mkdir -p /www/server
 mkdir -p /www/wwwroot
@@ -107,9 +107,9 @@ cd /www/server/mdserver-web/scripts && bash lib.sh
 chmod 755 /www/server/mdserver-web/data
 
 
-if [ ! -f /www/server/mdserver-web/bin/activate ];then
-    cd /www/server/mdserver-web && python3 -m venv .
-fi
+# if [ ! -f /www/server/mdserver-web/bin/activate ];then
+#     cd /www/server/mdserver-web && python3 -m venv .
+# fi
 
 if [ -f /www/server/mdserver-web/bin/activate ];then
     cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate && pip3 install -r /www/server/mdserver-web/requirements.txt
@@ -123,7 +123,7 @@ pip3 install gevent==21.1.2
 pip3 install gevent-websocket==0.10.1
 pip3 install requests==2.20.0
 pip3 install flask-caching==1.10.1
-pip3 install python-socketio==4.2.0
+pip3 install flask-socketio==5.2.0
 pip3 install psutil==5.9.1 
 pip3 install pymongo
 
