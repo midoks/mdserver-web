f1e2baa145098f536ba591b87e3d06d3376094d5
midoks <midoks@163.com>
2022-06-21 00:14:18 +0800

    up

--- scripts/update_cn.sh
+++ scripts/update_cn.sh
@@ -22,16 +22,22 @@ if [ ${_os} == "Darwin" ]; then
 	OSNAME='macos'
 elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
 	OSNAME='centos'
+	yum install -y wget zip unzip
 elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
 	OSNAME='fedora'
+	yum install -y wget zip unzip
 elif grep -Eqi "Rocky" /etc/issue || grep -Eq "Rocky" /etc/*-release; then
 	OSNAME='rocky'
+	yum install -y wget zip unzip
 elif grep -Eqi "AlmaLinux" /etc/issue || grep -Eq "AlmaLinux" /etc/*-release; then
 	OSNAME='alma'
+	yum install -y wget zip unzip
 elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
 	OSNAME='debian'
+	apt install -y wget zip unzip
 elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
 	OSNAME='ubuntu'
+	apt install -y wget zip unzip
 elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
 	OSNAME='raspbian'
 else
