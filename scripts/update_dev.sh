81cdcd3fd024570146f58193993f9e76e24e84ff
midoks <midoks@163.com>
2022-06-22 01:10:16 +0800

    almalinux warning fix

--- scripts/update_dev.sh
+++ scripts/update_dev.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-LANG=en_US.UTF-8
+# LANG=en_US.UTF-8
 is64bit=`getconf LONG_BIT`
 
 startTime=`date +%s`
