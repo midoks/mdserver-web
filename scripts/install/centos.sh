c2324f6f4dcb12f860cf82f2b8b66928c84079ab
midoks <midoks@163.com>
2022-06-21 19:20:28 +0800

    Update centos.sh

--- scripts/install/centos.sh
+++ scripts/install/centos.sh
@@ -105,16 +105,19 @@ cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirem
 
 
 echo  "start install python env lib"
+
 if [ ! -f /www/server/mdserver-web/bin/activate ];then
 	cd /www/server/mdserver-web && python3 -m venv .
 	source /www/server/mdserver-web/bin/activate
-	pip3 install -r /www/server/mdserver-web/requirements.txt
 	pip install --upgrade pip
+	pip3 install -r /www/server/mdserver-web/requirements.txt
 	pip3 install gunicorn==20.1.0
 	pip3 install gevent==21.1.2
 	pip3 install gevent-websocket==0.10.1
 	pip3 install requests==2.20.0
 	pip3 install flask-caching==1.10.1
+	pip3 install flask-session==0.3.2
+	pip3 install flask-sqlalchemy==2.3.2
 	pip3 install python-socketio==4.2.0
 	pip3 install psutil==5.9.1
 	pip3 install pymongo
