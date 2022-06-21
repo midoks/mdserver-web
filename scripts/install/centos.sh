8e4cf4971e3e4890df5f7dd8a367c8cfde2d4cad
midoks <midoks@163.com>
2022-06-21 17:12:25 +0800

    Update centos.sh

--- scripts/install/centos.sh
+++ scripts/install/centos.sh
@@ -105,20 +105,13 @@ pip3 install pymongo
 cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
 
 if [ ! -f /www/server/mdserver-web/bin/activate ];then
-    cd /www/server/mdserver-web && python3 -m venv .
+	cd /www/server/mdserver-web && python3 -m venv .
+	sleep 5
 fi
 
 if [ -f /www/server/mdserver-web/bin/activate ];then
     cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate && pip3 install -r /www/server/mdserver-web/requirements.txt
     pip install --upgrade pip
-    pip3 install gunicorn==20.1.0
-	pip3 install gevent==21.1.2
-	pip3 install gevent-websocket==0.10.1
-	pip3 install requests==2.20.0
-	pip3 install flask-caching==1.10.1
-	pip3 install python-socketio==4.2.0
-	pip3 install psutil==5.9.1 
-	pip3 install pymongo
 fi
 
 echo  "end install python lib"
