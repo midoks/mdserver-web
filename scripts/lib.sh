24d08f8f83fb63ec47c8b6a18ad0de447b0ca754
midoks <midoks@163.com>
2022-06-22 18:19:31 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -303,8 +303,9 @@ elif [ "$OSNAME" == "ubuntu"  ] || [ "$OSNAME" == "debian" ]; then
     apt install -y libxml2 libxml2-dev libbz2-dev libmcrypt-dev libpspell-dev librecode-dev
     apt install -y libgmp-dev libgmp3-dev libreadline-dev libxpm-dev
     apt install -y zlib1g-dev dia pkg-config  
-    apt install -y libjpeg62-turbo-dev libjpeg-dev libpng-dev
+    apt install -y libjpeg-dev libpng-dev
     apt install -y libfreetype6
+    apt install -y libjpeg62-turbo-dev
     apt install -y libfreetype6-dev
     apt install -y libevent-dev libncurses5-dev libldap2-dev
 
