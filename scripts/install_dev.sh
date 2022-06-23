eb867556e769ef5945182e979ab2a990c72347ea
xcsoft <email@xcsoft.top>
2022-06-23 16:04:08 +0800

    Update install_dev.sh

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -73,9 +73,10 @@ if [ $OSNAME != "macos" ];then
 fi
 
 echo "use system version: ${OSNAME}"
-
 curl -fsSL https://raw.githubusercontent.com/midoks/mdserver-web/dev/scripts/install/${OSNAME}.sh | bash
 
+chmod +x /www/server/mdserver-web/mw-cli
+ln -s /www/server/mdserver-web/mw-cli /usr/bin/mw-cli
 
 endTime=`date +%s`
 ((outTime=(${endTime}-${startTime})/60))
