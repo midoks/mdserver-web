f112995f9a706e7da2e165b28a7cc71aeb7cd18f
midoks <midoks@163.com>
2022-06-24 21:05:53 +0800

    debug use

--- scripts/quick/debug.sh
+++ scripts/quick/debug.sh
@@ -24,6 +24,53 @@ if [ ! -d /www/server/php/71 ];then
 	cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 71
 fi
 
+
+
+# PHP_VER_LIST=(53 54 55 56 71 72 73 74 80 81)
+# # PHP_VER_LIST=(81)
+# for PHP_VER in ${PHP_VER_LIST[@]}; do
+# 	echo "php${PHP_VER} -- start"
+# 	cd $DIR/php$PHP_VER && sh install.sh
+# 	dir=$(ls -l $DIR/php$PHP_VER |awk '/^d/ {print $NF}')
+# 	for i in $dir
+# 	do
+# 		cd $DIR/php$PHP_VER/$i && sh install.sh $PHP_VER
+# 	done
+# 	echo "php${PHP_VER} -- end"
+# done
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 53
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 53
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 54
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 54
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 55
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 55
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 56
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 56
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 70
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 70
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 71
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 71
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 72
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 72
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 73
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 73
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 74
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 74
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 80
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 80
+
+cd /www/server/mdserver-web/plugins/php && bash install.sh install 81
+cd /www/server/mdserver-web && python3 /www/server/mdserver-web/plugins/php/index.py start 81
+
+
 # mysql
 if [ ! -d /www/server/mysql ];then
 	# cd /www/server/mdserver-web/plugins/mysql && bash install.sh install 5.7
