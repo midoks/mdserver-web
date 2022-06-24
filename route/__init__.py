b4c7fb91fffeed3f2b6a4eacefa90f2ce358adac
midoks <midoks@163.com>
2022-06-25 02:13:00 +0800

    Update __init__.py

--- route/__init__.py
+++ route/__init__.py
@@ -378,11 +378,11 @@ def connect_ssh():
     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
 
     try:
-        ssh.connect(mw.getHostAddr(), mw.getSSHPort())
-    except Exception as e:
-        ssh.connect('localhost', mw.getSSHPort())
+        ssh.connect(mw.getHostAddr(), mw.getSSHPort(), timeout=10)
     except Exception as e:
         ssh.connect('127.0.0.1', mw.getSSHPort())
+    except Exception as e:
+        ssh.connect('localhost', mw.getSSHPort())
     except Exception as e:
         return False
 
