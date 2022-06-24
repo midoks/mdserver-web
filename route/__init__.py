91308da32a5a01b8f9a2c99a9c07e761e1839aec
midoks <midoks@163.com>
2022-06-24 23:54:24 +0800

    关闭ssh启动尝试时连接

--- route/__init__.py
+++ route/__init__.py
@@ -455,7 +455,7 @@ if not mw.isAppleSystem():
         ssh = paramiko.SSHClient()
 
         # 启动尝试时连接
-        connect_ssh()
+        # connect_ssh()
     except Exception as e:
         print("本地终端无法使用")
 
