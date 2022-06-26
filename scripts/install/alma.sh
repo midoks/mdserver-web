605f6306fa673bd30ce19297efea9bdc0d3bbc9a
midoks <midoks@163.com>
2022-06-26 19:07:10 +0800

    合并相同部分

--- scripts/install/alma.sh
+++ scripts/install/alma.sh
@@ -78,44 +78,8 @@ dnf install -y ncurses-devel cmake
 dnf --enablerepo=crb install mysql-devel
 
 
-
 cd /www/server/mdserver-web/scripts && bash lib.sh
 chmod 755 /www/server/mdserver-web/data
-
-
-if [ ! -f /usr/local/bin/pip3 ];then
-    python3 -m pip install --upgrade pip setuptools wheel -i https://mirrors.aliyun.com/pypi/simple
-fi
-
-
-pip install --upgrade pip
-cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
-pip3 install gunicorn==20.1.0
-pip3 install gevent==21.1.2
-pip3 install gevent-websocket==0.10.1
-pip3 install requests==2.20.0
-pip3 install flask-caching==1.10.1
-pip3 install flask-socketio==5.2.0
-pip3 install psutil==5.9.1 
-pip3 install pymongo
-pip3 install mysqlclient
-
-
-if [ ! -f /www/server/mdserver-web/bin/activate ];then
-    cd /www/server/mdserver-web && python3 -m venv .
-    cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate
-    pip install --upgrade pip
-    pip3 install -r /www/server/mdserver-web/requirements.txt
-	pip3 install gunicorn==20.1.0
-	pip3 install gevent==21.1.2
-	pip3 install gevent-websocket==0.10.1
-	pip3 install requests==2.20.0
-	pip3 install flask-caching==1.10.1
-	pip3 install flask-socketio==5.2.0
-	pip3 install psutil==5.9.1 
-	pip3 install pymongo
-	pip3 install mysqlclient
-fi
 
 
 cd /www/server/mdserver-web && ./cli.sh start
