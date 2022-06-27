d7250ebde5d4e29d60e8144e1f375dcd9acaca39
midoks <midoks@163.com>
2022-06-27 17:06:09 +0800

    测试php53编译

--- plugins/php/versions/53/install.sh
+++ plugins/php/versions/53/install.sh
@@ -52,6 +52,10 @@ else
 fi
 
 
+# 加快测试速度 For Github Action
+MAKEJN='${SYS_MAKEJN:+"-j1"}'
+echo "SYS_MAKEJN:$MAKEJN"
+
 if [ ! -d $serverPath/php/53 ];then
 	cd $sourcePath/php/php${PHP_VER} && ./configure \
 	--prefix=$serverPath/php/53 \
@@ -85,7 +89,7 @@ if [ ! -d $serverPath/php/53 ];then
 	--disable-fileinfo \
 	$OPTIONS \
 	--enable-fpm
-	make clean && make && make install && make clean
+	make clean && make $MAKEJN && make install && make clean
 fi
 
 
