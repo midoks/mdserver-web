7cd65e6e6576e7bfd1de349577d5c0bb1b5878a3
midoks <midoks@163.com>
2022-06-30 13:57:16 +0800

    Update ioncube.sh

--- plugins/php/versions/55/ioncube.sh
+++ plugins/php/versions/55/ioncube.sh
@@ -58,7 +58,7 @@ Install_lib()
 
 	echo "" >> $serverPath/php/$version/etc/php.ini
 	echo "[${LIBNAME}]" >> $serverPath/php/$version/etc/php.ini
-	echo "extension=${LIBNAME}.so" >> $serverPath/php/$version/etc/php.ini
+	echo "zend_extension=${LIBNAME}.so" >> $serverPath/php/$version/etc/php.ini
 
 	$serverPath/php/init.d/php$version reload
 	echo '==========================================================='
