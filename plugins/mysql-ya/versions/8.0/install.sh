766bc018bc5c1000a0510c21ac784a15758aa18d
midoks <midoks@163.com>
2022-06-30 16:18:08 +0800

    Update install.sh

--- plugins/mysql-ya/versions/8.0/install.sh
+++ plugins/mysql-ya/versions/8.0/install.sh
@@ -17,11 +17,68 @@ sysName=`uname`
 install_tmp=${rootPath}/tmp/mw_install.pl
 mysqlDir=${serverPath}/source/mysql
 
+
+_os=`uname`
+if [ ${_os} == "Darwin" ]; then
+    OSNAME='macos'
+elif grep -Eqi "CentOS" /etc/issue || grep -Eq "CentOS" /etc/*-release; then
+    OSNAME='centos'
+elif grep -Eqi "Rocky" /etc/issue || grep -Eq "Rocky" /etc/*-release; then
+    OSNAME='rocky'
+elif grep -Eqi "Red Hat Enterprise Linux Server" /etc/issue || grep -Eq "Red Hat Enterprise Linux Server" /etc/*-release; then
+    OSNAME='rhel'
+elif grep -Eqi "Aliyun" /etc/issue || grep -Eq "Aliyun" /etc/*-release; then
+    OSNAME='aliyun'
+elif grep -Eqi "Fedora" /etc/issue || grep -Eq "Fedora" /etc/*-release; then
+    OSNAME='fedora'
+elif grep -Eqi "Amazon Linux AMI" /etc/issue || grep -Eq "Amazon Linux AMI" /etc/*-release; then
+    OSNAME='amazon'
+elif grep -Eqi "Debian" /etc/issue || grep -Eq "Debian" /etc/*-release; then
+    OSNAME='debian'
+elif grep -Eqi "Ubuntu" /etc/issue || grep -Eq "Ubuntu" /etc/*-release; then
+    OSNAME='ubuntu'
+elif grep -Eqi "Raspbian" /etc/issue || grep -Eq "Raspbian" /etc/*-release; then
+    OSNAME='raspbian'
+elif grep -Eqi "Deepin" /etc/issue || grep -Eq "Deepin" /etc/*-release; then
+    OSNAME='deepin'
+else
+    OSNAME='unknow'
+fi
+
+VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
+
+
+
+YUM_INSTALL()
+{
+#######
+
+if [ "${OSNAME}" == "centos" ];then
+	wget -O /tmp/mysql80-community-release.rpm http://repo.mysql.com/mysql80-community-release-el${VERSION}.rpm
+else
+	wget -O /tmp/mysql80-community-release.rpm http://repo.mysql.com/mysql80-community-release-el8.rpm
+fi
+rpm -ivh /tmp/mysql80-community-release.rpm
+yum -y install mysql-server
+#######
+}
+
+APT_INSTALL()
+{
+########
+wget -O /tmp/mysql-apt-config_0.8.22-1_all.deb https://repo.mysql.com/mysql-apt-config_0.8.22-1_all.deb
+dpkg -i /tmp/mysql-apt-config_0.8.22-1_all.deb
+
+apt update -y
+apt install -y mysql-server
+#######
+}
+
+
 Install_mysql()
 {
-	mkdir -p ${mysqlDir}
-	echo '正在安装脚本文件...' > $install_tmp
 
+	echo '正在安装脚本文件...' > $install_tmp
 
 	if id mysql &> /dev/null ;then 
 	    echo "mysql UID is `id -u www`"
@@ -31,66 +88,28 @@ Install_mysql()
 		useradd -g mysql mysql
 	fi
 
-	INSTALL_CMD=cmake
-	if [ "$sysName" != "Darwin" ];then
-		mkdir -p /var/log/mariadb
-		touch /var/log/mariadb/mariadb.log
-		INSTALL_CMD=cmake
-	fi 
-
-	if [ ! -f ${mysqlDir}/mysql-boost-8.0.25.tar.gz ];then
-		wget -O ${mysqlDir}/mysql-boost-8.0.25.tar.gz --tries=3 https://cdn.mysql.com/archives/mysql-8.0/mysql-boost-8.0.25.tar.gz
+	isYum=`which yum`
+	if [ "$isYum" != "" ];then
+		YUM_INSTALL
 	fi
 
-	#检测文件是否损坏.
-	md5_mysql_ok=e142c2058313b4646c36fa9bb1b38493
-	if [ -f ${mysqlDir}/mysql-boost-8.0.25.tar.gz ];then
-		md5_mysql=`md5sum ${mysqlDir}/mysql-boost-8.0.25.tar.gz  | awk '{print $1}'`
-		if [ "${md5_mysql_ok}" == "${md5_mysql}" ]; then
-			echo "mysql8.0 file  check ok"
-		else
-			# 重新下载
-			rm -rf ${mysqlDir}/mysql-8.0.25
-			wget -O ${mysqlDir}/mysql-boost-8.0.25.tar.gz --tries=3 https://cdn.mysql.com/archives/mysql-8.0/mysql-boost-8.0.25.tar.gz
-		fi
+	isApt=`which apt`
+	if [ "$isApt" != "" ];then
+		APT_INSTALL
 	fi
 
-	if [ ! -d ${mysqlDir}/mysql-8.0.25 ];then
-		 cd ${mysqlDir} && tar -zxvf  ${mysqlDir}/mysql-boost-8.0.25.tar.gz
-	fi
-
-
-	if [ ! -d $serverPath/mysql ];then
-		cd ${mysqlDir}/mysql-8.0.25 && ${INSTALL_CMD} \
-		-DCMAKE_INSTALL_PREFIX=$serverPath/mysql \
-		-DMYSQL_USER=mysql \
-		-DMYSQL_TCP_PORT=3306 \
-		-DMYSQL_UNIX_ADDR=/var/tmp/mysql.sock \
-		-DWITH_MYISAM_STORAGE_ENGINE=1 \
-		-DWITH_INNOBASE_STORAGE_ENGINE=1 \
-		-DWITH_MEMORY_STORAGE_ENGINE=1 \
-		-DENABLED_LOCAL_INFILE=1 \
-		-DWITH_PARTITION_STORAGE_ENGINE=1 \
-		-DEXTRA_CHARSETS=all \
-		-DDEFAULT_CHARSET=utf8mb4 \
-		-DDEFAULT_COLLATION=utf8mb4_general_ci \
-		-DDOWNLOAD_BOOST=1 \
-		-DFORCE_INSOURCE_BUILD=1 \
-		-DCMAKE_C_COMPILER=/usr/bin/gcc \
-		-DCMAKE_CXX_COMPILER=/usr/bin/g++ \
-		-DWITH_BOOST=${mysqlDir}/mysql-8.0.25/boost/
-		make ${MAKEJN:--j2} && make install && make clean
-
-		if [ -d $serverPath/mysql ];then
-			echo '8.0' > $serverPath/mysql/version.pl
-		fi
+	if [ "$?" == "0" ];then
+		mkdir -p $serverPath/mysql-ya
+		echo '8.0' > $serverPath/mysql-ya/version.pl
 		echo '安装完成' > $install_tmp
+	else
+		echo "暂时不支持该系统" > $install_tmp
 	fi
 }
 
 Uninstall_mysql()
 {
-	rm -rf $serverPath/mysql
+	rm -rf $serverPath/mysql-ya
 	echo '卸载完成' > $install_tmp
 }
 
