e03817853db5a85ec09c757f1b052fc9d16f03a2
midoks <midoks@163.com>
2022-07-01 01:33:05 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -351,7 +351,6 @@ pip install --upgrade pip
 pip3 install --upgrade setuptools
 cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
 
-pip3 install gunicorn==20.1.0
 pip3 install gevent-websocket==0.10.1
 pip3 install requests==2.20.0
 pip3 install flask-caching==1.10.1
@@ -376,7 +375,6 @@ pip install --upgrade pip
 pip3 install --upgrade setuptools
 pip3 install -r /www/server/mdserver-web/requirements.txt
 
-pip3 install gunicorn==20.1.0
 pip3 install gevent-websocket==0.10.1
 pip3 install requests==2.20.0
 pip3 install flask-caching==1.10.1
