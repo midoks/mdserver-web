80879ffb537284ba1fe29c1c77958e67b7111718
midoks <midoks@163.com>
2022-07-01 13:46:02 +0800

    更新openssl版本

--- plugins/php/lib/openssl.sh
+++ plugins/php/lib/openssl.sh
@@ -13,13 +13,25 @@ rootPath=$(dirname "$rootPath")
 SERVER_ROOT=$rootPath/lib
 SOURCE_ROOT=$rootPath/source/lib
 
+# if [ ! -d ${SERVER_ROOT}/openssl ];then
+#     cd ${SOURCE_ROOT}
+#     if [ ! -f ${SOURCE_ROOT}/openssl-1.0.2q.tar.gz ];then
+#         wget https://github.com/midoks/mdserver-web/releases/download/init/openssl-1.0.2q.tar.gz -T 20
+#     fi 
+#     tar -zxf openssl-1.0.2q.tar.gz
+#     cd openssl-1.0.2q
+#     ./config --openssldir=${SERVER_ROOT}/openssl zlib-dynamic shared
+#     make && make install
+# fi
+
+
 if [ ! -d ${SERVER_ROOT}/openssl ];then
     cd ${SOURCE_ROOT}
-    if [ ! -f ${SOURCE_ROOT}/openssl-1.0.2q.tar.gz ];then
-        wget https://github.com/midoks/mdserver-web/releases/download/init/openssl-1.0.2q.tar.gz -T 20
+    if [ ! -f ${SOURCE_ROOT}/openssl-1.1.1p.tar.gz ];then
+        wget -O ${SOURCE_ROOT}/openssl-1.1.1p.tar.gz https://www.openssl.org/source/openssl-1.1.1p.tar.gz
     fi 
-    tar -zxf openssl-1.0.2q.tar.gz
-    cd openssl-1.0.2q
+    tar -zxf openssl-1.1.1p.tar.gz
+    cd openssl-1.1.1p
     ./config --openssldir=${SERVER_ROOT}/openssl zlib-dynamic shared
     make && make install
 fi
