03af9e8c0bfc43854b3f17874e46edc624f19961
midoks <midoks@163.com>
2022-07-01 14:35:31 +0800

    Update openssl.sh

--- plugins/php/lib/openssl.sh
+++ plugins/php/lib/openssl.sh
@@ -30,9 +30,9 @@ if [ ! -d ${SERVER_ROOT}/openssl ];then
     if [ ! -f ${SOURCE_ROOT}/openssl-1.1.1p.tar.gz ];then
         wget -O ${SOURCE_ROOT}/openssl-1.1.1p.tar.gz https://www.openssl.org/source/openssl-1.1.1p.tar.gz
     fi 
-    tar -zxf openssl-1.1.1p.tar.gz
+    tar -zxvf openssl-1.1.1p.tar.gz
     cd openssl-1.1.1p
-    ./config --openssldir=${SERVER_ROOT}/openssl zlib-dynamic shared
+    ./config --prefix=${SERVER_ROOT}/openssl zlib-dynamic shared
     make && make install
 fi
 
