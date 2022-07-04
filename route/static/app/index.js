726ec1b406af5a4510a6ea3554889d4b727a81c9
midoks <midoks@163.com>
2022-07-04 11:41:40 +0800

    Update index.js

--- route/static/app/index.js
+++ route/static/app/index.js
@@ -140,7 +140,7 @@ function getDiskInfo() {
 
             //判断linode信息是否存在
             var inodes = '';
-            if (typeof (rdata[i]['inodes']!=='undefined'){
+            if ( typeof(rdata[i]['inodes']) !=='undefined' ){
                 inodes = '<div class="mask" style="color:' + LoadColor + '" data="Inode信息<br>总数：' + rdata[i].inodes[0] + '<br>已使用：' + rdata[i].inodes[1] + '<br>可用：' + rdata[i].inodes[2] + '<br>Inode使用率：' + rdata[i].inodes[3] + '"><span>' + rdata[i].size[3].replace('%', '') + '</span>%</div>';
             }
             dBody = '<li class="col-xs-6 col-sm-3 col-md-3 col-lg-2 mtb20 circle-box text-center diskbox">' +
