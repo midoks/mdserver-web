130cdd15a252147e58937676efceae8c64a5884c
midoks <midoks@163.com>
2022-07-04 22:49:04 +0800

    up

--- scripts/install/freebsd.sh
+++ scripts/install/freebsd.sh
@@ -3,6 +3,10 @@ PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 LANG=en_US.UTF-8
 
+if grep -Eq "FreeBSD" /etc/*-release && [ ! -f /bin/bash ]; then
+    ln -sf /usr/local/bin/bash /bin/bash
+fi
+
 pkg update -y
 pkg install -y python3
 python3 -m ensurepip
