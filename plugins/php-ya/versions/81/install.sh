b4c9eed7231914cb605cdbd09fe7f926ceb1dd59
midoks <midoks@163.com>
2022-07-07 14:40:43 +0800

    Update install.sh

--- plugins/php-ya/versions/81/install.sh
+++ plugins/php-ya/versions/81/install.sh
@@ -13,8 +13,8 @@ install_tmp=${rootPath}/tmp/mw_install.pl
 
 #获取信息和版本
 # bash /www/server/mdsever-web/scripts/getos.sh
-bash ${serverPath}/scripts/getos.sh
-OSNAME=`cat ${serverPath}/data/osname.pl`
+bash ${rootPath}/scripts/getos.sh
+OSNAME=`cat ${rootPath}/data/osname.pl`
 VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
 
 version=8.1.x
