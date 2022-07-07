b76c1f0ff5a604b09c2aa8abdbf5f812d9601b32
midoks <midoks@163.com>
2022-07-07 10:48:59 +0800

    Update index.py

--- plugins/mysql-ya/index.py
+++ plugins/mysql-ya/index.py
@@ -624,7 +624,7 @@ def setDbBackup():
     if not data[0]:
         return data[1]
 
-    scDir = getServerDir() + '/scripts/backup.py'
+    scDir = getPluginDir() + '/scripts/backup.py'
 
     cmd = 'python3 ' + scDir + ' database ' + args['name'] + ' 3'
     os.system(cmd)
