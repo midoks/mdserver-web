a2ee0ae330b88d5a61eb0dd22c4877c3aa5998c4
midoks <midoks@163.com>
2022-07-07 15:04:12 +0800

    Update install.sh

--- plugins/php-ya/versions/81/install.sh
+++ plugins/php-ya/versions/81/install.sh
@@ -23,7 +23,7 @@ PHP_VER=81
 Install_php_CentOS(){
 ### centos start ################
 rpm -Uvh http://rpms.remirepo.net/enterprise/remi-release-#{VERSION_ID}.rpm
-yum install php81
+yum install php81 php81-php-fpm 
 ### centos start ################
 }
 
