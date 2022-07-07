1baca326779c823328e22f3a4993b04444663134
midoks <midoks@163.com>
2022-07-07 18:39:50 +0800

    Update common.sh

--- plugins/php-yum/versions/common.sh
+++ plugins/php-yum/versions/common.sh
@@ -12,21 +12,33 @@ version=$1
 action=$2
 extName=$3
 
-echo $1,$2,$3
+# echo $1,$2,$3
 
-echo $curPath
-echo $rootPath
-echo $serverPath
+# echo $curPath
+# echo $rootPath
+# echo $serverPath
+
+FILE=${curPath}/${version}/${extName}.sh
 
 # yum install -y php81-php-yar
 if [ "$action" == 'install' ];then
-	yum install -y php$1-php-$2
+	
+	if [ -f $FILE ];then
+		bash ${curPath}/${version}/${extName}.sh install
+	else
+		yum install -y php${version}-php-${extName}
+	fi
 fi
 
 
 # yum remove -y php81-php-yar
 if [ "$action" == 'uninstall' ];then
-	yum remove -y php$1-php-$2
+
+	if [ -f $FILE ];then
+		bash ${curPath}/${version}/${extName}.sh uninstall
+	else
+		yum remove -y php${version}-php-${extName}
+	fi
 fi
 
 
