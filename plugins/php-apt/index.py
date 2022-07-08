69f03ece226ec2f4d1bc4da06d06addc71cad849
midoks <midoks@163.com>
2022-07-08 08:29:24 +0800

    Update index.py

--- plugins/php-apt/index.py
+++ plugins/php-apt/index.py
@@ -70,7 +70,7 @@ def getFpmConfFile(version):
 
 def status(version):
     # ps -ef|grep 'php/81' |grep -v grep | grep -v python | awk '{print $2}
-    cmd = "ps -ef|grep 'remi/php" + version + \
+    cmd = "ps -ef|grep 'php/" + version + \
         "' |grep -v grep | grep -v python | awk '{print $2}'"
     data = mw.execShell(cmd)
     if data[0] == '':
