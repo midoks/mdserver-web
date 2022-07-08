9e8f641e900e4631f677018caed06b23cd380d09
midoks <midoks@163.com>
2022-07-08 20:50:56 +0800

    Update debian.sh

--- scripts/install/debian.sh
+++ scripts/install/debian.sh
@@ -25,6 +25,10 @@ apt install -y python3-dev
 apt install -y python3-venv
 apt install -y cron
 
+apt install -y locate
+locale-gen en_US.UTF-8
+# localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+
 if [ ! -d /root/.acme.sh ];then	
 	curl  https://get.acme.sh | sh
 fi
