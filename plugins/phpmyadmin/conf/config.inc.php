7c2bb1b8bd9a2309c7a04d165d4646947225decb
midoks <midoks@163.com>
2022-07-09 13:16:54 +0800

    phpmyadmin 改为socket 连接数据库

--- plugins/phpmyadmin/conf/config.inc.php
+++ plugins/phpmyadmin/conf/config.inc.php
@@ -2,9 +2,11 @@
 $cfg['blowfish_secret'] = 'd41d8cd98f00b204e9800998ecf8427e';
 $i                      = 0;
 $i++;
-$cfg['Servers'][$i]['auth_type']       = 'cookie';
-$cfg['Servers'][$i]['host']            = '127.0.0.1';
-$cfg['Servers'][$i]['connect_type']    = 'tcp';
+$cfg['Servers'][$i]['auth_type'] = 'cookie';
+// $cfg['Servers'][$i]['host']            = '127.0.0.1';
+// $cfg['Servers'][$i]['connect_type']    = 'tcp';
+$cfg['Servers'][$i]['socket']          = '/www/server/mysql/mysql.sock';
+$cfg['Servers'][$i]['connect_type']    = 'socket';
 $cfg['Servers'][$i]['compress']        = false;
 $cfg['Servers'][$i]['AllowNoPassword'] = false;
 $cfg['TempDir']                        = '/www/server/phpmyadmin/tmp';
