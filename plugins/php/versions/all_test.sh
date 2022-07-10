725730c665499b5abc4cf7d1a014b79288edd280
midoks <midoks@163.com>
2022-07-10 16:51:34 +0800

    up

--- plugins/php/versions/all_test.sh
+++ plugins/php/versions/all_test.sh
@@ -2,10 +2,8 @@
 export PATH=$PATH:/opt/local/bin:/opt/local/sbin:/opt/local/share/man:/usr/local/bin:/usr/bin:/bin:/usr/sbin:/sbin:/opt/X11/bin
 DIR=$(cd "$(dirname "$0")"; pwd)
 
-
-
 # cd /www/server/mdserver-web/scripts/quick && bash debug.sh
-# cd /www/server/mdserver-web/plugins/php && bash all_test.sh
+# cd /www/server/mdserver-web/plugins/php/versions && /bin/bash all_test.sh
 
 
 # cd /www/server/mdserver-web/plugins/php && bash install.sh install 71
@@ -14,6 +12,7 @@ DIR=$(cd "$(dirname "$0")"; pwd)
 # cd /www/server/mdserver-web/plugins/php/versions/52/ && bash openssl.sh install 52
 # cd /www/server/mdserver-web/plugins/php/versions/81/ && bash imagemagick.sh install 81
 # cd /www/server/mdserver-web/plugins/php/versions/70/ && bash imagemagick.sh install 70
+
 
 # PHP_VER=52
 # echo "php${PHP_VER} -- start"
@@ -35,19 +34,30 @@ PHP_VER_LIST=(53 54 55 56 70 71 72 73 74 80 81)
 for PHP_VER in ${PHP_VER_LIST[@]}; do
 	echo "php${PHP_VER} -- start"
 	if [ -d  /www/server/php/${PHP_VER} ];then
-		cmd_ext=$(ls -l $DIR/versions/$PHP_VER/ |awk '{print $9}')
-		for ii in $cmd_ext
-		do
-			echo "${ii}"
-			if [ "install.sh" == "$ii" ];then
-				echo '' > /tmp/t.log
-			else
-				cd $DIR/versions/$PHP_VER/ && bash $ii install ${PHP_VER}
-			fi 
-		done
+		cd $DIR/$PHP_VER && bash install.sh install
 	fi
 	echo "php${PHP_VER} -- end"
 done
 
+cd $DIR
+PHP_VER_LIST=(53 54 55 56 70 71 72 73 74 80 81)
+PHP_EXT_LIST=(ioncube pdo mysqlnd sqlite3 odbc opcache mcrypt fileinfo \
+	exif gd intl memcache memcached redis imagick xdebug xhprof \
+	swoole yaf yar mongodb yac solr seaslog mbstring zip zstd)
 
-rm -rf /tmp/t.log
+for PHP_VER in ${PHP_VER_LIST[@]}; do
+	echo "php${PHP_VER} -- start"
+
+	NON_ZTS_FILENAME=`ls /www/server/php/${PHP_VER}/lib/php/extensions | grep no-debug-non-zts`
+	for EXT in ${PHP_EXT_LIST[@]}; do
+		extFile=/www/server/php/${PHP_VER}/lib/php/extensions/${NON_ZTS_FILENAME}/${EXT}.so
+		echo "${PHP_VER} ${EXT} start"
+		if [ ! -f $extFile ];then
+			bash common.sh  $version  install ${EXT}
+		fi
+		echo "${PHP_VER} ${EXT} end"
+	do
+	
+	echo "php${PHP_VER} -- end"
+done
+
