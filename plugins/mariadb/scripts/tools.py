2d6de15db1a60cae9829a918d7b20e78154be0ba
midoks <midoks@163.com>
2022-07-13 02:29:48 +0800

    Update tools.py

--- plugins/mariadb/scripts/tools.py
+++ plugins/mariadb/scripts/tools.py
@@ -50,70 +50,20 @@ echo '==========================================='
 echo "root密码成功修改为: ${pwd}"
 echo "The root password set ${pwd}  successuful"'''
 
-    server = mw.getServerDir() + '/mysql'
+    server = mw.getServerDir() + '/mariadb'
     root_mysql = root_mysql.replace('${server}', server)
     mw.writeFile('mysql_root.sh', root_mysql)
     os.system("/bin/bash mysql_root.sh " + password)
     os.system("rm -f mysql_root.sh")
 
-    pos = mw.getServerDir() + '/mysql'
+    pos = mw.getServerDir() + '/mariadb'
     result = sql.table('config').dbPos(pos, 'mysql').where(
         'id=?', (1,)).setField('mysql_root', password)
-
-
-def set_panel_pwd(password, ncli=False):
-    # 设置面板密码
-    import db
-    sql = db.Sql()
-    result = sql.table('users').where('id=?', (1,)).setField(
-        'password', mw.md5(password))
-    username = sql.table('users').where('id=?', (1,)).getField('username')
-    if ncli:
-        print("|-用户名: " + username)
-        print("|-新密码: " + password)
-    else:
-        print(username)
-
-
-def set_panel_username(username=None):
-    # 随机面板用户名
-    import db
-    sql = db.Sql()
-    if username:
-        if len(username) < 5:
-            print("|-错误，用户名长度不能少于5位")
-            return
-        if username in ['admin', 'root']:
-            print("|-错误，不能使用过于简单的用户名")
-            return
-
-        sql.table('users').where('id=?', (1,)).setField('username', username)
-        print("|-新用户名: %s" % username)
-        return
-
-    username = sql.table('users').where('id=?', (1,)).getField('username')
-    if username == 'admin':
-        username = mw.getRandomString(8).lower()
-        sql.table('users').where('id=?', (1,)).setField('username', username)
-    print('username: ' + username)
-
-
-def getServerIp():
-    version = sys.argv[2]
-    ip = mw.execShell(
-        "curl -{} -sS --connect-timeout 5 -m 60 https://v6r.ipip.net/?format=text".format(version))
-    print(ip[0])
 
 
 if __name__ == "__main__":
     type = sys.argv[1]
     if type == 'root':
         set_mysql_root(sys.argv[2])
-    elif type == 'panel':
-        set_panel_pwd(sys.argv[2])
-    elif type == 'username':
-        set_panel_username()
-    elif type == 'getServerIp':
-        getServerIp()
     else:
         print('ERROR: Parameter error')
