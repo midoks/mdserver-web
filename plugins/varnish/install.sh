c349684d1c435204f86e4ba4cfe63d478cf4c302
midoks <midoks@163.com>
2022-07-12 23:15:27 +0800

    Update install.sh

--- plugins/varnish/install.sh
+++ plugins/varnish/install.sh
@@ -36,7 +36,7 @@ Install_varnish()
 
 	if [ "${OSNAME}" == "macos" ]; then
 		brew install varnish
-	elif [ "${OSNAME}" == "centos" ]; then
+	elif [ "${OSNAME}" == "centos" ] || [ "${OSNAME}" == "fedora" ]; then
 		yum install varnish -y
 	elif [ "${OSNAME}" == "debian" ] || [ "${OSNAME}" == "ubuntu" ]; then
 		apt install varnish -y
