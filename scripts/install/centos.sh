1a8ea2dffe75dd51ba27948745f01a9ee1054e77
midoks <midoks@163.com>
2022-07-17 17:30:43 +0800

    Update centos.sh

--- scripts/install/centos.sh
+++ scripts/install/centos.sh
@@ -84,13 +84,13 @@ if [ "$?" != "0" ];then
 fi
 
 
-
 yum install -y libtirpc libtirpc-devel
 yum install -y rpcgen
 yum install -y openldap openldap-devel  
-yum install -y bison re2c cmake
+yum install -y bison re2c
 yum install -y cmake3
 yum install -y autoconf
+yum install -y make cmake gcc gcc-c++
 
 yum install -y libmemcached libmemcached-devel
 yum install -y curl curl-devel
@@ -118,7 +118,7 @@ yum install -y ncurses-devel
 yum install -y vixie-cron
 
 
-for yumPack in make cmake gcc gcc-c++ flex bison file libtool libtool-libs autoconf kernel-devel patch wget libjpeg libjpeg-devel libpng libpng-devel  zlib zlib-devel glib2 glib2-devel tar bzip2 bzip2-devel libevent libevent-devel ncurses ncurses-devel curl curl-devel libcurl libcurl-devel e2fsprogs e2fsprogs-devel libidn libidn-devel vim-minimal gettext gettext-devel ncurses-devel gmp-devel libcap diffutils ca-certificates net-tools psmisc libXpm-devel git-core c-ares-devel libicu-devel libxslt libxslt-devel zip unzip glibc.i686 libstdc++.so.6 cairo-devel ncurses-devel libaio-devel perl perl-devel perl-Data-Dumper expat-devel readline-devel;
+for yumPack in flex file libtool libtool-libs kernel-devel patch wget glib2 glib2-devel tar bzip2 bzip2-devel libevent libevent-devel ncurses ncurses-devel curl curl-devel libcurl libcurl-devel e2fsprogs e2fsprogs-devel libidn libidn-devel vim-minimal gettext gettext-devel ncurses-devel gmp-devel libcap diffutils ca-certificates net-tools psmisc libXpm-devel git-core c-ares-devel libicu-devel libxslt libxslt-devel zip unzip glibc.i686 libstdc++.so.6 cairo-devel ncurses-devel libaio-devel perl perl-devel perl-Data-Dumper expat-devel readline-devel;
 do yum -y install $yumPack;done
 
 
