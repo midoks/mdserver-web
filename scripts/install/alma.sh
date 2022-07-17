87207738058e45946300b4843d263e90d0c74ea9
midoks <midoks@163.com>
2022-07-17 10:35:22 +0800

    Update alma.sh

--- scripts/install/alma.sh
+++ scripts/install/alma.sh
@@ -78,6 +78,8 @@ dnf --enablerepo=crb install -y mysql-devel
 dnf --enablerepo=crb install -y oniguruma oniguruma-devel
 dnf --enablerepo=crb install -y rpcgen
 dnf --enablerepo=crb install -y libzip-devel
+dnf --enablerepo=crb install -y libmemcached-devel
+dnf --enablerepo=crb install -y libtirpc libtirpc-devel
 
 dnf install -y langpacks-zh_CN langpacks-en langpacks-en_GB
 
