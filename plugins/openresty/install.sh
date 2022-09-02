8e587cf055c19c1128cd0820fbb4dada13bb0043
midoks <midoks@163.com>
2022-09-02 23:16:41 +0800

    Update install.sh

--- plugins/openresty/install.sh
+++ plugins/openresty/install.sh
@@ -62,7 +62,8 @@ Install_openresty()
 	--with-http_v2_module \
 	--with-http_ssl_module  \
 	--with-http_slice_module \
-	--with-http_stub_status_module
+	--with-http_stub_status_module \
+	--with-http_realip_module
 
 	make -j${cpuCore} && make install && make clean
 
