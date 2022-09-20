07e017c9a73a876730c0af16eb0affc8fdf9afde
midoks <midoks@163.com>
2022-09-20 17:46:01 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -104,7 +104,8 @@ fi
 #面板需要的库
 
 if [ ! -f /usr/local/bin/pip3 ];then
-    python3 -m pip install --upgrade pip setuptools wheel -i https://mirrors.aliyun.com/pypi/simple
+    # python3 -m pip install --upgrade pip setuptools wheel -i https://mirrors.aliyun.com/pypi/simple
+    python3 -m pip install --upgrade pip setuptools wheel -i https://pypi.python.org/pypi
 fi
 
 pip install --upgrade pip
