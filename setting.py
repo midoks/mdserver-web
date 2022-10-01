5934be02bd2b2aed08bc0649fb59c42ec409b442
midoks <midoks@163.com>
2022-10-01 16:56:18 +0800

    Update setting.py

--- setting.py
+++ setting.py
@@ -24,8 +24,9 @@ cpu_info = system_api.system_api().getCpuInfo()
 workers = cpu_info[1]
 
 
-if not os.path.exists(os.getcwd() + '/logs'):
-    os.mkdir(os.getcwd() + '/logs')
+log_dir = os.getcwd() + '/logs'
+if not os.path.exists(log_dir):
+    os.mkdir(log_dir)
 
 # default port
 mw_port = "7200"
@@ -58,9 +59,9 @@ preload_app = True
 capture_output = True
 access_log_format = '%(t)s %(p)s %(h)s "%(r)s" %(s)s %(L)s %(b)s %(f)s" "%(a)s"'
 loglevel = 'info'
-errorlog = chdir + '/logs/error.log'
-accesslog = chdir + '/logs/access.log'
-pidfile = chdir + '/logs/mw.pid'
+errorlog = log_dir + '/error.log'
+accesslog = log_dir + '/access.log'
+pidfile = log_dir + '/mw.pid'
 if os.path.exists(os.getcwd() + '/data/ssl.pl'):
     certfile = 'ssl/certificate.pem'
     keyfile = 'ssl/privateKey.pem'
