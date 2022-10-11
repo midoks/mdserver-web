02e2c422252cb6c0ad9fb15c11b56ef30c0333a6
midoks <midoks@163.com>
2022-10-11 17:10:13 +0800

    up

--- plugins/op_waf/t/test.sh
+++ plugins/op_waf/t/test.sh
@@ -2,8 +2,11 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+# apt install apache2-utils
 # yum -y install httpd-tools
-# ab -c 1000 -n 1000000 http://xx.xx.xx/
+# ab -c 1000 -n 1000000 http://www.zzzvps.com/
+# /cc https://www.zzzvps.com/ 120
+
 python3 index.py
 
 
