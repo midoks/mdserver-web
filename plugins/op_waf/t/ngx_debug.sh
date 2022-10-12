01e388ea4576494ddb5edb5c962f71afb64a341d
midoks <midoks@163.com>
2022-10-13 03:12:44 +0800

    up

--- plugins/op_waf/t/ngx_debug.sh
+++ plugins/op_waf/t/ngx_debug.sh
@@ -14,6 +14,13 @@ pid=`ps -ef|grep openresty | grep -v grep | awk '{print $2}'`
 name=$2
 
 
+
+# /opt/openresty-systemtap-toolkit/ngx-active-reqs -p 496435
+
+# /opt/openresty-systemtap-toolkit/sample-bt -p 496435 -t 5 -k > a.bt
+
+
+
 if [ ! -d /opt/openresty-systemtap-toolkit ];then
     cd /opt && git clone https://github.com/openresty/openresty-systemtap-toolkit
 fi
@@ -28,11 +35,17 @@ if [ $1 == "lua" ]; then
     # /opt/openresty-systemtap-toolkit/fix-lua-bt temp.bt >t1.bt
     /opt/openresty-systemtap-toolkit/fix-lua-bt temp.bt >${name}.bt
 elif [ $1 == "c" ]; then
+    # /opt/openresty-systemtap-toolkit/sample-bt -p 496435 -t 10 -u > t2.bt
     /opt/openresty-systemtap-toolkit/sample-bt -p $pid -t 10 -u > ${name}.bt
 else
     echo "type is only lua/c"
     exit
 fi
+
+
+
+# /opt/FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
+# /opt/FlameGraph/flamegraph.pl perf.folded > perf.svg
 
 /opt/FlameGraph/stackcollapse-stap.pl ${name}.bt >${name}.cbt
 /opt/FlameGraph/flamegraph.pl ${name}.cbt >${name}.svg
---
+++ plugins/op_waf/t/ngx_demo.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+# cd /www/server/mdserver-web/plugins/op_waf/t && sh ngx_demo.sh
+
+# cd /www/wwwroot/dev156.cachecha.com && sh ngx_demo.sh
+
+pid=`ps -ef|grep openresty | grep -v grep | awk '{print $2}'`
+
+perf record -F 99 -p $pid -g -- sleep 60
+
+
+perf script -i perf.data &> perf.unfold
+/opt/FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
+/opt/FlameGraph/flamegraph.pl perf.folded > perf.svg
