8864e91f217a8339581e5f81b10b39bf383c5ee4
midoks <midoks@163.com>
2022-10-12 15:31:42 +0800

    Update install.sh

--- plugins/op_waf/install.sh
+++ plugins/op_waf/install.sh
@@ -7,16 +7,18 @@ rootPath=$(dirname "$curPath")
 rootPath=$(dirname "$rootPath")
 serverPath=$(dirname "$rootPath")
 
-
 install_tmp=${rootPath}/tmp/mw_install.pl
 
+
+action=$1
+version=$2
 
 Install_of(){
 	
 	echo '正在安装脚本文件...' > $install_tmp
 	mkdir -p $serverPath/op_waf
 
-	echo '0.1' > $serverPath/op_waf/version.pl
+	echo "${version}" > $serverPath/op_waf/version.pl
 	echo 'install ok' > $install_tmp
 
 	cd ${rootPath} && python3 ${rootPath}/plugins/op_waf/index.py start
@@ -28,9 +30,6 @@ Uninstall_of(){
 	rm -rf $serverPath/op_waf
 }
 
-
-action=$1
-type=$2
 
 action=$1
 if [ "${1}" == 'install' ];then
