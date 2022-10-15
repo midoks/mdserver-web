6e6a41e6df156f75ec2ff6c2c86dff977ede1492
midoks <midoks@163.com>
2022-10-15 10:03:22 +0800

    Update ngx_demo.sh

--- plugins/op_waf/t/ngx_demo.sh
+++ plugins/op_waf/t/ngx_demo.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
 
 # cd /www/server/mdserver-web/plugins/op_waf/t && sh ngx_demo.sh
-
 # cd /www/wwwroot/dev156.cachecha.com && sh ngx_demo.sh
 
-pid=`ps -ef|grep openresty | grep -v grep | awk '{print $2}'`
+
+# only openresty
+# pid=`ps -ef|grep openresty | grep -v grep | awk '{print $2}'`
+# perf record -F 99 -p $pid -g -- sleep 60
 
 
-# perf record -F 99 -p 45266 -g -- sleep 60 
-perf record -F 99 -p $pid -g -- sleep 60
+#全部
+perf record -F 99  -g  -a  -- sleep 60 
+
 
 perf script -i perf.data &> perf.unfold
 /opt/FlameGraph/stackcollapse-perf.pl perf.unfold &> perf.folded
