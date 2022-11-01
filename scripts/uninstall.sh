2ac903be9bb684f0af705f55e2d741a1a5105947
midoks <midoks@163.com>
2022-11-02 00:09:35 +0800

    Update uninstall.sh

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-# LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
 is64bit=`getconf LONG_BIT`
 
 if [ -f /etc/motd ];then
@@ -18,14 +18,37 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+UNINSTALL_MySQL()
+{
+    MYSQLD_CHECK=$(ps -ef |grep mysqld|grep -v grep|grep /www/server/mysql)
+    if [ "$MYSQLD_CHECK" != "" ];then
+        echo -e "----------------------------------------------------"
+        echo -e "检查已有MySQL环境，卸载可能影响现有站点及数据"
+        echo -e "----------------------------------------------------"
+        echo -e "已知风险/输入yes强制卸载!"
+        read -p "输入yes强制卸载: " yes;
+        if [ "$yes" != "yes" ];then
+            echo -e "------------"
+            echo "取消卸载MySQL"
+        else
+            cd /www/server/mdserver-web/plugins/mysql && sh install.sh uninstall 8.0
+        fi
+    fi
+}
 
-rm -rf /usr/bin/mw
-rm -rf /www/server/mdserver-web
+UNINSTALL_MW()
+{
+    rm -rf /usr/bin/mw
+    rm -rf /etc/init.d/mw
+    systemctl daemon-reload
 
+    rm -rf /www/server/mdserver-web
+}
+
+
+UNINSTALL_MySQL
+UNINSTALL_MW
 
 endTime=`date +%s`
 ((outTime=(${endTime}-${startTime})/60))
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
-
-
-systemctl daemon-reload
