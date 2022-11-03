e2962146cf0bed3651905c6303efd29fd1729691
midoks <midoks@163.com>
2022-11-04 00:25:47 +0800

    调整debian和ubuntu的安装

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -37,65 +37,10 @@ elif [ "$OSNAME" == "fedora" ];then
     echo "fedora lib"
 elif [ "$OSNAME" == "alma" ];then
     echo "alma lib"
-elif [ "$OSNAME" == "ubuntu"  ] || [ "$OSNAME" == "debian" ]; then
-    
-    apt install -y devscripts
-    apt install -y net-tools
-    apt install -y python3-dev
-    apt install -y autoconf
-    apt install -y gcc
-
-    apt install -y libffi-dev
-    apt install -y cmake automake make
-
-    apt install -y webp scons
-    apt install -y libwebp-dev
-    apt install -y lzma lzma-dev
-    apt install -y libunwind-dev
-
-    apt install -y libpcre3 libpcre3-dev 
-    apt install -y openssl
-    apt install -y libssl-dev
-    
-    apt install -y libmemcached-dev
-    apt install -y libsasl2-dev
-    apt install -y imagemagick 
-    apt install -y libmagickwand-dev
-
-    apt install -y libxml2 libxml2-dev libbz2-dev libmcrypt-dev libpspell-dev librecode-dev
-    apt install -y libgmp-dev libgmp3-dev libreadline-dev libxpm-dev
-    apt install -y dia pkg-config
-    apt install -y zlib1g-dev
-    apt install -y libjpeg-dev libpng-dev
-    apt install -y libfreetype6
-    apt install -y libjpeg62-turbo-dev
-    apt install -y libfreetype6-dev
-    apt install -y libevent-dev libncurses5-dev libldap2-dev
-    apt install -y libzip-dev
-    apt install -y libicu-dev
-
-    apt install -y build-essential
-    
-    apt install -y libcurl4-openssl-dev
-    apt install -y curl libcurl4-gnutls-dev
-    #https://blog.csdn.net/qq_36228377/article/details/123154344
-    # ln -s  /usr/include/x86_64-linux-gnu/curl  /usr/include/curl
-    if [ ! -d /usr/include/curl ];then
-        ln -s  /usr/include/x86_64-linux-gnu/curl  /usr/include/curl
-    fi
-   
-
-    apt install -y graphviz bison re2c flex
-    apt install -y libsqlite3-dev
-    apt install -y libonig-dev
-
-    apt install -y perl g++ libtool    
-    apt install -y libxslt1-dev
-
-    apt install -y libmariadb-dev
-    #apt install -y libmysqlclient-dev   
-    apt install -y libmariadb-dev-compat
-    #apt install -y libmariadbclient-dev
+elif [ "$OSNAME" == "ubuntu" ];then
+    echo "ubuntu lib"
+elif [ "$OSNAME" == "debian" ]; then
+    echo "debian lib"
 else
     echo "OK"
 fi
