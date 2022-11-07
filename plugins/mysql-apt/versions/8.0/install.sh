502e5a18774226bf650bfddfacdab07f50013ae3
midoks <midoks@163.com>
2022-11-07 13:31:27 +0800

    up

--- plugins/mysql-apt/versions/8.0/install.sh
+++ plugins/mysql-apt/versions/8.0/install.sh
@@ -56,6 +56,8 @@ dpkg -i mysql-community-server-core_${SUFFIX_NAME}.deb
 # dpkg -i mysql-community-server_${SUFFIX_NAME}.deb
 dpkg -i mysql-server_${SUFFIX_NAME}.deb
 
+
+rm -rf $myDir
 #######
 }
 
