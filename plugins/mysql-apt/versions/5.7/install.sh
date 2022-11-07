2c9d9dd19c87388120968e3e91f797a4ecb1ccdc
midoks <midoks@163.com>
2022-11-07 13:28:03 +0800

    Update install.sh

--- plugins/mysql-apt/versions/5.7/install.sh
+++ plugins/mysql-apt/versions/5.7/install.sh
@@ -13,25 +13,51 @@ rootPath=$(dirname "$rootPath")
 serverPath=$(dirname "$rootPath")
 sysName=`uname`
 
-
 install_tmp=${rootPath}/tmp/mw_install.pl
-mysqlDir=${serverPath}/source/mysql
-
+myDir=${serverPath}/source/mysql-apt
 
 bash ${rootPath}/scripts/getos.sh
 OSNAME=`cat ${rootPath}/data/osname.pl`
 VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
 
+# cd /www/server/mdserver-web/plugins/mysql-apt && bash install.sh install 8.0
+
+MYSQL_VER=5.7.39
+SUFFIX_NAME=${MYSQL_VER}-1${OSNAME}${VERSION_ID}_amd64
+
+
+# /lib/systemd/system/mysql.service
+# /etc/mysql/my.cnf
+
 APT_INSTALL()
 {
 ########
-wget -O /tmp/mysql-apt-config_0.8.22-1_all.deb https://repo.mysql.com/mysql-apt-config_0.8.22-1_all.deb
-dpkg -i /tmp/mysql-apt-config_0.8.22-1_all.deb
+mkdir -p $myDir
+
+wget -O ${myDir}/mysql-server_${SUFFIX_NAME}.deb-bundle.tar https://cdn.mysql.com/archives/mysql-5.7/mysql-server_${SUFFIX_NAME}.deb-bundle.tar
+chmod +x ${myDir}/mysql-server_${SUFFIX_NAME}.deb-bundle.tar
+cd ${myDir} && tar vxf /tmp/mysql-server_${SUFFIX_NAME}.deb-bundle.tar
 
 apt update -y
-apt install -y mysql-server
+apt install -y libnuma1 libaio1 libmecab2
 
-rm -rf  /tmp/mysql-apt-config_0.8.22-1_all.deb
+# 安装
+dpkg -i mysql-common_${SUFFIX_NAME}.deb
+
+
+export DEBIAN_FRONTEND=noninteractive
+dpkg -i mysql-community-client-plugins_${SUFFIX_NAME}.deb
+dpkg -i mysql-community-client-core_${SUFFIX_NAME}.deb
+dpkg -i mysql-community-client_${SUFFIX_NAME}.deb
+dpkg -i mysql-client_${SUFFIX_NAME}.deb
+
+dpkg -i mysql-community-server-core_${SUFFIX_NAME}.deb
+
+# dpkg -X mysql-community-server_5.-1debian11_amd64.deb ./tmp
+#会覆盖/lib/systemd/system/mysql.service,不安装
+# dpkg -i mysql-community-server_${SUFFIX_NAME}.deb
+dpkg -i mysql-server_${SUFFIX_NAME}.deb
+
 #######
 }
 
@@ -45,9 +71,7 @@ apt remove -y mysql-server
 
 Install_mysql()
 {
-
 	echo '正在安装脚本文件...' > $install_tmp
-
 	if id mysql &> /dev/null ;then 
 	    echo "mysql uid is `id -u mysql`"
 	    echo "mysql shell is `grep "^mysql:" /etc/passwd |cut -d':' -f7 `"
@@ -55,6 +79,7 @@ Install_mysql()
 	    groupadd mysql
 		useradd -g mysql mysql
 	fi
+
 
 	isApt=`which apt`
 	if [ "$isApt" != "" ];then
