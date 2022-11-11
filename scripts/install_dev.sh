f6178c3ac2bff13c401f1ad6698982761b25707a
midoks <midoks@163.com>
2022-11-11 09:32:19 +0800

    Update install_dev.sh

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -99,9 +99,8 @@ if [ ! -e /usr/bin/mw ]; then
 	fi
 fi
 
+systemctl daemon-reload
+
 endTime=`date +%s`
 ((outTime=(${endTime}-${startTime})/60))
 echo -e "Time consumed:\033[32m $outTime \033[0mMinute!"
-
-systemctl daemon-reload
-
