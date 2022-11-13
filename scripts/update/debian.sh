ef13161bc7b7434026021320308130131d3926a7
midoks <midoks@163.com>
2022-11-13 16:03:16 +0800

    Update debian.sh

--- scripts/update/debian.sh
+++ scripts/update/debian.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
-export LANG=en_US.UTF-8
 export DEBIAN_FRONTEND=noninteractive
 
-localedef -v -c -i en_US -f UTF-8 en_US.UTF-8
+apt install -y locate
+locale-gen en_US.UTF-8
+export LC_CTYPE=en_US.UTF-8
+export LC_ALL=en_US.UTF-8
+export LANG=en_US.UTF-8
+
 
 if grep -Eq "Ubuntu" /etc/*-release; then
     sudo ln -sf /bin/bash /bin/sh
