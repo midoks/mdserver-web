d6856498f8a95703e31fefe4b3d0d1921827a4fc
midoks <midoks@163.com>
2022-11-19 22:05:45 +0800

    Update install_dev.sh

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -94,7 +94,7 @@ cd /www/server/mdserver-web && bash /etc/rc.d/init.d/mw default
 
 sleep 2
 if [ ! -e /usr/bin/mw ]; then
-	if [ -f /etc/init.d/mw ];then
+	if [ -f /etc/rc.d/init.d/mw ];then
 		ln -s /etc/rc.d/init.d/mw /usr/bin/mw
 	fi
 fi
