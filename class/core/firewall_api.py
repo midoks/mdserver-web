11a4d3acf8a6e16cfb4fb0e803af195c8de21520
midoks <midoks@163.com>
2022-11-20 20:02:39 +0800

    Update firewall_api.py

--- class/core/firewall_api.py
+++ class/core/firewall_api.py
@@ -211,12 +211,17 @@ class firewall_api:
         if ssh_status[0] != '':
             status = False
 
+        data['pass_prohibit_status'] = False
         # 密码登陆配置检查
         pass_rep = "#PasswordAuthentication\s+(\w*)\s*\n"
-        data['pass_prohibit_status'] = False
         pass_status = re.search(pass_rep, conf)
         if pass_status:
-            if pass_status.groups(0)[0].strip() == 'no':
+            data['pass_prohibit_status'] = True
+
+        if not data['pass_prohibit_status']:
+            pass_rep = "PasswordAuthentication\s+(\w*)\s*\n"
+            pass_status = re.search(pass_rep, conf)
+            if pass_status and pass_status.groups(0)[0].strip() == 'no':
                 data['pass_prohibit_status'] = True
 
         data['port'] = port
