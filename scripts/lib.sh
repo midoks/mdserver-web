a758fc226481d7a777af7f77abe888a861ecf30e
hiCasper <hiCasper@users.noreply.github.com>
2022-12-10 01:51:07 +0800

    Fix pip source

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -50,20 +50,19 @@ fi
 #面板需要的库
 if [ ! -f /usr/local/bin/pip3 ];then
     cn=$(curl -fsSL -m 10 http://ipinfo.io/json | grep "\"country\": \"CN\"")
+    PIPSRC="https://pypi.python.org/simple"
     if [ ! -z "$cn" ];then
-        python3 -m pip install --upgrade pip setuptools wheel -i https://mirrors.aliyun.com/pypi/simple
-    else
-        python3 -m pip install --upgrade pip setuptools wheel -i https://pypi.python.org/pypi
+        PIPSRC="https://mirrors.aliyun.com/pypi/simple"
     fi
+    python3 -m pip install --upgrade pip setuptools wheel -i $PIPSRC
 fi
 
-which pip && pip install --upgrade pip
-pip3 install --upgrade setuptools
-cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
+which pip && pip install --upgrade pip -i $PIPSRC
+pip3 install --upgrade setuptools -i $PIPSRC
+cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt -i $PIPSRC
 
 # pip3 install flask-caching==1.10.1
 # pip3 install mysqlclient
-
 
 if [ ! -f /www/server/mdserver-web/bin/activate ];then
     cd /www/server/mdserver-web && python3 -m venv .
@@ -72,9 +71,9 @@ else
     cd /www/server/mdserver-web && source /www/server/mdserver-web/bin/activate
 fi
 
-pip install --upgrade pip
-pip3 install --upgrade setuptools
-cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt
+pip install --upgrade pip -i $PIPSRC
+pip3 install --upgrade setuptools -i $PIPSRC
+cd /www/server/mdserver-web && pip3 install -r /www/server/mdserver-web/requirements.txt -i $PIPSRC
 
 echo "lib ok!"
 # pip3 install flask-caching==1.10.1
