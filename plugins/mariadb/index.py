a1550765aaecdd4ac0c9ad5b078624bf538184e7
midoks <midoks@163.com>
2022-12-31 14:52:39 +0800

    up

--- plugins/mariadb/index.py
+++ plugins/mariadb/index.py
@@ -1792,7 +1792,7 @@ def addMasterRepSlaveUser(version=''):
     # address = args['address'].strip()
     # dataAccess = args['dataAccess'].strip()
 
-    reg = "^[\w\.-]+$"
+    reg = "^[\w-]+$"
     if not re.match(reg, username):
         return mw.returnJson(False, '用户名不能带有特殊符号!')
     checks = ['root', 'mysql', 'test', 'sys', 'panel_logs']
