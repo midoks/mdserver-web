4afe2061e2f526fabd3cad78617df080bb2ad267
midoks <midoks@163.com>
2022-12-31 13:32:30 +0800

    Update mysql.js

--- plugins/mysql/js/mysql.js
+++ plugins/mysql/js/mysql.js
@@ -1920,7 +1920,7 @@ function addSlaveSyncUser(ip=''){
                 <div class='line'><span class='tname'>同步账户</span><div class='info-r'><input name='user' class='bt-input-text mr5' type='text' style='width:330px;' value='"+user+"'></div></div>\
                 <div class='line'><span class='tname'>同步密码</span><div class='info-r'><input name='pass' class='bt-input-text mr5' type='text' style='width:330px;' value='"+pass+"'></div></div>\
                 <div class='line'>\
-                <span class='tname'>CMD[可选]</span>\
+                <span class='tname'>CMD[最好填好]</span>\
                 <div class='info-r'><textarea class='bt-input-text mr5' row='20' cols='30' name='cmd' style='width:330px;height:150px;'></textarea></div>\
                 </div>\
                 <input type='hidden' name='mode' value='"+mode+"' />\
