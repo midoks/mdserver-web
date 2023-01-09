73b4be90f536b4fd7cf3f5e1cb51a3c548504b84
midoks <midoks@163.com>
2023-01-09 14:33:15 +0800

    Update install.sh

--- plugins/php-yum/install.sh
+++ plugins/php-yum/install.sh
@@ -82,6 +82,8 @@ if [ "${action}" == "install" ] && [ -d ${serverPath}/php-yum/${type} ];then
 	cd ${rootPath}/plugins/php-yum/versions && bash common.sh ${type} install mbstring
 	echo "install PHP-YUM[${type}] extend end"
 
+	cd ${rootPath} && python3 ${rootPath}/plugins/php-yum/index.py restart ${type}
+
 	if [ ! -f /usr/local/bin/composer ];then
 		cd /tmp
 		curl -sS https://getcomposer.org/installer | php
