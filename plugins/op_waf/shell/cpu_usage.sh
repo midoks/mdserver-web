f491641ab809dde56c60052bebc737bdde0f7995
midoks <midoks@163.com>
2023-02-05 12:25:57 +0800

    Create cpu_usage.sh

---
+++ plugins/op_waf/shell/cpu_usage.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+DST_DIR=/www/server/op_waf
+DIS_FILE=${DST_DIR}/cpu.info
+
+CPU_USAGE=`top -bn 1 | fgrep 'Cpu(s)' | awk '{print 100 -$8}' | awk -F . '{print $1}'`
+echo $CPU_USAGE > $DIS_FILE
