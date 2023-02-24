ec51ad016afbf1be1df901df6486f365619a1367
midoks <midoks@163.com>
2023-02-24 16:57:24 +0800

    Update install_dev.sh

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -99,7 +99,7 @@ if [ $OSNAME != "macos" ];then
 	if [ ! -d /root/.acme.sh ];then
 	    if [ "$LOCAL_ADDR" != "common" ];then
 	        # curl -sSL -o /tmp/acme.tar.gz ${HTTP_PREFIX}github.com/acmesh-official/acme.sh/archive/master.tar.gz
-	        curl -sSL -o /tmp/acme.tar.gz https://gitee.com/neilpang/acme.sh/repository/archive/master.zip
+	        curl -sSLo /tmp/acme.tar.gz https://gitee.com/neilpang/acme.sh/repository/archive/master.zip
 
 	        tar xvzf /tmp/acme.tar.gz -C /tmp
 	        cd /tmp/acme.sh-master
