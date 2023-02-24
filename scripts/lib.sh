006a62be22ad91cc4e48673afc298f7dc7d1b3e8
midoks <midoks@163.com>
2023-02-24 17:25:04 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -50,7 +50,7 @@ fi
 # cn=$(curl -fsSL -m 10 http://ipinfo.io/json | grep "\"country\": \"CN\"")
 HTTP_PREFIX="https://"
 LOCAL_ADDR=common
-ping  -c 1 pypi.python.org > /dev/null 2>&1
+ping  -c 1 github.com > /dev/null 2>&1
 if [ "$?" != "0" ];then
     LOCAL_ADDR=cn
     HTTP_PREFIX="https://ghproxy.com/"
