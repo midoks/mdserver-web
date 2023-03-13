3a6cb0ae2a7ca2160ad2e0e6162bddb126347059
midoks <midoks@163.com>
2023-03-14 01:15:14 +0800

    Update receive_faq.py

--- plugins/tgbot/startup/extend/receive_faq.py
+++ plugins/tgbot/startup/extend/receive_faq.py
@@ -25,20 +25,28 @@ def isThisCmd(cmd, msg):
     return False
 
 
-def searchFaq(bot, message):
+def getReadCmd(cmd, msg):
+    clen = len(cmd)
+    msg_len = len(msg)
+    real_msg = msg[clen:]
+    return real_msg
+
+
+def searchFaq(bot, message, cmd_text):
     # print(search_text)
-    bot.reply_to(message, message.chat.id)
+    bot.reply_to(message, cmd_text)
     return True
 
 
 def run(bot, message):
     text_body = message.text
-    for cmd in cmd_list:
-        if isThisCmd(cmd, text_body):
-            return searchFaq(bot, message)
+    if isThisCmd('/?:', text_body):
+        cmd_text = getReadCmd('/?:', text_body)
+        return searchFaq(bot, message, cmd_text)
 
     return bot
 
 
 if __name__ == "__main__":
     print(isThisCmd('/?:', '/?:如何在安装面板'))
+    print(getReadCmd('/?:', '/?:如何在安装面板'))
