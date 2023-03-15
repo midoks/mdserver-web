18312a35968af66ff01fa6d9b5e86bd2886401e7
midoks <midoks@163.com>
2023-03-15 17:37:35 +0800

    Update tgbot.py

--- plugins/tgbot/startup/tgbot.py
+++ plugins/tgbot/startup/tgbot.py
@@ -90,6 +90,7 @@ bot.delete_my_commands(scope=None, language_code=None)
 bot.set_my_commands(
     commands=[
         telebot.types.BotCommand("start", "查看帮助信息"),
+        telebot.types.BotCommand("faq:", "查看bbs帖子主题【超找时,不要忘记:冒号】"),
     ],
 )
 
