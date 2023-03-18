34285f605127f62023ed7f28de0e8caa430ce835
midoks <midoks@163.com>
2023-03-19 00:20:23 +0800

    Update push_tmp_msg.py

--- plugins/tgbot/startup/extend/push_tmp_msg.py
+++ plugins/tgbot/startup/extend/push_tmp_msg.py
@@ -17,6 +17,84 @@ from telebot import types
 from telebot.util import quick_markup
 
 # 轮播实例
+# coding:utf-8
+
+import sys
+import io
+import os
+import time
+import re
+import json
+import base64
+import threading
+
+sys.path.append(os.getcwd() + "/class/core")
+import mw
+
+import telebot
+from telebot import types
+from telebot.util import quick_markup
+
+# 广告推送实例
+
+
+chat_id = -1001578009023
+# chat_id = 5568699210
+
+
+def send_msg(bot, tag='ad', trigger_time=300):
+    # 信号只在一个周期内执行一次|start
+    lock_file = mw.getServerDir() + '/tgbot/lock.json'
+    if not os.path.exists(lock_file):
+        mw.writeFile(lock_file, '{}')
+
+    lock_data = json.loads(mw.readFile(lock_file))
+    if tag in lock_data:
+        diff_time = time.time() - lock_data[tag]['do_time']
+        if diff_time >= trigger_time:
+            lock_data[tag]['do_time'] = time.time()
+        else:
+            return False, 0, 0
+    else:
+        lock_data[tag] = {'do_time': time.time()}
+    mw.writeFile(lock_file, json.dumps(lock_data))
+    # 信号只在一个周期内执行一次|end
+
+    # https://t.me/gjgzs2022 | 每月19
+    keyboard = [
+        [
+            types.InlineKeyboardButton(
+                text="赞助联系ME", url='tg://user?id=5568699210')
+        ],
+        [
+            types.InlineKeyboardButton(
+                text="🙎‍♂️代实名🙍‍♀️过人脸🅾️国际阿里云腾讯云(赞助商)", url='https://t.me/gjgzs2022')
+        ],
+        [
+            types.InlineKeyboardButton(
+                text="💎DigitalVirt(赞助商)", url='https://digitalvirt.com/aff.php?aff=154')
+        ],
+        [
+            types.InlineKeyboardButton(
+                text="论坛", url='https://bbs.midoks.me'),
+            types.InlineKeyboardButton(
+                text="搜索", url='https://bbs.midoks.me/search.php')
+        ]
+    ]
+    markup = types.InlineKeyboardMarkup(keyboard)
+    image_file = mw.getPluginDir() + '/tgbot/static/image/ad.png'
+
+    telebot_image = telebot.types.InputFile(image_file)
+    msg = bot.send_photo(chat_id, telebot_image, reply_markup=markup)
+
+    # print(msg.message_id)
+    time.sleep(5 * 60)
+    del_msg = bot.delete_message(chat_id=chat_id, message_id=msg.message_id)
+    # print(del_msg)
+
+
+def run(bot):
+    send_msg(bot, 'ad', 1 * 60 * 60)
 
 chat_id = -1001578009023
 # chat_id = 5568699210
