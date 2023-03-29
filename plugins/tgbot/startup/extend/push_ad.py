aed8319dd3f00f9048a3fc72693dc2cbfab9ffeb
midoks <midoks@163.com>
2023-03-29 14:08:06 +0800

    Update push_ad.py

--- plugins/tgbot/startup/extend/push_ad.py
+++ plugins/tgbot/startup/extend/push_ad.py
@@ -44,10 +44,6 @@ def send_msg(bot, tag='ad', trigger_time=300):
     keyboard = [
         [
             types.InlineKeyboardButton(
-                text="赞助联系ME", url='tg://user?id=5568699210')
-        ],
-        [
-            types.InlineKeyboardButton(
                 text="CK资源采集", url='https://ckzy1.com/')
         ],
         [
@@ -70,7 +66,9 @@ def send_msg(bot, tag='ad', trigger_time=300):
             types.InlineKeyboardButton(
                 text="论坛", url='https://bbs.midoks.me'),
             types.InlineKeyboardButton(
-                text="搜索", url='https://bbs.midoks.me/search.php')
+                text="搜索", url='https://bbs.midoks.me/search.php'),
+            types.InlineKeyboardButton(
+                text="@ME", url='tg://user?id=5568699210')
         ]
     ]
     markup = types.InlineKeyboardMarkup(keyboard)
