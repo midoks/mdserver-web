cf0b233f634c466c58feb697098f3a9b05c167f0
midoks <midoks@163.com>
2023-05-01 14:51:54 +0800

    Update client_check_member.py

--- plugins/tgclient/startup/extend/client_check_member.py
+++ plugins/tgclient/startup/extend/client_check_member.py
@@ -21,23 +21,24 @@ from telebot.util import quick_markup
 
 
 # 指定群ID
-chat_id = -1001578009023
+chat_id_list = [-1001578009023]
 
 async def run(client):
     while True:
-        s = await client.send_message(chat_id, '开始自动检测已经注销群成员...')
-        count = 0
-        async for user in client.iter_participants(chat_id):
-            if user.deleted:
-                count += 1
-                msg = await client.kick_participant(chat_id, user)
+        for chat_id in chat_id_list:
+            s = await client.send_message(chat_id, '开始自动检测已经注销群成员...')
+            count = 0
+            async for user in client.iter_participants(chat_id):
+                if user.deleted:
+                    count += 1
+                    msg = await client.kick_participant(chat_id, user)
 
-        await client.edit_message(chat_id, s.id, '已经检测到有(%d)个账户已失效' % (count))
-        await asyncio.sleep(3)
-        await client.edit_message(chat_id, s.id, '自动检测已经注销群成员完毕!!!')
-        await asyncio.sleep(3)
-        await client.delete_messages(chat_id, s)
-        await asyncio.sleep(300)
+            await client.edit_message(chat_id, s.id, '已经检测到有(%d)个账户已失效' % (count))
+            await asyncio.sleep(3)
+            await client.edit_message(chat_id, s.id, '自动检测已经注销群成员完毕!!!')
+            await asyncio.sleep(3)
+            await client.delete_messages(chat_id, s)
+            await asyncio.sleep(300)
 
 
 if __name__ == "__main__":
