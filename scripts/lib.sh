92fa8da2f63d68cb5c0a9c8cba6a6f6f540e6462
midoks <midoks@163.com>
2023-06-13 17:19:23 +0800

    Update lib.sh

--- scripts/lib.sh
+++ scripts/lib.sh
@@ -8,6 +8,7 @@ function version_ge() { test "$(echo "$@" | tr " " "\n" | sort -rV | head -n 1)"
 
 P_VER=`python3 -V | awk '{print $2}'`
 echo "python:$P_VER"
+sleep 3
 
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
