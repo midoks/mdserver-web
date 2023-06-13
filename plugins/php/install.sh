9f02cf97443903835c8e4893549e5f83e19a0eb0
midoks <midoks@163.com>
2023-06-14 01:21:47 +0800

    Update install.sh

--- plugins/php/install.sh
+++ plugins/php/install.sh
@@ -59,6 +59,7 @@ if [ "${action}" == "install" ] && [ -d ${serverPath}/php/${type} ];then
 	echo "install PHP${type} extend start"
 
 	# cd /www/server/mdserver-web/plugins/php/versions/common  && bash iconv.sh install 53
+	# cd /www/server/mdserver-web/plugins/php/versions/common  && bash intl.sh install 73
 	# cd /www/server/mdserver-web/plugins/php/versions/common  && bash gd.sh install 56
 	# cd /www/server/mdserver-web/plugins/php/versions/common  && bash openssl.sh install 56
 	# cd /www/server/mdserver-web/plugins/php/versions/common  && bash sodium.sh install 81
