6fcc980d5d3a5bf849193d946957f139355035d5
midoks <midoks@163.com>
2023-08-05 17:19:39 +0800

    Update libzip.sh

--- plugins/php/lib/libzip.sh
+++ plugins/php/lib/libzip.sh
@@ -14,17 +14,17 @@ SERVER_ROOT=$rootPath/lib
 SOURCE_ROOT=$rootPath/source/lib
 
 
-# cn=$(curl -fsSL -m 10 http://ipinfo.io/json | grep "\"country\": \"CN\"")
-# HTTP_PREFIX="https://"
-# if [ ! -z "$cn" ];then
-#     HTTP_PREFIX="https://ghproxy.com/"
-# fi
+cn=$(curl -fsSL -m 10 http://ipinfo.io/json | grep "\"country\": \"CN\"")
+HTTP_PREFIX="https://"
+if [ ! -z "$cn" ];then
+    HTTP_PREFIX="https://ghproxy.com/"
+fi
 
 if [ ! -d ${SERVER_ROOT}/libzip ];then
 
     cd $SOURCE_ROOT
     if [ ! -f ${SOURCE_ROOT}/libzip-1.3.2.tar.gz ];then
-        wget --no-check-certificate -O libzip-1.3.2.tar.gz https://libzip.org/download/libzip-1.3.2.tar.gz -T 20
+        wget --no-check-certificate -O libzip-1.3.2.tar.gz ${HTTP_PREFIX}github.com/midoks/mdserver-web/releases/download/init/libzip-1.3.2.tar.gz -T 20
     fi
 
     tar -zxvf libzip-1.3.2.tar.gz
@@ -34,15 +34,5 @@ if [ ! -d ${SERVER_ROOT}/libzip ];then
     #cd $SOURCE_ROOT
     #rm -rf libzip-1.3.2
     #rm -rf libzip-1.3.2.tar.gz
-
-    # cd $SOURCE_ROOT
-    # if [ ! -f ${SOURCE_ROOT}/libzip-1.6.1.tar.gz ];then
-    #     wget --no-check-certificate -O libzip-1.6.1.tar.gz ${HTTP_PREFIX}github.com/nih-at/libzip/releases/download/rel-1-6-1/libzip-1.6.1.tar.gz -T 20
-    # fi
-
-    # tar -zxvf libzip-1.6.1.tar.gz
-    # cd ${SOURCE_ROOT}/libzip-1.6.1
-
-    # cmake -DCMAKE_INSTALL_PREFIX=${SERVER_ROOT}/libzip && make && make install
-
+    
 fi
