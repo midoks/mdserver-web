0692537cbd812008b787b47327daa5956eb35790
midoks <midoks@163.com>
2023-08-12 16:41:15 +0800

    update

--- plugins/php/versions/56/install.sh
+++ plugins/php/versions/56/install.sh
@@ -9,7 +9,7 @@ serverPath=$(dirname "$rootPath")
 sourcePath=${serverPath}/source
 sysName=`uname`
 install_tmp=${rootPath}/tmp/mw_install.pl
-
+SYS_ARCH=`arch`
 version=5.6.40
 PHP_VER=56
 Install_php()
@@ -87,6 +87,14 @@ else
 	cpuCore="1"
 fi
 # ----- cpu end ------
+
+
+
+if [ "${SYS_ARCH}" == "aarch64" ];then
+	# 修复aarch64架构下安装
+	# /www/server/mdserver-web/plugins/php/versions/56/src/zend_multiply.h > /www/server/source/php/php56/Zend/zend_multiply.h
+	cat ${curPath}/src/zend_multiply.h > $sourcePath/php/php${PHP_VER}/Zend/zend_multiply.h
+fi
 
 
 if [ ! -d $serverPath/php/56 ];then
