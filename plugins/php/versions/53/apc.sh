66fe66b479fb54d1b741b097b39d2eb6d7f7b86f
midoks <midoks@163.com>
2023-08-13 13:59:07 +0800

    update

--- plugins/php/versions/53/apc.sh
+++ plugins/php/versions/53/apc.sh
@@ -10,7 +10,7 @@ rootPath=$(dirname "$rootPath")
 rootPath=$(dirname "$rootPath")
 serverPath=$(dirname "$rootPath")
 sourcePath=${serverPath}/source/php
-
+SYS_ARCH=`arch`
 LIBNAME=apc
 LIBV=3.1.9
 sysName=`uname`
@@ -46,8 +46,13 @@ Install_lib()
 		fi
 		cd $php_lib/${_LIBNAME}-${LIBV}
 
+		OPTIONS=''
+		if [ "${SYS_ARCH}" == "aarch64" ] && [ "$version" -lt "56" ];then
+			OPTIONS="$OPTIONS --build=aarch64-unknown-linux-gnu --host=aarch64-unknown-linux-gnu"
+		fi
+
 		$serverPath/php/$version/bin/phpize
-		./configure --with-php-config=$serverPath/php/$version/bin/php-config && \
+		./configure --with-php-config=$serverPath/php/$version/bin/php-config $OPTIONS && \
 		make && make install && make clean
 
 	fi
