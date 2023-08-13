c80ce63a4b9a3ff4a0936694dab036953d7f554f
midoks <midoks@163.com>
2023-08-13 16:02:07 +0800

    Update push_notice_msg.py

--- plugins/tgbot/startup/extend/push_notice_msg.py
+++ plugins/tgbot/startup/extend/push_notice_msg.py
@@ -54,6 +54,7 @@ def send_msg(bot, tag='ad', trigger_time=300):
     # 高价收量 👑 集团收量 ❤️ 流量变现/支付宝代付 ❤️ 微信代付/实名认证/过人脸🕵️‍♀️各种账号处理✅ ｜ 28/m
     # https://zhaoziyuan.la/ | web | 15/m | 2m | next,9/15
     # 腾云机场 ｜9/m
+    # 🚀鲲鹏VPN | 13/m
     keyboard = [
         [
             types.InlineKeyboardButton(
@@ -65,7 +66,7 @@ def send_msg(bot, tag='ad', trigger_time=300):
         ],
         [
             types.InlineKeyboardButton(
-                text="🐶干净卫生|就是看片", url='https://www.zzzkan.com/?code=tg_mdserver-web')
+                text="🚀鲲鹏VPN，解锁流媒体和chatGPT", url='https://down.xbjc888.top/?agent_code=github')
         ],
         [
             types.InlineKeyboardButton(
