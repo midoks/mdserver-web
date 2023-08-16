a45eaf1c3b0f904c447d6cf4efeb31059a9c9366
midoks <midoks@163.com>
2023-08-16 22:09:35 +0800

    Update debian.sh

--- plugins/mongodb/versions/7.0/debian.sh
+++ plugins/mongodb/versions/7.0/debian.sh
@@ -14,7 +14,7 @@ MG_DIR=$serverPath/source/mongodb
 mkdir -p $MG_DIR
 
 if [ ! -f $MG_DIR/mongodb-linux-x86_64-debian11-${VERSION}.tgz ]; then
-	wget --no-check-certificate -O mongodb-linux-x86_64-debian11-${VERSION}.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian11-${VERSION}.tgz
+	wget --no-check-certificate -O $MG_DIR/mongodb-linux-x86_64-debian11-${VERSION}.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian11-${VERSION}.tgz
 	echo "wget --no-check-certificate -O mongodb-linux-x86_64-debian11-${VERSION}.tgz https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-debian11-${VERSION}.tgz"
 fi
 
