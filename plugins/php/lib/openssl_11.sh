907851c41399cfa50d27ba41db638bccd49c2f86
midoks <midoks@163.com>
2023-08-17 17:29:04 +0800

    update

---
+++ plugins/php/lib/openssl_11.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+opensslVersion="1.1.1p"
+# echo $rootPath
+
+SERVER_ROOT=$rootPath/lib
+SOURCE_ROOT=$rootPath/source/lib
+mkdir -p $SOURCE_ROOT
+
+if [ ! -d ${SERVER_ROOT}/openssl11 ];then
+    cd ${SOURCE_ROOT}
+    if [ ! -f ${SOURCE_ROOT}/openssl-${opensslVersion}.tar.gz ];then
+        wget --no-check-certificate -O ${SOURCE_ROOT}/openssl-${opensslVersion}.tar.gz https://www.openssl.org/source/openssl-${opensslVersion}.tar.gz
+    fi 
+    tar -zxvf openssl-${opensslVersion}.tar.gz
+    cd openssl-${opensslVersion}
+    ./config --prefix=${SERVER_ROOT}/openssl11 zlib-dynamic shared
+    make && make install
+fi
+
