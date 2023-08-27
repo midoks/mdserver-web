b7d50540ac5571a3845afa7807cddaa71a940f60
midoks <midoks@163.com>
2023-08-27 16:45:06 +0800

    Create ubuntu.sh

---
+++ plugins/dynamic-tracking/versions/ubuntu.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+install_tmp=${rootPath}/tmp/mw_install.pl
+SYS_ARCH=`arch`
