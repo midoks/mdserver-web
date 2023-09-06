b76e0f606a6917e6cdef19c26669a9ed3cd4e2f5
midoks <midoks@163.com>
2023-09-06 19:24:25 +0800

    Update install.sh

--- plugins/openresty/install.sh
+++ plugins/openresty/install.sh
@@ -72,7 +72,7 @@ Install_openresty()
 
 	# Last Download Method
 	if [ ! -f ${openrestyDir}/openresty-${VERSION}.tar.gz ];then
-		wget --no-check-certificate -O ${openrestyDir}/openresty-${VERSION}.tar.gz https://dl.midoks.me/soft/openresty/openresty-${VERSION}.tar.gz -T 3
+		wget --no-check-certificate -O ${openrestyDir}/openresty-${VERSION}.tar.gz http://dl.midoks.me/soft/openresty/openresty-${VERSION}.tar.gz -T 3
 	fi
 
 	cd ${openrestyDir} && tar -zxvf openresty-${VERSION}.tar.gz
