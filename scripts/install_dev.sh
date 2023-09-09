b703758d0ebceb42d234244938e38a5e6df3469a
midoks <midoks@163.com>
2023-09-09 11:37:18 +0800

    update

--- scripts/install_dev.sh
+++ scripts/install_dev.sh
@@ -68,9 +68,9 @@ fi
 # 	HTTP_PREFIX="https://ghproxy.com/"
 # fi
 
-cn=$(curl -fsSL -m 10 -s http://ipinfo.io/json | grep "\"country\": \"CN\"")
 HTTP_PREFIX="https://"
 LOCAL_ADDR=common
+cn=$(curl -fsSL -m 10 -s http://ipinfo.io/json | grep "\"country\": \"CN\"")
 if [ ! -z "$cn" ] || [ "$?" != "0" ] ;then
 	LOCAL_ADDR=cn
     HTTP_PREFIX="https://ghproxy.com/"
