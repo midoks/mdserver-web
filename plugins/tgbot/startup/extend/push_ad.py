8c18023f64770b64ecd8f13f59b01cf81169225e
midoks <midoks@163.com>
2023-09-14 00:17:01 +0800

    update

--- plugins/tgbot/startup/extend/push_ad.py
+++ plugins/tgbot/startup/extend/push_ad.py
@@ -45,7 +45,7 @@ def send_msg(bot, tag='ad', trigger_time=300):
     # 高价收量 👑 集团收量 ❤️ 流量变现/支付宝代付 ❤️ 微信代付/实名认证/过人脸🕵️‍♀️各种账号处理✅ ｜ 28/m
     # https://zhaoziyuan.la/ | web | 15/m | 2m | next,7/15
     # 腾云机场 ｜9/m
-    # 🚀鲲鹏VPN | 13/m
+    # 🚀鲲鹏VPN | 13/m | next,10/13
     # ✨极品资源采集 ｜ 14/m
     keyboard = [
         [
