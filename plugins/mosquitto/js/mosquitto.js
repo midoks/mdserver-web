101792a08121f88dfd7da0fe363bc852cafe1640
midoks <midoks@163.com>
2023-09-29 23:31:25 +0800

    Update mosquitto.js

--- plugins/mosquitto/js/mosquitto.js
+++ plugins/mosquitto/js/mosquitto.js
@@ -1,4 +1,4 @@
-function redisPost(method, version, args,callback){
+function mqPost(method, version, args,callback){
     var loadT = layer.msg('正在获取...', { icon: 16, time: 0, shade: 0.3 });
 
     var req_data = {};
@@ -26,7 +26,7 @@ function redisPost(method, version, args,callback){
     },'json'); 
 }
 
-function redisPostCallbak(method, version, args,callback){
+function mqPostCallbak(method, version, args,callback){
     var loadT = layer.msg('正在获取...', { icon: 16, time: 0, shade: 0.3 });
 
     var req_data = {};
@@ -51,57 +51,5 @@ function redisPostCallbak(method, version, args,callback){
             callback(data);
         }
     },'json'); 
-}
-
-//配置修改
-function getRedisConfig(version) {
-    redisPost('get_redis_conf', version,'',function(data){
-        // console.log(data);
-        var rdata = $.parseJSON(data.data);
-        // console.log(rdata);
-        var mlist = '';
-        for (var i = 0; i < rdata.length; i++) {
-            var w = '70'
-            if (rdata[i].name == 'error_reporting') w = '250';
-            var ibody = '<input style="width: ' + w + 'px;" class="bt-input-text mr5" name="' + rdata[i].name + '" value="' + rdata[i].value + '" type="text" >';
-            switch (rdata[i].type) {
-                case 0:
-                    var selected_1 = (rdata[i].value == 1) ? 'selected' : '';
-                    var selected_0 = (rdata[i].value == 0) ? 'selected' : '';
-                    ibody = '<select class="bt-input-text mr5" name="' + rdata[i].name + '" style="width: ' + w + 'px;"><option value="1" ' + selected_1 + '>开启</option><option value="0" ' + selected_0 + '>关闭</option></select>'
-                    break;
-                case 1:
-                    var selected_1 = (rdata[i].value == 'On') ? 'selected' : '';
-                    var selected_0 = (rdata[i].value == 'Off') ? 'selected' : '';
-                    ibody = '<select class="bt-input-text mr5" name="' + rdata[i].name + '" style="width: ' + w + 'px;"><option value="On" ' + selected_1 + '>开启</option><option value="Off" ' + selected_0 + '>关闭</option></select>'
-                    break;
-            }
-            mlist += '<p><span>' + rdata[i].name + '</span>' + ibody + ', <font>' + rdata[i].ps + '</font></p>'
-        }
-        var phpCon = '<style>.conf_p p{margin-bottom: 2px}</style><div class="conf_p" style="margin-bottom:0">' + mlist + '\
-                        <div style="margin-top:10px; padding-right:15px" class="text-right"><button class="btn btn-success btn-sm mr5" onclick="getRedisConfig(\'' + version + '\')">刷新</button>\
-                        <button class="btn btn-success btn-sm" onclick="submitConf(\'' + version + '\')">保存</button></div>\
-                    </div>'
-        $(".soft-man-con").html(phpCon);
-    });
-}
-
-//提交PHP配置
-function submitConf(version) {
-    var data = {
-        version: version,
-        bind: $("input[name='bind']").val(),
-        'port': $("input[name='port']").val(),
-        'timeout': $("input[name='timeout']").val(),
-        maxclients: $("input[name='maxclients']").val(),
-        databases: $("input[name='databases']").val(),
-        requirepass: $("input[name='requirepass']").val(),
-        maxmemory: $("input[name='maxmemory']").val(),
-    };
-
-    redisPost('submit_redis_conf', version, data, function(ret_data){
-        var rdata = $.parseJSON(ret_data.data);
-        layer.msg(rdata.msg, { icon: rdata.status ? 1 : 2 });
-    });
 }
 
