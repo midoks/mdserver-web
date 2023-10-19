d1dcbcb0d61701556ecb2f20b7eafbc70c8ee673
midoks <midoks@163.com>
2023-10-20 02:47:22 +0800

    Update libmemcached.sh

--- plugins/php/lib/libmemcached.sh
+++ plugins/php/lib/libmemcached.sh
@@ -14,26 +14,26 @@ SERVER_ROOT=$rootPath/lib
 SOURCE_ROOT=$rootPath/source/lib
 
 #----------------------------- libmemcached start -------------------------#
-if [ ! -d ${SERVER_ROOT}/libmemcached ];then
-    cd ${SOURCE_ROOT}
-    if [ ! -f ${SOURCE_ROOT}/libmemcached-1.0.4.tar.gz ];then
-    	wget -O libmemcached-1.0.4.tar.gz https://launchpad.net/libmemcached/1.0/1.0.4/+download/libmemcached-1.0.4.tar.gz -T 20
-    fi 
-    tar -zxf libmemcached-1.0.4.tar.gz
-    cd libmemcached-1.0.4
-    ./configure --prefix=${SERVER_ROOT}/libmemcached -with-memcached && make && make install
-fi
+# if [ ! -d ${SERVER_ROOT}/libmemcached ];then
+#     cd ${SOURCE_ROOT}
+#     if [ ! -f ${SOURCE_ROOT}/libmemcached-1.0.4.tar.gz ];then
+#     	wget -O libmemcached-1.0.4.tar.gz https://launchpad.net/libmemcached/1.0/1.0.4/+download/libmemcached-1.0.4.tar.gz -T 20
+#     fi 
+#     tar -zxf libmemcached-1.0.4.tar.gz
+#     cd libmemcached-1.0.4
+#     ./configure --prefix=${SERVER_ROOT}/libmemcached -with-memcached && make && make install
+# fi
 #----------------------------- libmemcached end -------------------------#
 
 
 #----------------------------- libmemcached start -------------------------#
-# if [ ! -d ${SERVER_ROOT}/libmemcached ];then
-#     cd ${SOURCE_ROOT}
-#     if [ ! -f ${SOURCE_ROOT}/libmemcached-1.0.18.tar.gz ];then
-#         wget --no-check-certificate -O libmemcached-1.0.18.tar.gz https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz -T 20
-#     fi 
-#     tar -zxf libmemcached-1.0.18.tar.gz
-#     cd libmemcached-1.0.18
-#     ./configure --prefix=${SERVER_ROOT}/libmemcached -with-memcached && make && make install
-# fi
+if [ ! -d ${SERVER_ROOT}/libmemcached ];then
+    cd ${SOURCE_ROOT}
+    if [ ! -f ${SOURCE_ROOT}/libmemcached-1.0.18.tar.gz ];then
+        wget --no-check-certificate -O libmemcached-1.0.18.tar.gz https://launchpad.net/libmemcached/1.0/1.0.18/+download/libmemcached-1.0.18.tar.gz -T 20
+    fi 
+    tar -zxf libmemcached-1.0.18.tar.gz
+    cd libmemcached-1.0.18
+    ./configure --prefix=${SERVER_ROOT}/libmemcached -with-memcached && make && make install
+fi
 #----------------------------- libmemcached end -------------------------#
