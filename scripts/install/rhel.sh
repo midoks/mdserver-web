a578eeef014ff1c7d9af919073fedc03a04051ca
midoks <midoks@163.com>
2023-10-20 13:15:44 +0800

    Update rhel.sh

--- scripts/install/rhel.sh
+++ scripts/install/rhel.sh
@@ -181,7 +181,7 @@ if [ "$VERSION_ID" -ge "8" ];then
         libpng libpng-devel libstdc++.so.6 libtirpc libtirpc-devel libtool libtool-libs libwebp libwebp-devel \
         libxml2 libxml2-devel libxslt libxslt-devel libarchive make mysql-devel ncurses ncurses-devel net-tools \
         oniguruma oniguruma-devel patch pcre pcre-devel perl perl-Data-Dumper perl-devel procps psmisc python3-devel \
-        openssl openssl-devel patchelf libargon2-dev\
+        openssl openssl-devel patchelf libargon2-devel\
         readline-devel rpcgen sqlite-devel rar unrar tar unzip vim-minimal wget zip zlib zlib-devel;
     do
         # dnf --enablerepo=remi,appstream,baseos,epel,extras,powertools install -y oniguruma5php-devel
@@ -202,7 +202,7 @@ else
         make mysql-devel ncurses ncurses-devel net-tools oniguruma oniguruma-devel openldap openldap-devel \
         openssl openssl-devel patch pcre pcre-devel perl perl-Data-Dumper perl-devel psmisc python-devel \
         python3-devel python3-pip re2c readline-devel rpcgen sqlite-devel tar unzip rar unrar vim-minimal vixie-cron \
-        wget zip zlib zlib-devel ImageMagick ImageMagick-devel patchelf libargon2-dev;
+        wget zip zlib zlib-devel ImageMagick ImageMagick-devel patchelf libargon2-devel;
     do
         yum install -y $rpms;
     done
