e24bf5313c42168dd34c0bdb2e68bb6d966d1a8e
midoks <midoks@163.com>
2023-11-07 19:41:29 +0800

    Update debian.sh

--- scripts/install/debian.sh
+++ scripts/install/debian.sh
@@ -179,7 +179,9 @@ apt install -y libmagickwand-dev
 
 apt install -y libxml2 libxml2-dev libbz2-dev libmcrypt-dev libpspell-dev librecode-dev
 apt install -y libgmp-dev libgmp3-dev libreadline-dev libxpm-dev
-apt install -y dia pkg-config
+apt install -y dia
+
+apt install -y pkg-config
 apt install -y zlib1g-dev
 
 apt install -y libevent-dev libncurses5-dev libldap2-dev
