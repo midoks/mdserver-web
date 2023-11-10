b4cee435178f6ce105d960e32d16b9c0cc8b6824
Mr Chen <midoks@163.com>
2023-11-10 19:29:17 +0800

    Update macos.sh

--- scripts/install/macos.sh
+++ scripts/install/macos.sh
@@ -28,7 +28,7 @@ brew install librdkafka
 brew install coreutils libxml2 xml2
 brew install md5sum libevent pidof bison
 brew install pcre2 libxpm libelf
-brew install automake
+brew install automake icu4c
 
 if [ ! -d $DEV/server/mdserver-web ]; then
 	wget -O /tmp/master.zip https://codeload.github.com/midoks/mdserver-web/zip/master
