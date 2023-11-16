ad644c3cf9b52d97098169b46ad60638fe9a5e6f
Mr Chen <midoks@163.com>
2023-11-16 17:19:35 +0800

    Update chk_mysql.sh

--- plugins/keepalived/scripts/chk_mysql.sh
+++ plugins/keepalived/scripts/chk_mysql.sh
@@ -3,13 +3,29 @@
 # 计划任务,恢复后,可自动拉起keepalived
 # {$SERVER_PATH}/keepalived/scripts/chk_mysql.sh
 
-date +'%Y-%m-%d %H:%M:%S'
-echo "start check mysql status ..."
 counter=$(netstat -na|grep "LISTEN"|grep "3306"|wc -l)
+
 if [ "${counter}" -eq 0 ]; then
+    echo "start check mysql status ..."
+    date +'%Y-%m-%d %H:%M:%S'
     echo "check mysql is down, stop keepalive"
     systemctl stop keepalived
+    date +'%Y-%m-%d %H:%M:%S'
+    echo "start check mysql end !!!"
 fi
 
-echo "start check mysql end"
-date +'%Y-%m-%d %H:%M:%S'
+
+# 恢复后，自动拉起
+# systemctl start keepalived
+if [ "${counter}" -gt 0 ]; then
+    echo "start check mysql status ..."
+    date +'%Y-%m-%d %H:%M:%S'
+    echo "check mysql is up, start keepalive"
+
+    keepalived_status=`systemctl status keepalived | grep Active | grep inactive`
+    if [ "$keepalived_status" != "" ];then
+        systemctl start keepalived
+    fi
+    date +'%Y-%m-%d %H:%M:%S'
+    echo "start check mysql end !!!"
+fi
