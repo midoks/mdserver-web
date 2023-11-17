e2d680cb2a3b52bc0e653b53064463003e571bb9
Mr Chen <midoks@163.com>
2023-11-18 02:36:18 +0800

    修复反向代理代过来的数据

--- plugins/webstats/lua/webstats_common.lua
+++ plugins/webstats/lua/webstats_common.lua
@@ -121,6 +121,10 @@ end
 
 function _M.split(self, str, reps)
     local arr = {}
+    -- 修复反向代理代过来的数据
+    if "table" == type(data) then
+        return str
+    end
     string.gsub(str,'[^'..reps..']+',function(w) table.insert(arr,w) end)
     return arr
 end
