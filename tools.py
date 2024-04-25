a48429f9f8698fd7fab2aaaa16e86cbf38c15339
Mr Chen <midoks@163.com>
2024-04-25 19:19:57 +0800

    Update tools.py

--- tools.py
+++ tools.py
@@ -243,8 +243,10 @@ def set_panel_username(username=None):
 
 def getServerIp():
     version = sys.argv[2]
+    # ip = mw.execShell(
+    #     "curl --insecure -{} -sS --connect-timeout 5 -m 60 https://v6r.ipip.net/?format=text".format(version))
     ip = mw.execShell(
-        "curl --insecure -{} -sS --connect-timeout 5 -m 60 https://v6r.ipip.net/?format=text".format(version))
+        "curl --insecure -{} -sS --connect-timeout 5 -m 60 https://ip.cachecha.com/?format=text".format(version))
     print(ip[0])
 
 
