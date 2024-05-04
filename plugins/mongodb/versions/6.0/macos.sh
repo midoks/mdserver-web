847353374fb6be410de9c8ffaafb14bb1d2cf27f
Mr Chen <midoks@163.com>
2024-05-04 21:44:32 +0800

    update

--- plugins/mongodb/versions/6.0/macos.sh
+++ plugins/mongodb/versions/6.0/macos.sh
@@ -27,3 +27,51 @@ if [ ! -d  $serverPath/mongodb/bin ];then
 	mkdir -p $serverPath/mongodb
 	cd $MG_DIR/mongodb-macos-x86_64-${VERSION} && cp -rf ./bin $serverPath/mongodb
 fi
+
+#--------------- mongosh tool install ------------------ #
+TOOL_VERSION=2.2.5
+TOOL_FILE_NAME=mongosh-${TOOL_VERSION}-darwin-x64
+if [ "aarch64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongosh-${TOOL_VERSION}-darwin-arm64
+fi
+
+if [ "arm64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongosh-${TOOL_VERSION}-darwin-arm64
+fi
+TOOL_FILE_NAME_TGZ=${TOOL_FILE_NAME}.zip
+if [ ! -f $MG_DIR/${TOOL_FILE_NAME_TGZ} ]; then
+	wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://downloads.mongodb.com/compass/${TOOL_FILE_NAME_TGZ}
+	echo "wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://downloads.mongodb.com/compass/${TOOL_FILE_NAME_TGZ}"
+fi
+
+if [ ! -d $MG_DIR/${TOOL_FILE_NAME_TGZ} ];then 
+	cd $MG_DIR && unzip ${TOOL_FILE_NAME_TGZ}
+fi
+
+cd ${MG_DIR}/${TOOL_FILE_NAME} && cp -rf ./bin $serverPath/mongodb
+cd ${MG_DIR} && rm -rf ${MG_DIR}/${TOOL_FILE_NAME}
+
+#--------------- mongodb-database-tools ------------------ #
+# https://fastdl.mongodb.org/tools/db/mongodb-database-tools-macos-arm64-100.9.4.zip
+TOOL_VERSION=100.9.4
+TOOL_FILE_NAME=mongodb-database-tools-macos-x86_64-${TOOL_VERSION}
+if [ "aarch64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongodb-database-tools-macos-arm64-${TOOL_VERSION}
+fi
+
+if [ "arm64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongodb-database-tools-macos-arm64-${TOOL_VERSION}
+fi
+
+TOOL_FILE_NAME_TGZ=${TOOL_FILE_NAME}.zip
+if [ ! -f $MG_DIR/${TOOL_FILE_NAME_TGZ} ]; then
+	wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://fastdl.mongodb.org/tools/db/${TOOL_FILE_NAME_TGZ}
+	echo "wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://fastdl.mongodb.org/tools/db/${TOOL_FILE_NAME_TGZ}"
+fi
+
+if [ ! -d $MG_DIR/${TOOL_FILE_NAME_TGZ} ];then 
+	cd $MG_DIR && unzip ${TOOL_FILE_NAME_TGZ}
+fi
+
+cd ${MG_DIR}/${TOOL_FILE_NAME} && cp -rf ./bin $serverPath/mongodb
+cd ${MG_DIR} && rm -rf ${MG_DIR}/${TOOL_FILE_NAME}
