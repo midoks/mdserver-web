ec67913cda3fe98ea17876c44fac068e11bfe123
Mr Chen <midoks@163.com>
2024-05-04 22:26:09 +0800

    update

--- plugins/mongodb/versions/5.0/ubuntu.sh
+++ plugins/mongodb/versions/5.0/ubuntu.sh
@@ -65,3 +65,27 @@ fi
 cd ${MG_DIR}/${TOOL_FILE_NAME} && cp -rf ./bin $serverPath/mongodb
 cd ${MG_DIR} && rm -rf ${MG_DIR}/${TOOL_FILE_NAME}
 
+# https://fastdl.mongodb.org/tools/db/mongodb-database-tools-ubuntu2204-x86_64-100.9.4.tgz
+#--------------- mongodb database install ------------------ #
+TOOL_VERSION=100.9.4
+TOOL_FILE_NAME=mongodb-database-tools-ubuntu${SYS_NAME}-x86_64-${TOOL_VERSION}
+if [ "aarch64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongodb-database-tools-ubuntu${SYS_NAME}-arm64-${TOOL_VERSION}
+fi
+
+if [ "arm64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongodb-database-tools-ubuntu${SYS_NAME}-arm64-${TOOL_VERSION}
+fi
+
+TOOL_FILE_NAME_TGZ=${TOOL_FILE_NAME}.tgz
+if [ ! -f $MG_DIR/${TOOL_FILE_NAME_TGZ} ]; then
+	wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://fastdl.mongodb.org/tools/db/${TOOL_FILE_NAME_TGZ}
+fi
+
+if [ ! -d $MG_DIR/${TOOL_FILE_NAME_TGZ} ];then 
+	cd $MG_DIR && tar -zxvf ${TOOL_FILE_NAME_TGZ}
+fi
+
+cd ${MG_DIR}/${TOOL_FILE_NAME} && cp -rf ./bin $serverPath/mongodb
+cd ${MG_DIR} && rm -rf ${MG_DIR}/${TOOL_FILE_NAME}
+
