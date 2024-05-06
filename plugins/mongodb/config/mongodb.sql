01055e340328d34296bfba20ba644f5436a8f384
Mr Chen <midoks@163.com>
2024-05-06 17:07:08 +0800

    Update mongodb.sql

--- plugins/mongodb/config/mongodb.sql
+++ plugins/mongodb/config/mongodb.sql
@@ -5,14 +5,6 @@ CREATE TABLE IF NOT EXISTS `config` (
 
 INSERT INTO `config` (`id`, `mg_root`) VALUES (1, 'mg_root');
 
-CREATE TABLE IF NOT EXISTS `users` (
-  `id` INTEGER PRIMARY KEY AUTOINCREMENT,
-  `username` TEXT,
-  `password` TEXT,
-  `role` TEXT DEFAULT '',
-  `ps` TEXT,
-  `addtime` TEXT
-);
 
 CREATE TABLE IF NOT EXISTS `databases` (
   `id` INTEGER PRIMARY KEY AUTOINCREMENT,
