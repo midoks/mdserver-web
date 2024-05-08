862c09778afadc01495b4442457cdae73c38325b
Mr Chen <midoks@163.com>
2024-05-08 12:32:52 +0800

    Update euler.sh

--- scripts/install/euler.sh
+++ scripts/install/euler.sh
@@ -31,7 +31,7 @@ yum install -y libmcrypt
 yum install -y libmcrypt-devel
 yum install -y mysql-devel
 yum install -y expect
-dnf install -y pv
+yum install -y pv
 
 
 # if [ -f /usr/sbin/iptables ];then
