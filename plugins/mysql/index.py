0787bff95907e81e28da34f10a5fd50ff7049ce3
Mr Chen <midoks@163.com>
2024-05-11 12:36:57 +0800

    Update index.py

--- plugins/mysql/index.py
+++ plugins/mysql/index.py
@@ -745,7 +745,7 @@ def getMyDbPos():
     return tmp.groups()[0].strip()
 
 
-def setMyDbPos():
+def setMyDbPos(version):
     args = getArgs()
     data = checkArgs(args, ['datadir'])
     if not data[0]:
@@ -760,7 +760,7 @@ def setMyDbPos():
         mw.execShell('mkdir -p ' + t_datadir)
 
     # mw.execShell('/etc/init.d/mysqld stop')
-    stop()
+    stop(version)
     mw.execShell('cp -rf ' + s_datadir + '/* ' + t_datadir + '/')
     mw.execShell('chown -R mysql mysql ' + t_datadir)
     mw.execShell('chmod -R 755 ' + t_datadir)
@@ -774,7 +774,7 @@ def setMyDbPos():
 
     mycnf = mycnf.replace(s_datadir, t_datadir)
     mw.writeFile(myfile, mycnf)
-    start()
+    start(version)
 
     result = mw.execShell(
         'ps aux|grep mysqld| grep -v grep|grep -v python')
@@ -784,7 +784,7 @@ def setMyDbPos():
     else:
         mw.execShell('pkill -9 mysqld')
         mw.writeFile(myfile, mw.readFile(path + '/etc/my_backup.cnf'))
-        start()
+        start(version)
         return mw.returnJson(False, '文件迁移失败!')
 
 
@@ -3365,7 +3365,7 @@ if __name__ == "__main__":
     elif func == 'my_db_pos':
         print(getMyDbPos())
     elif func == 'set_db_pos':
-        print(setMyDbPos())
+        print(setMyDbPos(version))
     elif func == 'my_port':
         print(getMyPort())
     elif func == 'set_my_port':
