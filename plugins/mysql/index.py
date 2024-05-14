c89ee89061517e6f11b1ca8044748d2ef6b64e4a
Mr Chen <midoks@163.com>
2024-05-15 01:20:42 +0800

    Update index.py

--- plugins/mysql/index.py
+++ plugins/mysql/index.py
@@ -3119,12 +3119,12 @@ def doFullSyncUser(version=''):
         db.execute('reset master')
 
         # 不锁表，需要删除原来数据表
-        tables = db.query('show tables from `%s`' % sync_db_import)
-        table_key = "Tables_in_" + sync_db_import
-        for tname in tables:
-            drop_db_cmd = 'drop table if exists '+sync_db_import+'.'+tname[table_key]
-            # print(drop_db_cmd)
-            db.query(drop_db_cmd)
+        # tables = db.query('show tables from `%s`' % sync_db_import)
+        # table_key = "Tables_in_" + sync_db_import
+        # for tname in tables:
+        #     drop_db_cmd = 'drop table if exists '+sync_db_import+'.'+tname[table_key]
+        #     # print(drop_db_cmd)
+        #     db.query(drop_db_cmd)
 
         pwd = pSqliteDb('config').where('id=?', (1,)).getField('mysql_root')
         sock = getSocketFile()
