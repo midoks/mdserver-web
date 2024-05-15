05992b5f829d12d6184e7001e0e33cd475c401a9
Mr Chen <midoks@163.com>
2024-05-15 17:31:49 +0800

    Update index.py

--- plugins/mysql/index.py
+++ plugins/mysql/index.py
@@ -3055,7 +3055,7 @@ def syncDatabaseRepairLog(version=''):
     # print(cmd)
 
     if op == 'get':
-        log = mw.getLastLine(tmp_log, 50)
+        log = mw.getLastLine(tmp_log, 17)
         return mw.returnJson(True, log)
 
     if op == 'cmd':
