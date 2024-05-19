41109af522be8f4f9ade4cc378b98bbeeeeac105
Mr Chen <midoks@163.com>
2024-05-19 11:27:13 +0800

    Update install.sh

--- plugins/sphinx/install.sh
+++ plugins/sphinx/install.sh
@@ -11,6 +11,7 @@ sysName=`uname`
 install_tmp=${rootPath}/tmp/mw_install.pl
 
 # cd /www/server/mdserver-web && source bin/activate && python3 plugins/sphinx/index.py rebuild
+# /www/server/sphinx/bin/bin/indexer -c /www/server/sphinx/sphinx.conf --rotate
 
 
 bash ${rootPath}/scripts/getos.sh
