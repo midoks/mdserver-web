47a17fbc465be719f8198d7fda5bdd26f352cea0
Mr Chen <midoks@163.com>
2024-05-21 20:40:31 +0800

    Update sphinx_make.py

--- plugins/sphinx/class/sphinx_make.py
+++ plugins/sphinx/class/sphinx_make.py
@@ -255,11 +255,11 @@ index {$DB_NAME}_{$TABLE_NAME}_delta:{$DB_NAME}_{$TABLE_NAME}
 		sph_field = self.makeSqlToSphinxTable(db, table)
 		ver = self.ver.replace(".1",'')
 		if float(ver) >= 3.6:
-			conf = conf.replace("{$SPH_FIELD}", sph_field)
-			conf = conf.replace("{$SPH_FIELD_INDEX}", '')
-		else:
 			conf = conf.replace("{$SPH_FIELD}", '')
 			conf = conf.replace("{$SPH_FIELD_INDEX}", sph_field)
+		else:
+			conf = conf.replace("{$SPH_FIELD}", sph_field)
+			conf = conf.replace("{$SPH_FIELD_INDEX}", '')
 		
 		return conf;
 
