a61c63645575d6285c31bf7ac704ad0ebc217166
Mr Chen <midoks@163.com>
2024-06-07 19:39:38 +0800

    Update public.js

--- route/static/app/public.js
+++ route/static/app/public.js
@@ -1397,7 +1397,7 @@ function remind(a){
 						<td>\
 							<div class="titlename c3">'+g.data[d].name+'</span>\
 								<span class="rs-status">【'+lan.bt.task_ok+'】<span>\
-								<span class="rs-time">安装等待中...</span>\
+								<span class="rs-time">安装完成</span>\
 							</div>\
 						</td>\
 						<td class="text-right c3">'+g.data[d].addtime+'</td>\
