ab3c395f2f4633e66227daa250131c4a7074d37a
Mr Chen <midoks@163.com>
2024-06-10 00:19:09 +0800

    Update index.py

--- plugins/mysql-apt/index.py
+++ plugins/mysql-apt/index.py
@@ -495,7 +495,7 @@ def my8cmd(version, method):
         isInited = True
         if version == '5.7':
             isInited = initMysql57Data()
-        elif version == '8.0':
+        elif mw.inArray(['8.0','8.1','8.2','8.3'], version):
             isInited = initMysql8Data()
 
         if not isInited:
