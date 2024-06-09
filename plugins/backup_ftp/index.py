64ac1b2188da5e4e09b0a6c9635d152599b11ae3
Mr Chen <midoks@163.com>
2024-06-09 14:13:38 +0800

    backup_ftp插件-目录备份修复2

--- plugins/backup_ftp/index.py
+++ plugins/backup_ftp/index.py
@@ -182,7 +182,7 @@ def findPathName(path, filename):
             t['filename'] = path + '/' + ff.name
             l.append(t)
     return l
-    
+
 def backupAllFunc(stype):
     os.chdir(mw.getRunDir())
 
@@ -247,6 +247,10 @@ def backupAllFunc(stype):
         bk_name = stype
 
     find_path = mw.getBackupDir() + '/' + bk_name + '/' + bk_prefix + '_' + name
+    if stype == 'path':
+        _name = 'path_{}'.format(os.path.basename(name))
+        find_path = mw.getBackupDir() + '/path/'+_name
+
     find_new_file = "ls " + find_path + \
         "_* | grep '.gz' | cut -d \  -f 1 | awk 'END {print}'"
 
@@ -255,7 +259,7 @@ def backupAllFunc(stype):
         mw.echoInfo("not find upload file!")
         return ''
 
-    print("|-准备上传文件 {}".format(filename))
+    mw.echoInfo("准备上传文件 {}".format(filename))
     ftp = FtpPSClient()
     ftp.uploadFile(filename, stype)
 
@@ -269,7 +273,7 @@ def backupAllFunc(stype):
             fn = os.path.basename(backup['filename'])
             object_name = ftp.buildDirName(stype, fn)
             ftp.deleteFile(object_name)
-            mw.echoInfo("---已清理远程过期备份文件：" + object_name)
+            mw.echoInfo("已清理远程过期备份文件：" + object_name)
             sep -= 1
             if sep < 0:
                 break
