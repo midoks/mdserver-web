1d36b669b0e77e4dcae21eff0f47b4389b8cb169
Mr Chen <midoks@163.com>
2024-06-10 15:53:37 +0800

    update

--- plugins/mysql-yum/index.py
+++ plugins/mysql-yum/index.py
@@ -423,7 +423,11 @@ def initMysql8Data():
         user = pGetDbUser()
         cmd = serverdir + '/bin/usr/sbin/mysqld --basedir=' + serverdir + '/bin/usr --datadir=' + \
             datadir + ' --initialize-insecure --lower-case-table-names=1'
-        mw.execShell(cmd)
+        data = mw.execShell(cmd)
+        
+        if data[1] != '':
+        print(data[1])
+        exit(-1)
 
         if not mw.isAppleSystem():
             mw.execShell('chown -R mysql:mysql ' + datadir)
