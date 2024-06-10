9e9e0d8f3505ff167ff2e0a81620f0d65f94050e
Mr Chen <midoks@163.com>
2024-06-10 14:52:35 +0800

    Update mysql-apt.js

--- plugins/mysql-apt/js/mysql-apt.js
+++ plugins/mysql-apt/js/mysql-apt.js
@@ -66,7 +66,7 @@ function myPostCallbak(method, version, args,callback){
     var req_data = {};
     req_data['name'] = 'mysql';
     req_data['func'] = method;
-    req_data['script']='index_mysql';
+    req_data['script']='index_mysql_apt';
     args['version'] = version;
 
  
@@ -94,7 +94,7 @@ function myPostCallbakN(method, version, args,callback){
     var req_data = {};
     req_data['name'] = 'mysql';
     req_data['func'] = method;
-    req_data['script']='index_mysql';
+    req_data['script']='index_mysql_apt';
     args['version'] = version;
 
  
