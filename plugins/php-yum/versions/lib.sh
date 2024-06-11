7b8737006cb44c34de0c60b859dabf6fc8261d16
Mr Chen <midoks@163.com>
2024-06-11 18:03:55 +0800

    Update lib.sh

--- plugins/php-yum/versions/lib.sh
+++ plugins/php-yum/versions/lib.sh
@@ -11,9 +11,15 @@ serverPath=$(dirname "$rootPath")
 version=$1
 action=$2
 
+
+php_status=`systemctl status php${version}-php-fpm | grep inactive`
+if [ "$php_status" != "" ];then
+	systemctl ${action} php${version}-php-fpm
+fi
+
 if [ -f /lib/systemd/system/php${version}-php-fpm.service ];then
 	systemctl ${action} php${version}-php-fpm
-elif [[ -f /usr/lib/systemd/system/php${version}-php-fpm.service ]]; then
+elif [ -f /usr/lib/systemd/system/php${version}-php-fpm.service ]; then
 	systemctl ${action} php${version}-php-fpm
 else
 	$serverPath/php/init.d/php${version} ${action}
