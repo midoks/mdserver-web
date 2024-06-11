c1459377a1842fa45127893c884e27f13870663c
Mr Chen <midoks@163.com>
2024-06-11 16:03:04 +0800

    Update all_test.sh

--- plugins/php-yum/all_test.sh
+++ plugins/php-yum/all_test.sh
@@ -18,7 +18,7 @@ done
 echo "php${PHP_VER} -- end"
 
 
-PHP_VER_LIST=(53 54 55 56 70 71 72 73 74 80 81)
+PHP_VER_LIST=(53 54 55 56 70 71 72 73 74 80 81 82 83)
 # PHP_VER_LIST=(81)
 for PHP_VER in ${PHP_VER_LIST[@]}; do
 	echo "php${PHP_VER} -- start"
