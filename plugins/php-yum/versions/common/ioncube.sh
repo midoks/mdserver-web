df64e00a278bd8a965ca6ef352143ca4f6d64450
Mr Chen <midoks@163.com>
2024-06-14 16:18:51 +0800

    update

---
+++ plugins/php-yum/versions/common/ioncube.sh
@@ -0,0 +1,93 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sourcePath=${serverPath}/source/php
+
+actionType=$1
+version=$2
+
+sysName=`uname`
+LIBNAME=ioncube
+LIBV=0
+
+if [ `echo "$version > 8.2"|bc` -eq 1 ];then
+	echo "I won't support it"
+	exit 0
+fi
+
+
+extVer=`bash $curPath/lib.sh $version`
+extFile=/opt/remi/php${version}/root/usr/lib64/php
+extSoFile=$extFile/${LIBNAME}.so
+cfgDir=/etc/opt/remi/php${version}/php.d
+extIni=${cfgDir}/10-${LIBNAME}.ini
+
+if [ "$sysName" == "Darwin" ];then
+	BAK='_bak'
+else
+	BAK=''
+fi
+
+SORT_LIBNAME="10-${LIBNAME}"
+
+Install_lib()
+{
+	if [ -f $extSoFile ];then
+		echo "php-$version 已安装${LIBNAME},请选择其它版本!"
+		return
+	fi
+	
+	if [ ! -f "$extFile" ];then
+
+		php_lib=$sourcePath/php_lib
+		mkdir -p $php_lib
+		if [ ! -f $php_lib/ioncube_loaders_lin.tar.gz ];then
+			wget -O $php_lib/ioncube_loaders_lin.tar.gz https://downloads.ioncube.com/loader_downloads/ioncube_loaders_lin_x86-64.tar.gz
+			cd $php_lib && tar -zxvf ioncube_loaders_lin.tar.gz
+		fi 
+		cd $php_lib/ioncube
+		
+		cp -rf $php_lib/ioncube/ioncube_loader_lin_${version}.so $extFile
+
+	fi
+
+	echo "$extFile checking ..."
+	if [ ! -f "$extFile" ];then
+		echo "ERROR!"
+		return;
+	fi
+	
+	echo  "" >> $extIni
+	echo  "[${LIBNAME}]" >> $extIni
+	echo "zend_extension=${LIBNAME}.so" >> $extIni
+	
+	systemctl restart php${version}-fpm 
+	echo '==========================================================='
+	echo 'successful!'
+}
+
+
+Uninstall_lib()
+{
+
+	if [ -f $extIni ];then
+		rm -rf $extIni
+	fi
+
+	echo '==============================================='
+	echo 'successful!'
+}
+
+
+if [ "$actionType" == 'install' ];then
+	Install_lib
+elif [ "$actionType" == 'uninstall' ];then
+	Uninstall_lib
+fi
