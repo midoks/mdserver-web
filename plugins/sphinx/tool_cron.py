33cb0149f2695bd51dbdcc19b7a59dcc5170aa49
Mr Chen <midoks@163.com>
2024-06-26 17:46:37 +0800

    Update tool_cron.py

--- plugins/sphinx/tool_cron.py
+++ plugins/sphinx/tool_cron.py
@@ -40,7 +40,7 @@ def getConfigData():
     return {
         "task_id": -1,
         "period": "day-n",
-        "where1": "7",
+        "where1": "1",
         "hour": "0",
         "minute": "15",
     }
