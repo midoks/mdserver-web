6287dc523dfe82a0cc1b7c398bad4b1418c689b9
Mr Chen <midoks@163.com>
2024-06-28 15:39:00 +0800

    Update install.sh

--- plugins/mysql-apt/versions/8.4/install.sh
+++ plugins/mysql-apt/versions/8.4/install.sh
@@ -23,10 +23,10 @@ VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -
 # cd /www/server/mdserver-web/plugins/mysql-apt && bash install.sh install 8.0
 
 # 暂时debian12没有标准版,先用11使用
-if [ "$OSNAME" == 'debian' ] && [ "$VERSION_ID" == '12' ] ;then 
-	echo "暂时不支持该${OSNAME}${VERSION_ID}" > $install_tmp
-	exit 1
-fi
+# if [ "$OSNAME" == 'debian' ] && [ "$VERSION_ID" == '12' ] ;then 
+# 	echo "暂时不支持该${OSNAME}${VERSION_ID}" > $install_tmp
+# 	exit 1
+# fi
 
 
 ARCH="amd64"
