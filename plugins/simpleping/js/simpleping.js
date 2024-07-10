818ab604e235e166c80a46a75dd7cc88b55d804d
Mr Chen <midoks@163.com>
2024-07-11 02:35:33 +0800

    update

--- plugins/simpleping/js/simpleping.js
+++ plugins/simpleping/js/simpleping.js
@@ -1,10 +1,10 @@
-function appPost(method, version, args,callback){
+
+function pingPost(method, args, callback){
     var loadT = layer.msg('正在获取...', { icon: 16, time: 0, shade: 0.3 });
 
     var req_data = {};
     req_data['name'] = 'simpleping';
     req_data['func'] = method;
-    req_data['version'] = version;
  
     if (typeof(args) == 'string'){
         req_data['args'] = JSON.stringify(toArrayObject(args));
@@ -26,13 +26,13 @@ function appPost(method, version, args,callback){
     },'json'); 
 }
 
-function appPostCallbak(method, version, args,callback){
+function pingPostCallbak(method, args, callback){
     var loadT = layer.msg('正在获取...', { icon: 16, time: 0, shade: 0.3 });
 
     var req_data = {};
     req_data['name'] = 'simpleping';
+    req_data['script'] = 'simpleping_index';
     req_data['func'] = method;
-    args['version'] = version;
  
     if (typeof(args) == 'string'){
         req_data['args'] = JSON.stringify(toArrayObject(args));
@@ -63,10 +63,188 @@ function appReadme(){
     $('.soft-man-con').html(readme);
 }
 
+// 把Unix时间戳转化成普通日期
+function toCommonTime(unix_time){
+    var unixTimestamp = new Date(unix_time*1000);
+    var commonTime = unixTimestamp.toLocaleString();
+    return commonTime;
+}
 
+function getToday(){
+   var mydate = new Date();
+   var str = "" + mydate.getFullYear() + "/";
+   str += (mydate.getMonth()+1) + "/";
+   str += mydate.getDate();
+   return str;
+}
 
+//定义周期时间
+function getBeforeDate(n){
+    var n = n;
+    var d = new Date();
+    var year = d.getFullYear();
+    var mon=d.getMonth()+1;
+    var day=d.getDate();
+    if(day <= n){
+        if(mon>1) {
+            mon = mon-1;
+        } else {
+            year = year-1;
+            mon = 12;
+        }
+    }
+    d.setDate(d.getDate()-n);
+    year = d.getFullYear();
+    mon=d.getMonth()+1;
+    day=d.getDate();
+    s = year+"/"+(mon<10?('0'+mon):mon)+"/"+(day<10?('0'+day):day);
+    return s;
+}
+
+function pingDataGraphRender(day){
+    var now = (new Date().getTime())/1000;
+    if(day==0){
+        var start = (new Date(getToday() + " 00:00:01").getTime())/1000;
+        start = Math.round(start);
+        var end = Math.round(now);
+    }
+    if(day==1){
+        var start = (new Date(getBeforeDate(day) + " 00:00:01").getTime())/1000;
+        var end = (new Date(getBeforeDate(day) + " 23:59:59").getTime())/1000;
+        start = Math.round(start);
+        end = Math.round(end);
+    } else {
+        var start = (new Date(getBeforeDate(day) + " 00:00:01").getTime())/1000;
+            start = Math.round(start);
+        var end = Math.round(now);
+    }
+
+    // console.log(start,end);
+    pingPostCallbak('pingData', {'type':'range', 'start':start, 'end':end}, function(data){
+        var xData = [];
+        var yData = [];
+
+        var rdata = data.data;
+        
+        for(var i = 0; i < rdata.length; i++){
+            xData.push(toCommonTime(rdata[i].created_unix));
+            yData.push(rdata[i].speed/1000000);
+        }
+        var option = {
+            tooltip: {
+                trigger: 'axis',
+                axisPointer: { type: 'cross' },
+                formatter: '{b}<br />{a}: {c}'
+            },
+            xAxis: {
+                type: 'category',
+                boundaryGap: false,
+                data: xData,
+                axisLine:{ lineStyle:{ color:"#666"} } 
+            },
+            yAxis: {
+                type: 'value',
+                name: "PING延迟(ms)",
+                // boundaryGap: [0, '100%'],
+                // min:0,
+                // max: 100,
+                splitLine:{ lineStyle:{ color:"#ddd" } },
+                axisLine:{ lineStyle:{ color:"#666" } }
+            },
+            series: [
+                {
+                    name:'PING',
+                    type:'line',
+                    smooth:true,
+                    symbol: 'none',
+                    sampling: 'average',
+                    itemStyle: { normal: { color: 'rgb(0, 153, 238)' } },
+                    data: yData
+                }
+            ]
+        };
+        chartPing.setOption(option);
+    });
+}
+
+// console.log('pingDataGraph');
+var chartPing;
 function pingDataGraph(){
-    $('.soft-man-con').html('');
+    var tpl = '\
+    <div class="col-xs-12 col-sm-12 col-md-12 pull-left pd0 view1">\
+        <div class="pr8">\
+            <div class="bgw pb15">\
+                <div class="title c6 plr15">\
+                    <h3 class="c-tit f16">连通性</h3>\
+                    <div class="searcTime pull-right">\
+                        <span class="tit">区间检索：</span>\
+                        <span class="gt" onclick="pingDataGraphRender(1)">昨天</span>\
+                        <span class="gt on" onclick="pingDataGraphRender(0)">今天</span>\
+                        <span class="gt" onclick="pingDataGraphRender(7)">最近7天</span>\
+                    </div>\
+                </div>\
+                <div id="pingview" style="width:100%; height:330px"></div>\
+            </div>\
+        </div>\
+    </div>';
+    $('.soft-man-con').html(tpl);
+
+    chartPing = echarts.init(document.getElementById('pingview'));
+
+    var option = {
+        tooltip: {
+            trigger: 'axis',
+            axisPointer: { type: 'cross' },
+            formatter: '{b}<br />{a}: {c}'
+        },
+        xAxis: {
+            type: 'category',
+            boundaryGap: false,
+            data: [],
+            axisLine:{ lineStyle:{ color:"#666"} } 
+        },
+        yAxis: {
+            type: 'value',
+            name: "PING延迟(ms)",
+            splitLine:{ lineStyle:{ color:"#ddd" } },
+            axisLine:{ lineStyle:{ color:"#666" } }
+        },
+        dataZoom: [{
+            type: 'inside',
+            start: 0,
+            end: 100,
+            zoomLock:true
+        }, {
+            start: 0,
+            end: 100,
+            handleIcon: 'M10.7,11.9v-1.3H9.3v1.3c-4.9,0.3-8.8,4.4-8.8,9.4c0,5,3.9,9.1,8.8,9.4v1.3h1.3v-1.3c4.9-0.3,8.8-4.4,8.8-9.4C19.5,16.3,15.6,12.2,10.7,11.9z M13.3,24.4H6.7V23h6.6V24.4z M13.3,19.6H6.7v-1.4h6.6V19.6z',
+            handleSize: '80%',
+            handleStyle: {
+                color: '#fff',
+                shadowBlur: 3,
+                shadowColor: 'rgba(0, 0, 0, 0.6)',
+                shadowOffsetX: 2,
+                shadowOffsetY: 2
+            }
+        }],
+        series: [
+            {
+                name:'PING',
+                type:'line',
+                smooth:true,
+                symbol: 'none',
+                sampling: 'average',
+                itemStyle: { normal: { color: 'rgb(0, 153, 238)' } },
+                data: []
+            }
+        ]
+    };
+    chartPing.setOption(option);
+    window.addEventListener("resize",function(){
+        chartPing.resize();
+    });
+
+    pingDataGraphRender(0);
 }
 
 
---
+++ plugins/simpleping/simpleping_index.py
@@ -0,0 +1,42 @@
+# coding:utf-8
+
+import sys
+import io
+import os
+import time
+import re
+import json
+import shutil
+
+sys.path.append(os.getcwd() + "/class/core")
+import mw
+
+app_debug = False
+if mw.isAppleSystem():
+    app_debug = True
+
+def getPluginName():
+    return 'simpleping'
+
+
+def getPluginDir():
+    return mw.getPluginDir() + '/' + getPluginName()
+
+def getServerDir():
+    return mw.getServerDir() + '/' + getPluginName()
+
+def pingData(args = ()):
+    conn = mw.M('sp_ping').dbPos(getServerDir()+'/data', 'simpleping', 'db3')
+    field = 'id,speed,created_unix'
+    conn = conn.field(field)
+    data = []
+    atype = args['type']
+    if atype == 'pos':
+        pos = args['pos']
+        data = conn.where('created_unix>=?',pos).select()
+    elif atype == 'range':
+        start = args['start']
+        end = args['end']
+        data = conn.where('created_unix>=? and created_unix<=?',(start,end)).select()
+    return data
+
