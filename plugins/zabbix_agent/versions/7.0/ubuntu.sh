b52635c747b6791d1a97394b2212fe925e29ce00
Mr Chen <midoks@163.com>
2024-07-19 23:41:41 +0800

    Update ubuntu.sh

--- plugins/zabbix_agent/versions/7.0/ubuntu.sh
+++ plugins/zabbix_agent/versions/7.0/ubuntu.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
+export DEBIAN_FRONTEND=noninteractive
 
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
