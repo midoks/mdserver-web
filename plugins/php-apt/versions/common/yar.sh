93ddd3c2c48177d9673d76fb21d6da546f83e44b
Mr Chen <midoks@163.com>
2024-07-23 21:42:42 +0800

    Update yar.sh

--- plugins/php-apt/versions/common/yar.sh
+++ plugins/php-apt/versions/common/yar.sh
@@ -15,18 +15,14 @@ version=$2
 
 sysName=`uname`
 LIBNAME=yar
-LIBV=2.3.2
-
-if [[  "$version" =~ "8.0" ]];then
-	LIBV=3.3.6
-fi
+LIBV=2.3.3
 
 if [[  "$version" =~ "5.0" ]];then
 	LIBV=1.2.5
 fi
 
 if [[  "$version" =~ "7.0" ]];then
-	LIBV=2.3.2
+	LIBV=2.3.3
 fi
 
 extVer=`bash $curPath/lib.sh $version`
