f2d5e166daaeb8081dcce738be46409704401583
Mr Chen <midoks@163.com>
2024-07-25 00:36:11 +0800

    Update install.sh

--- plugins/mysql/versions/5.6/install.sh
+++ plugins/mysql/versions/5.6/install.sh
@@ -165,7 +165,6 @@ Install_mysql()
 		else
 			# rm -rf ${mysqlDir}/mysql-5.6.*
 			echo "${VERSION}安装失败"
-			echo 'install fail'>&2
 			exit 1
 		fi
 	fi
@@ -174,7 +173,7 @@ Install_mysql()
 Uninstall_mysql()
 {
 	rm -rf $serverPath/mysql
-	echo '卸载完成' > $install_tmp
+	echo '卸载完成'
 }
 
 action=$1
