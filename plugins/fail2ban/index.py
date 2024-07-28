fc982674f8f77ec4d1f9b24d291d1b72a90f69ad
Mr Chen <midoks@163.com>
2024-07-28 19:22:17 +0800

    fail2ban init

---
+++ plugins/fail2ban/index.py
@@ -0,0 +1,314 @@
+# coding:utf-8
+
+import sys
+import io
+import os
+import time
+import re
+
+sys.path.append(os.getcwd() + "/class/core")
+import mw
+
+app_debug = False
+if mw.isAppleSystem():
+    app_debug = True
+
+
+def getPluginName():
+    return 'fail2ban'
+
+
+def getPluginDir():
+    return mw.getPluginDir() + '/' + getPluginName()
+
+
+def getServerDir():
+    return mw.getServerDir() + '/' + getPluginName()
+
+
+def getInitDFile():
+    current_os = mw.getOs()
+    if current_os == 'darwin':
+        return '/tmp/' + getPluginName()
+
+    if current_os.startswith('freebsd'):
+        return '/etc/rc.d/' + getPluginName()
+
+    return '/etc/init.d/' + getPluginName()
+
+
+def getConf():
+    path = getServerDir() + "/redis.conf"
+    return path
+
+
+def getConfTpl():
+    path = getPluginDir() + "/config/redis.conf"
+    return path
+
+
+def getInitDTpl():
+    path = getPluginDir() + "/init.d/" + getPluginName() + ".tpl"
+    return path
+
+
+def getArgs():
+    args = sys.argv[3:]
+    tmp = {}
+    args_len = len(args)
+
+    if args_len == 1:
+        t = args[0].strip('{').strip('}')
+        if t.strip() == '':
+            tmp = []
+        else:
+            t = t.split(':')
+            tmp[t[0]] = t[1]
+        tmp[t[0]] = t[1]
+    elif args_len > 1:
+        for i in range(len(args)):
+            t = args[i].split(':')
+            tmp[t[0]] = t[1]
+    return tmp
+
+def checkArgs(data, ck=[]):
+    for i in range(len(ck)):
+        if not ck[i] in data:
+            return (False, mw.returnJson(False, '参数:(' + ck[i] + ')没有!'))
+    return (True, mw.returnJson(True, 'ok'))
+
+def configTpl():
+    path = getPluginDir() + '/tpl'
+    pathFile = os.listdir(path)
+    tmp = []
+    for one in pathFile:
+        file = path + '/' + one
+        tmp.append(file)
+    return mw.getJson(tmp)
+
+
+def readConfigTpl():
+    args = getArgs()
+    data = checkArgs(args, ['file'])
+    if not data[0]:
+        return data[1]
+
+    content = mw.readFile(args['file'])
+    content = contentReplace(content)
+    return mw.returnJson(True, 'ok', content)
+
+def getPidFile():
+    file = getConf()
+    content = mw.readFile(file)
+    rep = r'pidfile\s*(.*)'
+    tmp = re.search(rep, content)
+    return tmp.groups()[0].strip()
+
+def status():
+    pid_file = getPidFile()
+    if not os.path.exists(pid_file):
+        return 'stop'
+
+    # data = mw.execShell(
+    #     "ps aux|grep redis |grep -v grep | grep -v python | grep -v mdserver-web | awk '{print $2}'")
+
+    # if data[0] == '':
+    #     return 'stop'
+    return 'start'
+
+def contentReplace(content):
+    service_path = mw.getServerDir()
+    content = content.replace('{$ROOT_PATH}', mw.getRootDir())
+    content = content.replace('{$SERVER_PATH}', service_path)
+    return content
+
+
+
+def initDreplace():
+
+    file_tpl = getInitDTpl()
+    service_path = os.path.dirname(os.getcwd())
+
+    initD_path = getServerDir() + '/init.d'
+    if not os.path.exists(initD_path):
+        os.mkdir(initD_path)
+    file_bin = initD_path + '/' + getPluginName()
+
+    # initd replace
+    if not os.path.exists(file_bin):
+        content = mw.readFile(file_tpl)
+        content = content.replace('{$SERVER_PATH}', service_path)
+        mw.writeFile(file_bin, content)
+        mw.execShell('chmod +x ' + file_bin)
+
+    # log
+    dataLog = getServerDir() + '/data'
+    if not os.path.exists(dataLog):
+        mw.execShell('chmod +x ' + file_bin)
+
+    # config replace
+    dst_conf = getServerDir() + '/redis.conf'
+    dst_conf_init = getServerDir() + '/init.pl'
+    if not os.path.exists(dst_conf_init):
+        conf_content = mw.readFile(getConfTpl())
+        conf_content = conf_content.replace('{$SERVER_PATH}', service_path)
+        conf_content = conf_content.replace(
+            '{$REDIS_PASS}', mw.getRandomString(10))
+
+        mw.writeFile(dst_conf, conf_content)
+        mw.writeFile(dst_conf_init, 'ok')
+
+    # systemd
+    systemDir = mw.systemdCfgDir()
+    systemService = systemDir + '/' + getPluginName() + '.service'
+    if os.path.exists(systemDir) and not os.path.exists(systemService):
+        systemServiceTpl = getPluginDir() + '/init.d/' + getPluginName() + '.service.tpl'
+        service_path = mw.getServerDir()
+        se_content = mw.readFile(systemServiceTpl)
+        se_content = se_content.replace('{$SERVER_PATH}', service_path)
+        mw.writeFile(systemService, se_content)
+        mw.execShell('systemctl daemon-reload')
+
+    return file_bin
+
+
+def f2bOp(method):
+    file = initDreplace()
+
+    current_os = mw.getOs()
+    if current_os == "darwin":
+        data = mw.execShell(file + ' ' + method)
+        if data[1] == '':
+            return 'ok'
+        return data[1]
+
+    if current_os.startswith("freebsd"):
+        data = mw.execShell('service ' + getPluginName() + ' ' + method)
+        if data[1] == '':
+            return 'ok'
+        return data[1]
+
+    data = mw.execShell('systemctl ' + method + ' ' + getPluginName())
+    if data[1] == '':
+        return 'ok'
+    return data[1]
+
+
+def start():
+    return redisOp('start')
+
+
+def stop():
+    return redisOp('stop')
+
+
+def restart():
+    status = redisOp('restart')
+
+    log_file = runLog()
+    mw.execShell("echo '' > " + log_file)
+    return status
+
+
+def reload():
+    return redisOp('reload')
+
+
+def getPort():
+    conf = getServerDir() + '/redis.conf'
+    content = mw.readFile(conf)
+
+    rep = r"^(" + r'port' + r')\s*([.0-9A-Za-z_& ~]+)'
+    tmp = re.search(rep, content, re.M)
+    if tmp:
+        return tmp.groups()[1]
+
+    return '6379'
+
+
+def initdStatus():
+    current_os = mw.getOs()
+    if current_os == 'darwin':
+        return "Apple Computer does not support"
+
+    if current_os.startswith('freebsd'):
+        initd_bin = getInitDFile()
+        if os.path.exists(initd_bin):
+            return 'ok'
+
+    shell_cmd = 'systemctl status ' + \
+        getPluginName() + ' | grep loaded | grep "enabled;"'
+    data = mw.execShell(shell_cmd)
+    if data[0] == '':
+        return 'fail'
+    return 'ok'
+
+
+def initdInstall():
+    current_os = mw.getOs()
+    if current_os == 'darwin':
+        return "Apple Computer does not support"
+
+    # freebsd initd install
+    if current_os.startswith('freebsd'):
+        import shutil
+        source_bin = initDreplace()
+        initd_bin = getInitDFile()
+        shutil.copyfile(source_bin, initd_bin)
+        mw.execShell('chmod +x ' + initd_bin)
+        mw.execShell('sysrc ' + getPluginName() + '_enable="YES"')
+        return 'ok'
+
+    mw.execShell('systemctl enable ' + getPluginName())
+    return 'ok'
+
+
+def initdUinstall():
+    current_os = mw.getOs()
+    if current_os == 'darwin':
+        return "Apple Computer does not support"
+
+    if current_os.startswith('freebsd'):
+        initd_bin = getInitDFile()
+        os.remove(initd_bin)
+        mw.execShell('sysrc ' + getPluginName() + '_enable="NO"')
+        return 'ok'
+
+    mw.execShell('systemctl disable ' + getPluginName())
+    return 'ok'
+
+
+def runLog():
+    return getServerDir() + '/data/redis.log'
+
+
+if __name__ == "__main__":
+    func = sys.argv[1]
+    if func == 'status':
+        print(status())
+    elif func == 'start':
+        print(start())
+    elif func == 'stop':
+        print(stop())
+    elif func == 'restart':
+        print(restart())
+    elif func == 'reload':
+        print(reload())
+    elif func == 'initd_status':
+        print(initdStatus())
+    elif func == 'initd_install':
+        print(initdInstall())
+    elif func == 'initd_uninstall':
+        print(initdUinstall())
+    elif func == 'run_info':
+        print(runInfo())
+    elif func == 'conf':
+        print(getConf())
+    elif func == 'run_log':
+        print(runLog())
+    elif func == 'config_tpl':
+        print(configTpl())
+    elif func == 'read_config_tpl':
+        print(readConfigTpl())
+    else:
+        print('error')
---
+++ plugins/fail2ban/install.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+install_tmp=${rootPath}/tmp/mw_install.pl
+VERSION=$2
+
+Install_App()
+{
+	echo '正在安装脚本文件...' > $install_tmp
+	mkdir -p $serverPath/source
+
+	mkdir -p $serverPath/fail2ban
+	sed '/^ *#/d' fail2ban.conf > $serverPath/fail2ban/redis.conf
+
+	echo "${VERSION}" > $serverPath/fail2ban/version.pl
+	echo '安装fail2ban完成'
+
+	cd ${rootPath} && python3 ${rootPath}/plugins/fail2ban/index.py start
+	cd ${rootPath} && python3 ${rootPath}/plugins/fail2ban/index.py initd_install
+		
+
+}
+
+Uninstall_App()
+{
+	if [ -f /usr/lib/systemd/system/fail2ban.service ];then
+		systemctl stop fail2ban
+		systemctl disable fail2ban
+		rm -rf /usr/lib/systemd/system/fail2ban.service
+		systemctl daemon-reload
+	fi
+
+	if [ -f /lib/systemd/system/fail2ban.service ];then
+		systemctl stop fail2ban
+		systemctl disable fail2ban
+		rm -rf /lib/systemd/system/fail2ban.service
+		systemctl daemon-reload
+	fi
+
+	if [ -f $serverPath/fail2ban/initd/fail2ban ];then
+		$serverPath/fail2ban/initd/fail2ban stop
+	fi
+
+	if [ -d $serverPath/fail2ban ];then
+		rm -rf $serverPath/fail2ban
+	fi
+	
+	echo "卸载fail2ban成功"
+}
+
+action=$1
+if [ "${1}" == 'install' ];then
+	Install_App
+else
+	Uninstall_App
+fi
