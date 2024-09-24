0be966646b4f32921f25ab42724695a5a77c705b
Mr Chen <midoks@163.com>
2024-09-24 13:38:51 +0800

    Update rocky.sh

--- scripts/install/rocky.sh
+++ scripts/install/rocky.sh
@@ -23,6 +23,7 @@ yum install -y expect
 yum install -y curl curl-devel libcurl libcurl-devel
 yum install -y bzip2 
 yum install -y bzip2-devel
+yum install -y re2c 
 
 if [ -f /usr/sbin/iptables ];then
 
