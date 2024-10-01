d3fb392f28b1c4ca793d31829270522ec6086ca3
Mr Chen <midoks@163.com>
2024-10-02 00:02:23 +0800

    Update config_local.py

--- plugins/pgadmin/conf/config_local.py
+++ plugins/pgadmin/conf/config_local.py
@@ -1,5 +1,5 @@
-LOG_FILE = '${SERVER_PATH}/pgadmin4/pgadmin4.log'
-SQLITE_PATH = '${SERVER_PATH}/pgadmin4/pgadmin4.db'
-SESSION_DB_PATH = '${SERVER_PATH}/pgadmin4/sessions'
-STORAGE_DIR = '${SERVER_PATH}/pgadmin4/storage'
+LOG_FILE = '${DATA_PATH}/pgadmin4/pgadmin4.log'
+SQLITE_PATH = '${DATA_PATH}/pgadmin4/pgadmin4.db'
+SESSION_DB_PATH = '${DATA_PATH}/pgadmin4/sessions'
+STORAGE_DIR = '${DATA_PATH}/pgadmin4/storage'
 SERVER_MODE = True
