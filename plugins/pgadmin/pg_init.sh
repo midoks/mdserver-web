a1464006db43e8b28b1350cc298e1b478f56de55
Mr Chen <midoks@163.com>
2024-10-02 01:35:51 +0800

    Update pg_init.sh

--- plugins/pgadmin/pg_init.sh
+++ plugins/pgadmin/pg_init.sh
@@ -1,12 +1,17 @@
-#!/bin/expect -d
-set timeout 20
+#!/bin/bash
 
+python_ver=`ls /www/server/pgadmin/run/lib/ | grep python | cut -d \  -f 1 | awk 'END {print}'`
+
+expect <<-EOF
+set time 10
 spawn gunicorn --bind unix:/tmp/pgadmin4.sock \
 --workers=1 \
 --threads=25 \
---chdir /www/server/pgadmin/run/lib/python3.10/site-packages/pgadmin4 pgAdmin4:app
+--chdir /www/server/pgadmin/run/lib/${python_ver}/site-packages/pgadmin4 pgAdmin4:app
 expect {
     "Email address:" {  send "mdserver-web@gmail.com\r"; exp_continue }
-    "Password" {  send "123123\r"  }
+    "Password" {  send "123123\r"; exp_continue  }
+    "Retype password" {  send "123123\r"  }
 }
 expect eof
+EOF
