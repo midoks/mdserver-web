c42b92bb9595b935260ec11b504410a6446bac01
Mr Chen <midoks@163.com>
2024-10-19 21:32:18 +0800

    update

--- web/admin/submodules.py
+++ web/admin/submodules.py
@@ -2,10 +2,12 @@
 from .dashboard import blueprint as DashboardModule
 from .site import blueprint as SiteModule
 from .task import blueprint as TaskModule
+from .config import blueprint as ConfigModule
 
 def get_submodules():
     return [
         DashboardModule,
         SiteModule,
         TaskModule,
+        ConfigModule
     ]
