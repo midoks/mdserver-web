2790a3890bb5153a738f1216b864fc7b9b93ea62
Mr Chen <midoks@163.com>
2024-10-23 20:53:37 +0800

    Update __init__.py

--- web/admin/files/__init__.py
+++ web/admin/files/__init__.py
@@ -20,6 +20,7 @@ blueprint = Blueprint('files', __name__, url_prefix='/files', template_folder='.
 def index():
     return render_template('files.html', data={})
 
+# 获取文件内容
 @blueprint.route('/get_body', endpoint='getFileBody', methods=['POST'])
 def getFileBody():
     path = request.form.get('path', '')
@@ -53,6 +54,31 @@ def getFileBody():
         return mw.returnData(False, '文件未正常打开!')
     return mw.returnData(True, 'OK', data)
 
+# 获取文件内容
+@blueprint.route('/save_body', endpoint='saveBody', methods=['POST'])
+def saveBody():
+    path = request.form.get('path', '')
+    data = request.form.get('data', '')
+    encoding = request.form.get('encoding', '')
+    if not os.path.exists(path):
+        return mw.returnData(False, '文件不存在')
+    try:
+        if encoding == 'ascii':
+            encoding = 'utf-8'
+
+        data = data.encode(encoding, errors='ignore').decode(encoding)
+        fp = open(path, 'w+', encoding=encoding)
+        fp.write(data)
+        fp.close()
+
+        if path.find("web_conf") > 0:
+            mw.restartWeb()
+        # mw.writeLog('文件管理', '文件[{1}]保存成功', (path,))
+        return mw.returnData(True, '文件保存成功')
+    except Exception as ex:
+        return mw.returnData(False, '文件保存错误:' + str(ex))
+
+# 获取文件内容(最新行数)
 @blueprint.route('/get_last_body', endpoint='getFileLastBody', methods=['POST'])
 def getFileLastBody():
     path = request.form.get('path', '')
