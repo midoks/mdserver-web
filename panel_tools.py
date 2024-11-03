7c696c24f3ac9a1d877cf91463c8bb788a79cf2a
Mr Chen <midoks@163.com>
2024-11-03 18:48:17 +0800

    Update panel_tools.py

--- panel_tools.py
+++ panel_tools.py
@@ -230,7 +230,7 @@ def set_panel_pwd(password, ncli=False):
 
 def show_panel_pwd():
     # 面板密码展示
-    info = model.getUserByRoot()
+    info = thisdb.getUserByRoot()
     defailt_pwd_file = mw.getPanelDir()+'/data/default.pl'
     pwd = ''
     if os.path.exists(defailt_pwd_file):
