27e1e0c73c827a36020877d9ed018e71288749a7
Mr Chen <midoks@163.com>
2024-11-24 05:11:35 +0800

    Update mw.py

--- web/core/mw.py
+++ web/core/mw.py
@@ -205,7 +205,7 @@ def readFile(filename):
         print(e)
         return False
 
-def writeFile(filename, content, mode='aw+'):
+def writeFile(filename, content, mode='w+'):
     # 写文件内容
     try:
         fp = open(filename, mode)
