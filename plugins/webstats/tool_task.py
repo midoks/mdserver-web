7eb88d6a9215711bbdf40905d4faaf812e4b227d
Mr Chen <midoks@163.com>
2024-11-24 04:43:00 +0800

    Update tool_task.py

--- plugins/webstats/tool_task.py
+++ plugins/webstats/tool_task.py
@@ -6,10 +6,14 @@ import os
 import time
 import json
 
-from utils.crontab import crontab as MwCrontab
 
-sys.path.append(os.getcwd() + "/class/core")
-import mw
+web_dir = os.getcwd() + "/web"
+if os.path.exists(web_dir):
+    sys.path.append(web_dir)
+    os.chdir(web_dir)
+
+import core.mw as mw
+from utils.crontab import crontab as MwCrontab
 
 
 app_debug = False
