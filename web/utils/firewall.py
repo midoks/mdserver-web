d5678d3f34db6ed582cc2f6641e01598b5ea6f96
Mr Chen <midoks@163.com>
2024-11-25 03:08:33 +0800

    Update firewall.py

--- web/utils/firewall.py
+++ web/utils/firewall.py
@@ -315,6 +315,8 @@ class Firewall(object):
 
         if port.find(':') > 0:
             pass
+        elif port.find('-') > 0:
+            pass
         else:
             if(int(port) == int(panel_port)):
                 return mw.returnData(False, '失败，不能删除当前面板端口!')
