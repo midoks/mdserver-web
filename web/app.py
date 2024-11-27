ef59c3a654d3b13be05a105b77db5526db43cee5
Mr Chen <midoks@163.com>
2024-11-28 01:24:24 +0800

    Update app.py

--- web/app.py
+++ web/app.py
@@ -21,6 +21,9 @@ from geventwebsocket.handler import WebSocketHandler
 # from gevent import monkey
 # monkey.patch_all()
 
+import eventlet
+eventlet.monkey_patch()
+
 
 # from requests.packages.urllib3.util.ssl_ import create_urllib3_context
 # create_urllib3_context()
