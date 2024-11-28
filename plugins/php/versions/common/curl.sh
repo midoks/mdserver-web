dc5a81ea89c1f8a7280e496c77f528fed22b33bb
Mr Chen <midoks@163.com>
2024-11-29 01:07:57 +0800

    Create curl.sh

---
+++ plugins/php/versions/common/curl.sh
@@ -0,0 +1,123 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH=$PATH:/opt/homebrew/bin
+
+curPath=`pwd`
+
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sourcePath=${serverPath}/source/php
+SYS_ARCH=`arch`
+
+actionType=$1
+version=$2
+
+
+LIBNAME=curl
+LIBV=0
+
+LIB_PATH_NAME=lib/php
+if [ -d $serverPath/php/${version}/lib64 ];then
+	LIB_PATH_NAME=lib64
+fi
+
+NON_ZTS_FILENAME=`ls $serverPath/php/${version}/${LIB_PATH_NAME}/extensions | grep no-debug-non-zts`
+extFile=$serverPath/php/${version}/${LIB_PATH_NAME}/extensions/${NON_ZTS_FILENAME}/${LIBNAME}.so
+
+sysName=`uname`
+if [ "$sysName" == "Darwin" ];then
+	BAK='_bak'
+else
+	BAK=''
+fi
+
+
+Install_lib()
+{
+
+	isInstall=`cat $serverPath/php/$version/etc/php.ini|grep "${LIBNAME}.so"`
+	if [ "${isInstall}" != "" ];then
+		echo "php-$version 已安装${LIBNAME},请选择其它版本!"
+		return
+	fi
+	
+	if [ ! -f "$extFile" ];then
+
+		if [ ! -d $sourcePath/php${version}/ext ];then
+			cd ${rootPath}/plugins/php && /bin/bash ${rootPath}/plugins/php/versions/${version}/install.sh install 
+		fi
+
+		cd $sourcePath/php${version}/ext/${LIBNAME}
+		if [ "${SYS_ARCH}" == "aarch64" ] && [ "$version" -lt "56" ];then
+			OPTIONS="$OPTIONS --build=aarch64-unknown-linux-gnu --host=aarch64-unknown-linux-gnu"
+		fi
+
+		if [ "$sysName" == "Darwin" ];then
+
+			BREW_DIR=`which brew`
+			BREW_DIR=${BREW_DIR/\/bin\/brew/}
+			LIB_DEPEND_DIR=`brew info curl | grep ${BREW_DIR}/Cellar/curl | cut -d \  -f 1 | awk 'END {print}'`
+			OPTIONS="$OPTIONS --with-curl=$(brew --prefix curl)"
+			export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$LIB_DEPEND_DIR/lib/pkgconfig
+		fi		
+
+
+		$serverPath/php/$version/bin/phpize
+		echo "./configure --with-php-config=$serverPath/php/$version/bin/php-config $OPTIONS"
+		./configure --with-php-config=$serverPath/php/$version/bin/php-config $OPTIONS
+		
+		make clean && make && make install && make clean
+		
+		if [ -d $sourcePath/php${version} ];then
+			cd ${sourcePath} && rm -rf $sourcePath/php${version}
+		fi
+	fi
+
+	if [ ! -f "$extFile" ];then
+		echo "ERROR!"
+		return
+	fi
+
+
+    echo "" >> $serverPath/php/$version/etc/php.ini
+	echo "[${LIBNAME}]" >> $serverPath/php/$version/etc/php.ini
+	echo "extension=${LIBNAME}.so" >> $serverPath/php/$version/etc/php.ini
+	
+	cd  ${curPath} && bash ${rootPath}/plugins/php/versions/lib.sh $version restart
+	echo '==========================================================='
+	echo 'successful!'
+}
+
+
+Uninstall_lib()
+{
+	if [ ! -f "$serverPath/php/$version/bin/php-config" ];then
+		echo "php-$version 未安装,请选择其它版本!"
+		return
+	fi
+	
+	if [ ! -f "$extFile" ];then
+		echo "php-$version 未安装${LIBNAME},请选择其它版本!"
+		return
+	fi
+	
+	echo $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/${LIBNAME}.so/d" $serverPath/php/$version/etc/php.ini
+	sed -i $BAK "/${LIBNAME}/d" $serverPath/php/$version/etc/php.ini
+		
+	rm -f $extFile
+	cd  ${curPath} && bash ${rootPath}/plugins/php/versions/lib.sh $version restart
+	echo '==============================================='
+	echo 'successful!'
+}
+
+
+
+if [ "$actionType" == 'install' ];then
+	Install_lib
+elif [ "$actionType" == 'uninstall' ];then
+	Uninstall_lib
+fi
