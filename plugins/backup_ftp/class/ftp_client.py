7a1bc2ee3971386fc8bfbc51b8bf7aae9665148a
Mr Chen <midoks@163.com>
2024-11-29 18:45:45 +0800

    Update ftp_client.py

--- plugins/backup_ftp/class/ftp_client.py
+++ plugins/backup_ftp/class/ftp_client.py
@@ -14,8 +14,12 @@ import json
 import paramiko
 import ftplib
 
-sys.path.append(os.getcwd() + "/class/core")
-import mw
+web_dir = os.getcwd() + "/web"
+if os.path.exists(web_dir):
+    sys.path.append(web_dir)
+    os.chdir(web_dir)
+
+import core.mw as mw
 
 DEBUG = True
 BLOCK_SIZE = 1024 * 1024 * 2
