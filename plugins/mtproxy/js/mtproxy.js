1c0dbe76f9db715e9fb77403b9308a4e83869ef6
Mr Chen <midoks@163.com>
2024-12-01 22:30:36 +0800

    update

---
+++ plugins/mtproxy/js/mtproxy.js
@@ -0,0 +1,35 @@
+function mtPost(method, version, args,callback){
+    var loadT = layer.msg('正在获取...', { icon: 16, time: 0, shade: 0.3 });
+
+    var req_data = {};
+    req_data['name'] = 'mtproxy';
+    req_data['func'] = method;
+    req_data['version'] = version;
+ 
+    if (typeof(args) == 'string'){
+        req_data['args'] = JSON.stringify(toArrayObject(args));
+    } else {
+        req_data['args'] = JSON.stringify(args);
+    }
+
+    $.post('/plugins/run', req_data, function(data) {
+        layer.close(loadT);
+        if (!data.status){
+            //错误展示10S
+            layer.msg(data.msg,{icon:0,time:2000,shade: [10, '#000']});
+            return;
+        }
+
+        if(typeof(callback) == 'function'){
+            callback(data);
+        }
+    },'json'); 
+}
+
+function mtCommonFunc(){
+	var con = '<hr/><p class="conf_p" style="text-align:center;">\
+        <button class="btn btn-default btn-sm" onclick="">获取连接地址</button>\
+    </p>';
+
+    $(".soft-man-con").html(con);
+}
