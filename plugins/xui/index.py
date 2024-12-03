d2f586dadd1bc504c5d3615629252b6c9bcf25be
Mr Chen <midoks@163.com>
2024-12-03 18:25:04 +0800

    Update index.py

--- plugins/xui/index.py
+++ plugins/xui/index.py
@@ -165,7 +165,7 @@ def getXuiInfo():
 
     data['username'] = info['username']
     data['password'] = info['password']
-    data['port'] = setting_data['key']
+    data['port'] = setting_data['value']
     return mw.returnJson(True, 'ok', data)
 
 def installPreInspection():
