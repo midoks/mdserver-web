7c9ca83b468e36beb604f9284c16037bcd2eda2d
Mr Chen <midoks@163.com>
2024-12-14 12:22:50 +0800

    Update install.sh

--- scripts/old/install.sh
+++ scripts/old/install.sh
@@ -106,7 +106,7 @@ if [ $OSNAME != "macos" ];then
 			rm -rf /tmp/mdserver-web-master
 		else
 			# curl --insecure -sSLo /tmp/master.zip https://code.midoks.icu/midoks/mdserver-web/archive/master.zip
-			wget --no-check-certificate -O /tmp/master.zip https://code.midoks.icu/midoks/mdserver-web/archive/master.zip
+			wget --no-check-certificate -O /tmp/master.zip https://code.midoks.icu/midoks/mdserver-web/archive/${VERSION}.zip
 			cd /tmp && unzip /tmp/master.zip
 			mv -f /tmp/mdserver-web /www/server/mdserver-web
 			rm -rf /tmp/master.zip
