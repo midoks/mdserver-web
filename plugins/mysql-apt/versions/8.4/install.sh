6060faa3ace8558bdd2e24d2c9761b9ffbc551fb
Mr Chen <midoks@163.com>
2024-12-15 04:16:36 +0800

    Update install.sh

--- plugins/mysql-apt/versions/8.4/install.sh
+++ plugins/mysql-apt/versions/8.4/install.sh
@@ -19,7 +19,7 @@ bash ${rootPath}/scripts/getos.sh
 OSNAME=`cat ${rootPath}/data/osname.pl`
 VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
 
-# cd /www/server/mdserver-web/plugins/mysql-apt && bash install.sh install 8.0
+# cd /www/server/mdserver-web/plugins/mysql-apt && bash install.sh install 8.4
 
 # 暂时debian12没有标准版,先用11使用
 # if [ "$OSNAME" == 'debian' ] && [ "$VERSION_ID" == '12' ] ;then 
@@ -111,7 +111,7 @@ Install_mysql()
 	if [ "$?" == "0" ];then
 		mkdir -p $serverPath/mysql-apt
 		echo '8.4' > $serverPath/mysql-apt/version.pl
-		echo '安装完成' >
+		echo '安装完成'
 	else
 		echo '8.4' > $serverPath/mysql-apt/version.pl
 		echo "暂时不支持该系统"
