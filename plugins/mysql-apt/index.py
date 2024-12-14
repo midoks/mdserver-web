fd77048d4b74b87cde71325d38af2f7711256d1a
Mr Chen <midoks@163.com>
2024-12-15 01:30:09 +0800

    Update index.py

--- plugins/mysql-apt/index.py
+++ plugins/mysql-apt/index.py
@@ -400,7 +400,7 @@ def getShowLogFile():
     return tmp.groups()[0].strip()
 
 def getMdb8Ver():
-    return ['8.0','8.1','8.2','8.3','8.4','9.0']
+    return ['8.0','8.1','8.2','8.3','8.4','9.0','9.1']
 
 def pGetDbUser():
     if mw.isAppleSystem():
