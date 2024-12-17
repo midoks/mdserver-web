4383a202f281b8de9efc769ae38c92c6ac11c8e2
Mr Chen <midoks@163.com>
2024-12-18 01:55:29 +0800

    update

--- plugins/valkey/index.py
+++ plugins/valkey/index.py
@@ -261,9 +261,9 @@ def runInfo():
     cmd = getRedisCmd()
     cmd = cmd + 'info'
 
-    # print(cmd)
+    print(cmd)
     data = mw.execShell(cmd)[0]
-    # print(data)
+    print(data)
     res = [
         'tcp_port',
         'uptime_in_days',  # 已运行天数
