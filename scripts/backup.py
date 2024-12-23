0d4b157b1bc794d5f55e0c21b8808caae28ce154
Mr Chen <midoks@163.com>
2024-12-24 02:14:20 +0800

    Update backup.py

--- scripts/backup.py
+++ scripts/backup.py
@@ -175,7 +175,7 @@ class backupTools:
         pid = mw.M('databases').dbPos(db_path, db_name).where(
             'name=?', (name,)).getField('id')
 
-        mw.M('backup').add('type,name,pid,filename,addtime,size', (1, os.path.basename(
+        mw.M('backup').add('type,name,pid,filename,add_time,size', (1, os.path.basename(
             filename), pid, filename, endDate, os.path.getsize(filename)))
         log = "数据库[" + name + "]备份成功,用时[" + str(round(outTime, 2)) + "]秒"
         mw.writeLog('计划任务', log)
