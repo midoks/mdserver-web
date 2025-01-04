43f752afbc3801a76f5eebcfc8b3343c73c07e9a
Mr Chen <midoks@163.com>
2025-01-04 13:01:57 +0800

    update

--- plugins/php-apt/conf/app_start.php
+++ plugins/php-apt/conf/app_start.php
@@ -35,8 +35,10 @@ function app_xhprof_end() {
 	$run_id = $xhprof_runs->save_run($xhprof_data, 'xhprof_foo');
 
 	$profiler_url = sprintf('http://{$LOCAL_IP}:5858/index.php?run=%s&source=xhprof_foo', $run_id);
-	echo "<script language='javascript'>window.open('{$profiler_url}')</script>";
-	// echo '<a href="' . $profiler_url . '" target="_blank">Profiler output</a>';
+	// echo "<script language='javascript'>window.open('{$profiler_url}')</script>";
+
+	$style_css = 'position:fixed;bottom: 0px;right: 0px;z-index: 100000000;color: red;background-color: black;padding: 0px;margin: 0px;';
+	echo '<a href="' . $profiler_url . '" target="_blank" style="' . $style_css . '">XHProf分析结果</a>';
 
 }
 
