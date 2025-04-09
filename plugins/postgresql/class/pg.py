897e54cb8667b53c3af4a36b335a71f4bf8eb00a
dami <midoks@163.com>
2025-04-09 11:41:01 +0800

    Update pg.py

--- plugins/postgresql/class/pg.py
+++ plugins/postgresql/class/pg.py
@@ -7,8 +7,12 @@ import sys
 import psycopg2
 
 
-sys.path.append(os.getcwd() + "/class/core")
-import mw
+web_dir = os.getcwd() + "/web"
+if os.path.exists(web_dir):
+    sys.path.append(web_dir)
+    os.chdir(web_dir)
+
+import core.mw as mw
 
 
 class ORM:
