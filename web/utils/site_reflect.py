fba98f5ed6a903a7d9f9ed0eea5c90b5a6c57444
dami <midoks@163.com>
2025-04-26 00:32:43 +0800

    up

---
+++ web/utils/site_reflect.py
@@ -0,0 +1,23 @@
+# coding:utf-8
+
+# ---------------------------------------------------------------------------------
+# MW-Linux面板
+# ---------------------------------------------------------------------------------
+# copyright (c) 2018-∞(https://github.com/midoks/mdserver-web) All rights reserved.
+# ---------------------------------------------------------------------------------
+# Author: midoks <midoks@163.com>
+# ---------------------------------------------------------------------------------
+
+import os
+import sys
+import re
+import json
+import time
+import threading
+import multiprocessing
+
+import core.mw as mw
+import thisdb
+
+def parse():
+	print('21')
