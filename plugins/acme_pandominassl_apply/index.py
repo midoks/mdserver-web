00dc48b439cde25a58169ec9eb8c99bc59a1f58f
dami <midoks@163.com>
2025-05-25 20:11:06 +0800

    Update index.py

--- plugins/acme_pandominassl_apply/index.py
+++ plugins/acme_pandominassl_apply/index.py
@@ -601,6 +601,8 @@ def runHookDstDomain(row):
 
     hookWriteLog('开始申请【'+domain+'】SSL证书')
     cmd = "#!/bin/bash\n"
+    cmd += "PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin:/root/.acme.sh\n"
+    cmd += "export PATH\n"
 
     if mw.isAppleSystem():
         user = getRunUser()
