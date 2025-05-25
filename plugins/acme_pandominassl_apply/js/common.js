719e8165f1ddbf866e70f06081a77d63476a8aad
dami <midoks@163.com>
2025-05-25 20:20:10 +0800

    Update common.js

--- plugins/acme_pandominassl_apply/js/common.js
+++ plugins/acme_pandominassl_apply/js/common.js
@@ -716,13 +716,13 @@ function domainList(page, search){
             if (rdata.data[i]['effective_date'] == ''){
                 list += '<td>空/未申请</td>';
             } else {
-                list += '<td>'+rdata.data[i]['effective_date']+'</td>';
+                list += '<td>'+getFormatTime(rdata.data[i]['effective_date'],'yyyy/MM/dd')+'</td>';
             }
 
             if (rdata.data[i]['expiration_date'] == ''){
                 list += '<td>空/未申请</td>';
             } else {
-                list += '<td>'+rdata.data[i]['expiration_date']+'</td>';
+                list += '<td>'+getFormatTime(rdata.data[i]['expiration_date'],'yyyy/MM/dd')+'</td>';
             }
 
             if (rdata.data[i]['status'] == '0'){
