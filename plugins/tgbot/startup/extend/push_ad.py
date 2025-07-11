3cdc78af1f866e584ed7992c8f445b9cea8332a0
dami <midoks@163.com>
2025-07-11 15:37:26 +0800

    Update push_ad.py

--- plugins/tgbot/startup/extend/push_ad.py
+++ plugins/tgbot/startup/extend/push_ad.py
@@ -48,6 +48,10 @@ def send_msg(bot, tag='ad', trigger_time=300):
     keyboard = [
         [
             types.InlineKeyboardButton(
+                text="高价收一切流量 @caifutong555", url='https://t.me/caifutong555')
+        ],
+        [
+            types.InlineKeyboardButton(
                 text="18+资源采集", url='https://ckzy1.com')
         ],
         [
