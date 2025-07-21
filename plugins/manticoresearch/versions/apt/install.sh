08304196586def04209a7c60ae0396508b2bc61a
dami <midoks@163.com>
2025-07-21 16:28:51 +0800

    Update install.sh

--- plugins/manticoresearch/versions/apt/install.sh
+++ plugins/manticoresearch/versions/apt/install.sh
@@ -65,102 +65,25 @@ fi
 
 Install_App()
 {
-	echo '正在安装Sphinx...'
-	mkdir -p $serverPath/sphinx
+	echo '正在安装manticoresearch...'
+	mkdir -p $serverPath/manticoresearch
 
-	SPHINX_DIR=${serverPath}/source/sphinx
-	mkdir -p $SPHINX_DIR
+	MC_DIR=${serverPath}/source/manticoresearch
+	mkdir -p $MC_DIR
 
-	SPH_NAME=amd64
-	if [ "$sysArch" == "arm64" ];then
-		SPH_NAME=amd64
-	elif [ "$sysArch" == "x86_64" ]; then
-		SPH_NAME=amd64
-	elif [ "$sysArch" == "aarch64" ]; then
-		SPH_NAME=aarch64
-	fi
+	wget --no-check-certificate -O $MC_DIR/manticore-repo.noarch.deb https://repo.manticoresearch.com/manticore-repo.noarch.deb
+	dpkg -i $MC_DIR/manticore-repo.noarch.deb
+	apt update
+	apt install manticore manticore-extra
 
-	if [ "$sysName" == "Darwin" ] && [ "$VERSION" == "3.7.1" ];then
-		SPH_NAME=aarch64
-	fi
-
-	SPH_SYSNAME=linux
-	if [ $sysName == 'Darwin' ]; then
-		SPH_SYSNAME=darwin
-	elif [ "$sysName" == "aarch64" ]; then
-		SPH_NAME=aarch64
-	elif [ "$sysName" == "freebsd" ]; then
-		SPH_NAME=freebsd
-	fi
-
-	if [ "$SPH_SYSNAME" == "linux" ];then
-		glibc_ver=`ldd  --version | grep libc | awk -F ')' '{print $2}'|awk '{gsub(/^\s+|\s+$/, "");print}'`
-		if [ "$VERSION" == "3.7.1" ] && [ `echo "2.29 > $glibc_ver " | bc` -eq 1 ];then
-			SPH_NAME=${SPH_NAME}-glibc2.17
-		fi
-		if [ "$VERSION" == "3.6.1" ] && [ `echo "2.29 > $glibc_ver " | bc` -eq 1 ];then
-			SPH_NAME=${SPH_NAME}-glibc2.17
-		fi
-	fi
-	
-
-	FILE_NAME=sphinx-${VERSION_NUM}-${SPH_SYSNAME}-${SPH_NAME}
-	FILE_TGZ=${FILE_NAME}.tar.gz
-
-	echo $FILE_TGZ
-	# curl -sSLo ${SPHINX_DIR}/${FILE_TGZ} http://sphinxsearch.com/files/${FILE_TGZ}
-	if [ ! -f ${SPHINX_DIR}/${FILE_TGZ} ];then
-		wget --no-check-certificate -O ${SPHINX_DIR}/${FILE_TGZ} http://sphinxsearch.com/files/${FILE_TGZ}
-	fi
-
-	cd ${SPHINX_DIR} && tar -zxvf ${FILE_TGZ}
-	
-	if [ "$?" == "0" ];then
-		mkdir -p $SPHINX_DIR
-		cp -rf ${SPHINX_DIR}/sphinx-${VERSION}/ $serverPath/sphinx/bin
-	fi
-	
-	if [ -d $serverPath/sphinx ];then
-		echo "${VERSION}" > $serverPath/sphinx/version.pl
-		echo '安装Sphinx完成'
-		cd ${rootPath} && python3 ${rootPath}/plugins/sphinx/index.py start
-
-		if [ $sysName != 'Darwin' ]; then
-			cd ${rootPath} && python3 ${rootPath}/plugins/sphinx/index.py initd_install
-		fi
-	fi
-
-	if [ -d ${SPHINX_DIR}/sphinx-${VERSION} ];then
-		rm -rf ${SPHINX_DIR}/sphinx-${VERSION}
+	if [ -d ${MC_DIR} ];then
+		rm -rf ${MC_DIR}
 	fi
 }
 
 Uninstall_App()
 {
-	if [ -f /usr/lib/systemd/system/sphinx.service ] || [ -f /lib/systemd/system/sphinx.service ];then
-		systemctl stop sphinx
-		systemctl disable sphinx
-
-		if [ -f /usr/lib/systemd/system/sphinx.service ];then
-			rm -rf /usr/lib/systemd/system/sphinx.service
-		fi
-
-		if [ -f /lib/systemd/system/sphinx.service ];then
-			rm -rf /lib/systemd/system/sphinx.service
-		fi
-		systemctl daemon-reload
-	fi
-
-	if [ -f $serverPath/sphinx/initd/sphinx ];then
-		$serverPath/sphinx/initd/sphinx stop
-	fi
-
-	if [ -d $serverPath/sphinx ];then
-		echo "rm -rf $serverPath/sphinx"
-		rm -rf $serverPath/sphinx
-	fi
-
-	echo "卸载sphinx成功"
+	echo "卸载manticoresearch成功"
 }
 
 action=$1
