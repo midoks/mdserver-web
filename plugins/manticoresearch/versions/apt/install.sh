0b4299d6c384d7c761ddc8814b2f56c06ef60ee3
dami <midoks@163.com>
2025-07-21 16:29:19 +0800

    Update install.sh

--- plugins/manticoresearch/versions/apt/install.sh
+++ plugins/manticoresearch/versions/apt/install.sh
@@ -9,59 +9,11 @@ serverPath=$(dirname "$rootPath")
 sysName=`uname`
 sysArch=`arch`
 
-
 if [ -f ${rootPath}/bin/activate ];then
 	source ${rootPath}/bin/activate
 fi
 
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/sphinx/index.py rebuild
-# cd /www/server/mdserver-web/plugins/sphinx && bash install.sh install 3.6.1
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/sphinx/index.py db_to_sphinx && /www/server/sphinx/bin/bin/indexer -c /www/server/sphinx/sphinx.conf --all --rotate
-# /Users/midoks/Desktop/mwdev/server/sphinx/bin/bin/indexer /Users/midoks/Desktop/mwdev/server/sphinx/sphinx.conf --all --rotate
-
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/sphinx/index.py sphinx_cmd
-
-# /Users/midoks/Desktop/mwdev/server/sphinx/bin/bin/indexer /Users/midoks/Desktop/mwdev/server/sphinx/sphinx.conf --all --rotate
-
-# cd /www/server/mdserver-web && source bin/activate && python3 plugins/sphinx/index.py start
-bash ${rootPath}/scripts/getos.sh
-# echo "bash ${rootPath}/scripts/getos.sh"
-OSNAME="macos"
-if [ -f ${rootPath}/data/osname.pl ];then
-	OSNAME=`cat ${rootPath}/data/osname.pl`	
-fi
-
-if [ "${OSNAME}" == "centos" ] || 
-	[ "${OSNAME}" == "fedora" ] ||
-	[ "${OSNAME}" == "alma" ]; then
-	yum install -y postgresql-libs unixODBC
-fi
-
-# http://sphinxsearch.com/files/sphinx-3.7.1-da9f8a4-linux-amd64.tar.gz
-
 VERSION=$2
-
-# echo $VERSION
-
-if [ "$VERSION" == "3.1.1" ];then
-	VERSION_NUM=${VERSION}-612d99f
-elif [ "$VERSION" == "3.2.1" ]; then
-	VERSION_NUM=${VERSION}-f152e0b
-elif [ "$VERSION" == "3.3.1" ]; then
-	VERSION_NUM=${VERSION}-b72d67b
-elif [ "$VERSION" == "3.4.1" ]; then
-	VERSION_NUM=${VERSION}-efbcc65
-elif [ "$VERSION" == "3.5.1" ]; then
-	VERSION_NUM=${VERSION}-82c60cb
-elif [ "$VERSION" == "3.6.1" ]; then
-	VERSION_NUM=${VERSION}-c9dbeda
-elif [ "$VERSION" == "3.7.1" ]; then
-	VERSION_NUM=${VERSION}-da9f8a4
-elif [ "$VERSION" == "3.8.1" ]; then
-	VERSION_NUM=${VERSION}-d25e0bb
-fi
-
-# echo $VERSION_NUM
 
 Install_App()
 {
