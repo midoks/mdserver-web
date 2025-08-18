7c848aa37267bffd6b2eae7d0a72a34685864283
dami <midoks@163.com>
2025-08-19 00:10:40 +0800

    Update sites.py

--- web/thisdb/sites.py
+++ web/thisdb/sites.py
@@ -96,7 +96,6 @@ def getSitesList(
     start = (int(page) - 1) * (int(size))
     limit = str(start) + ',' +str(size)
 
-    print("order:",order)
     if order.find("none") > -1:
         site_list = dbM.limit(limit).order('').select()
     elif order is not None:
