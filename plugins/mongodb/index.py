f558095a0e5d93890803e03337c0cd7af3ade79e
dami <midoks@163.com>
2025-09-10 23:52:55 +0800

    Update index.py

--- plugins/mongodb/index.py
+++ plugins/mongodb/index.py
@@ -1527,7 +1527,7 @@ def runLog():
 
 def installPreInspectionDebainCheck(sysId,version):
     if version == '8.0':
-        if sysId < 12:
+        if int(sysId) < 12:
             return "[%s]需要至少debain[12]" % (version,)
     return ''
 
