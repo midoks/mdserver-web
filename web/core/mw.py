b3f693556c1f2ac3c89c2cb8182f4f2369fce129
dami <midoks@163.com>
2025-09-11 16:36:05 +0800

    Update mw.py

--- web/core/mw.py
+++ web/core/mw.py
@@ -423,9 +423,9 @@ def md5(content):
         return False
 
 def hasPwd(password):
+    # 加密密码字符
     # python3 -c "import crypt"
     # python3 -c 'import crypt; print(crypt.crypt(""))'
-    # 加密密码字符
     # import crypt
     # return crypt.crypt(password, password)
     import bcrypt
