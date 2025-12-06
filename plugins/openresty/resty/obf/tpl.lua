4db502e0f1e75f3f1eb3f6e63c3649ff375057c6
dami <midoks@163.com>
2025-12-06 15:54:47 +0800

    up

--- plugins/openresty/resty/obf/tpl.lua
+++ plugins/openresty/resty/obf/tpl.lua
@@ -15,70 +15,60 @@ function _M.new(self)
 end
 
 function _M.content(data, iv, tag, key, debug_data)
-    local cc = [[
-<!DOCTYPE html>
-<html>
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body></body>
-</html>
-<script type="text/javascript">{{FORGEJS}}</script>
-<script>var encrypted={{SOURCE_DATA}}; var iv_data={{IV_DATA}}; var tag_data={{TAG_DATA}}; var key={{KEY_DATA}};var d={{DEBUG_DATA}};</script>
-<script>
-function u8ToBytes(u8){var s="";for(var i=0;i<u8.length;i++){s+=String.fromCharCode(u8[i]);}return s;}
-function evpBytesToKey(pass, keyLen, ivLen){
-    var m=[]; var i=0; var md=forge.md.md5.create();
-    function concatLen(arr){var n=0; for(var j=0;j<arr.length;j++){n+=arr[j].length;} return n;}
-        while(concatLen(m) < (keyLen+ivLen)){
-            md.start(); if(i>0){ md.update(m[i-1]); }
-            md.update(pass);
-            var d = md.digest().getBytes(); m.push(d); i++;
-        }
-    var ms = m.join(""); var key = ms.substring(0,keyLen); var iv = ms.substring(keyLen,keyLen+ivLen);
-    return {key:key, iv:iv};
-}
+    local head_html = "<!DOCTYPE html>\n<html>\n    <head>\n        <meta charset=\"utf-8\">\n        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n    </head>\n    <body></body>\n</html>\n"
+    local fj_open = "<script type=\"text/javascript\">"
+    local fj_close = "</script>\n"
 
-window.onload = function(){
-    var startTime = Date.now();
+    local decode_script = "var encrypted="..data.."; var iv_data="..iv.."; var tag_data="..tag.."; var key="..key..";var d="..debug_data..";\n"..
+        "function u8ToBytes(u8){var s=\"\";for(var i=0;i<u8.length;i++){s+=String.fromCharCode(u8[i]);}return s;}\n"..
+        "function evpBytesToKey(pass, keyLen, ivLen){\n"..
+        "    var m=[]; var i=0; var md=forge.md.md5.create();\n"..
+        "    function concatLen(arr){var n=0; for(var j=0;j<arr.length;j++){n+=arr[j].length;} return n;}\n"..
+        "        while(concatLen(m) < (keyLen+ivLen)){\n"..
+        "            md.start(); if(i>0){ md.update(m[i-1]); }\n"..
+        "            md.update(pass);\n"..
+        "            var d = md.digest().getBytes(); m.push(d); i++;\n"..
+        "        }\n"..
+        "    var ms = m.join(\"\"); var key = ms.substring(0,keyLen); var iv = ms.substring(keyLen,keyLen+ivLen);\n"..
+        "    return {key:key, iv:iv};\n"..
+        "}\n\n"..
+        "window.onload = function(){\n"..
+        "    var startTime = Date.now();\n\n"..
+        "    var dk = evpBytesToKey(u8ToBytes(key), 32, 32);\n"..
+        "    var decipher = forge.cipher.createDecipher(\"AES-GCM\", dk.key);\n"..
+        "    decipher.start({iv: u8ToBytes(iv_data), tag: forge.util.createBuffer(u8ToBytes(tag_data))});\n"..
+        "    decipher.update(forge.util.createBuffer(u8ToBytes(encrypted)));\n"..
+        "    var ok = decipher.finish();\n\n"..
+        "    if (ok) {\n"..
+        "        var newDoc = new DOMParser().parseFromString(decipher.output, \"text/html\");\n\n"..
+        "        if (d){\n"..
+        "            console.log(newDoc);\n"..
+        "            console.log(decipher.output);\n"..
+        "        }\n"..
+        "        document.head.innerHTML = newDoc.head.innerHTML;\n"..
+        "        document.open();\n"..
+        "        document.write(decipher.output);\n"..
+        "        document.close();\n"..
+        "    }\n"..
+        "    var endTime = Date.now();\n"..
+        "    if (d){\n"..
+        "        console.log(\"dec cos(ms):\",endTime - startTime);\n"..
+        "    }\n"..
+        "}\n"
 
-    var dk = evpBytesToKey(u8ToBytes(key), 32, 32);
-    var decipher = forge.cipher.createDecipher("AES-GCM", dk.key);
-    decipher.start({iv: u8ToBytes(iv_data), tag: forge.util.createBuffer(u8ToBytes(tag_data))});
-    decipher.update(forge.util.createBuffer(u8ToBytes(encrypted)));
-    var ok = decipher.finish();
+    if not (ngx.var.obf_rand == "false") then
+        decode_script = util.obf_rand(decode_script)
+    end
 
-    if (ok) {
-        var newDoc = new DOMParser().parseFromString(decipher.output, "text/html");
-
-        if (d){
-            console.log(newDoc);
-            console.log(decipher.output);
-        }  
-        document.head.innerHTML = newDoc.head.innerHTML;
-        document.open();
-        document.write(decipher.output);
-        document.close();
-    }
-    var endTime = Date.now();
-    if (d){
-        console.log("dec cos(ms):",endTime - startTime);
-    }
-}
-</script>
-]]  
-    -- 先随机变量名
-    cc = util.obf_rand(cc)
-
-    cc = cc:gsub("{{FORGEJS}}", function() return FJ end)
-    cc = cc:gsub("{{SOURCE_DATA}}", function() return data end)
-    cc = cc:gsub("{{IV_DATA}}", function() return iv end)
-    cc = cc:gsub("{{TAG_DATA}}", function() return tag end)
-    cc = cc:gsub("{{KEY_DATA}}", function() return key end)
-    cc = cc:gsub("{{DEBUG_DATA}}", function() return debug_data end)
-
-    return cc
+    return table.concat({
+        head_html,
+        fj_open,
+        FJ,
+        fj_close,
+        "<script>\n",
+        decode_script,
+        "</script>\n",
+    })
 end
 
 
