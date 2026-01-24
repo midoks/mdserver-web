664d847a713fd7aec6658b9a2f6475d489e2e61b
dami <midoks@163.com>
2026-01-24 20:35:28 +0800

    Update swoole.sh

--- plugins/php-apt/versions/common/swoole.sh
+++ plugins/php-apt/versions/common/swoole.sh
@@ -26,8 +26,8 @@ elif [ "$version" == "7.1" ];then
 	LIBV=4.5.2
 elif [ "$version" == "7.0" ];then
 	LIBV=4.3.0
-elif [ "$version" == "6.1.6" ];then
-	LIBV=4.3.0
+elif [ "$version" == "8.5" ];then
+	LIBV=6.1.6
 else
 	echo 'ok'
 	APT_INSTALL=1
