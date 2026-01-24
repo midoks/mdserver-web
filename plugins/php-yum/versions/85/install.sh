88a0c5dc57cb326050c715fba551a55f98278985
dami <midoks@163.com>
2026-01-24 20:29:30 +0800

    update

---
+++ plugins/php-yum/versions/85/install.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+sourcePath=${serverPath}/source
+sysName=`uname`
+
+version=8.5.x
+PHP_VER=85
+
+
+Install_php()
+{
+#------------------------ install start ------------------------------------#
+yum install -y php85 php85-php-fpm
+if [ "$?" == "0" ];then
+	mkdir -p $serverPath/php-yum/${PHP_VER}
+fi
+
+#------------------------ install end ------------------------------------#
+}
+
+Uninstall_php()
+{
+	yum remove -y php85 php85-php-fpm php85-*
+	rm -rf $serverPath/php-yum/${PHP_VER}
+	echo "卸载php-${version}..."
+}
+
+action=${1}
+if [ "${1}" == 'install' ];then
+	Install_php
+else
+	Uninstall_php
+fi
