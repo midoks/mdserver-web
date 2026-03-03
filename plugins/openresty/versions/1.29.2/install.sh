ab1ca7af8046a66c26ccc705b00e2f6ee6919a8d
dami <midoks@163.com>
2026-03-03 15:09:01 +0800

    Update install.sh

--- plugins/openresty/versions/1.29.2/install.sh
+++ plugins/openresty/versions/1.29.2/install.sh
@@ -2,8 +2,8 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin
 export PATH
 
-# cd /Users/midoks/Desktop/mwdev/server/mdserver-web/plugins/openresty && bash install.sh install 1.21.4.2
-# cd /www/server/mdserver-web/plugins/openresty && bash install.sh install 1.27.1
+# cd /Users/midoks/Desktop/mwdev/server/mdserver-web/plugins/openresty && bash install.sh install 1.29.2
+# cd /www/server/mdserver-web/plugins/openresty && bash install.sh install 1.29.2
 
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
