f837fb31fc851424312c44a8499bfc49271f4dd9
dami <midoks@163.com>
2026-03-17 18:41:35 +0800

    Update push_notice_msg.py

--- plugins/tgbot/startup/extend/push_notice_msg.py
+++ plugins/tgbot/startup/extend/push_notice_msg.py
@@ -57,7 +57,7 @@ def send_msg(bot, tag='ad', trigger_time=300):
     # https://t.me/gjgzs2022 ｜ 22/m | @GJ_gzs
     # 实名认证/过人脸🕵️‍♀️各种账号处理✅ | 30/m| next,6/30 | @nngzs
     # 18+资源采集| 4/m | next,7/14 | @liuxingyu123
-    # 海内外实名KYC-执照代付✅域名| 17/m | next,6/17 | @liuxingyu123
+    # 海内外实名KYC-执照代付✅域名| 17/m | next,6/17 | @kdgzs
 
     keyboard = [
         [
