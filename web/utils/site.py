422b904e00779bce0e37bfe0f0549903a74635a1
dami <midoks@163.com>
2026-04-03 00:32:41 +0800

    Update site.py

--- web/utils/site.py
+++ web/utils/site.py
@@ -529,10 +529,9 @@ class sites(object):
             # add_header Alt-Svc 'h3=":443";ma=86400,h3-29=":443";ma=86400';
             http3Header = """
     add_header Strict-Transport-Security "max-age=63072000";
-    add_header Alt-Svc 'h3=":443";ma=86400';
 """
-            if not version.startswith('1.25') or version.startswith('1.27'):
-                http3Header = '';
+            # if not version.startswith('1.25') or version.startswith('1.27'):
+            #     http3Header = '';
 
             sslStr = """#error_page 404/404.html;
     ssl_certificate    %s;
