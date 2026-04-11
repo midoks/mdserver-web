584ac773b31461b3808ee3a4ab533b786b0c2910
dami <midoks@163.com>
2026-04-11 17:01:44 +0800

    Update install.sh

--- plugins/apache/install.sh
+++ plugins/apache/install.sh
@@ -13,7 +13,8 @@ export PATH
 # apt install ncat -y
 # nc -u -v www.xx.com 443
 
-# cd /www/server/mdserver-web && python3 plugins/openresty/index.py run_info
+# cd /www/server/mdserver-web && python3 plugins/apache/index.py run_info
+# cd /www/server/mdserver-web && python3 plugins/apache/index.py run_info
 
 curPath=`pwd`
 rootPath=$(dirname "$curPath")
