c551c2ee1ae895a7582174952b5c6f197c659310
dami <midoks@163.com>
2026-05-07 16:55:58 +0800

    Update index.py

--- plugins/php-apt/index.py
+++ plugins/php-apt/index.py
@@ -81,8 +81,8 @@ def status(version):
     # if data[0] == '':
     #     return 'stop'
     # return 'start'
-    sock_file = "/run/php/php{}-fpm.sock".format(version)
-    if os.path.exists(sock_file):
+    pid_file = "/run/php/php{}-fpm.pid".format(version)
+    if os.path.exists(pid_file):
         return 'start'
     return 'stop'
 
