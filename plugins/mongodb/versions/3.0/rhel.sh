1eb76f2b9025d826e18fce5cfae16a13304a5425
dami <midoks@163.com>
2026-05-07 13:56:40 +0800

    update

---
+++ plugins/mongodb/versions/3.0/rhel.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin:/opt/homebrew/bin
+export PATH
+
+curPath=`pwd`
+rootPath=$(dirname "$curPath")
+rootPath=$(dirname "$rootPath")
+rootPath=$(dirname "$rootPath")
+serverPath=$(dirname "$rootPath")
+
+VERSION=3.0.1
+SYS_ARCH=`arch`
+
+SYS_VERSION_ID=`cat /etc/*-release | grep VERSION_ID | awk -F = '{print $2}' | awk -F "\"" '{print $2}'`
+SYS_NAME=${SYS_VERSION_ID/./}
+SYS_NAME_LEN=`echo "$SYS_NAME" | wc -L`
+
+if [ "$SYS_NAME_LEN" == "1" ];then
+	SYS_NAME=${SYS_NAME}0	
+fi
+
+# https://fastdl.mongodb.org/linux/mongodb-linux-x86_64-rhel70-4.4.23.tgz
+
+MG_DIR=$serverPath/source/mongodb
+mkdir -p $MG_DIR
+
+FILE_NAME=mongodb-linux-${SYS_ARCH}-ubuntu${SYS_NAME}-${VERSION}
+FILE_NAME_TGZ=${FILE_NAME}.tgz
+
+if [ ! -f $MG_DIR/${FILE_NAME_TGZ} ]; then
+	wget --no-check-certificate -O $MG_DIR/${FILE_NAME_TGZ} https://fastdl.mongodb.org/linux/${FILE_NAME_TGZ}
+	echo "wget --no-check-certificate -O $MG_DIR/${FILE_NAME_TGZ} https://fastdl.mongodb.org/linux/${FILE_NAME_TGZ}"
+fi
+
+if [ ! -d $MG_DIR/${FILE_NAME} ];then 
+	cd $MG_DIR && tar -zxvf ${FILE_NAME_TGZ}
+fi
+
+if [ ! -d  $serverPath/mongodb/bin ];then
+	mkdir -p $serverPath/mongodb
+	cd $MG_DIR/${FILE_NAME} && cp -rf ./bin $serverPath/mongodb
+fi
+
+cd ${MG_DIR} && rm -rf ${MG_DIR}/${FILE_NAME}
+
+#--------------- mongosh tool install ------------------ #
+TOOL_VERSION=2.2.5
+TOOL_FILE_NAME=mongosh-${TOOL_VERSION}-linux-x64
+if [ "aarch64" == ${SYS_ARCH} ];then
+	TOOL_FILE_NAME=mongosh-${TOOL_VERSION}-linux-arm64
+fi
+TOOL_FILE_NAME_TGZ=${TOOL_FILE_NAME}.tgz
+if [ ! -f $MG_DIR/${TOOL_FILE_NAME_TGZ} ]; then
+	wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://downloads.mongodb.com/compass/${TOOL_FILE_NAME_TGZ}
+	echo "wget --no-check-certificate -O $MG_DIR/${TOOL_FILE_NAME_TGZ} https://downloads.mongodb.com/compass/${TOOL_FILE_NAME_TGZ}"
+fi
+
+if [ ! -d $MG_DIR/${TOOL_FILE_NAME_TGZ} ];then 
+	cd $MG_DIR && tar -zxvf ${TOOL_FILE_NAME_TGZ}
+fi
+
+cd ${MG_DIR}/${TOOL_FILE_NAME} && cp -rf ./bin $serverPath/mongodb
+cd ${MG_DIR} && rm -rf ${MG_DIR}/${TOOL_FILE_NAME}
