3c6f3c50c45c81a26188b305e0d515344c9b04fc
dami <midoks@163.com>
2026-06-22 17:28:40 +0800

    Update update.sh

--- plugins/nezha/update.sh
+++ plugins/nezha/update.sh
@@ -7,4 +7,14 @@ export PATH
 curPath=`pwd`
 echo $curPath
 
+REPO="nezhahq/nezha"
+LATEST_VERSION=$(curl -sL "https://api.github.com/repos/$REPO/releases/latest" | jq -r '.tag_name')
+echo "最新版本是: $LATEST_VERSION"
+NUMBER_LATEST_VERSION=${LATEST_VERSION:1}
+echo "最新[NUMBER]是: $NUMBER_LATEST_VERSION"
+
+INSTALL_VERSION=`cat /www/server/nezha/version.pl`
+echo "安装的版本: $INSTALL_VERSION"
+
+
 echo "ok"
