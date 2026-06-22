c52e707421018da0aa91e1fabd5f040c639e5a5b
dami <midoks@163.com>
2026-06-22 17:21:11 +0800

    Update update.sh

--- plugins/nezha/update.sh
+++ plugins/nezha/update.sh
@@ -2,4 +2,9 @@
 PATH=/bin:/sbin:/usr/bin:/usr/sbin:/usr/local/bin:/usr/local/sbin:~/bin
 export PATH
 
+# cd /www/server/mdserver-web && bash plugins/nezha/update.sh
+
+curPath=`pwd`
+echo $curPath
+
 echo "ok"
