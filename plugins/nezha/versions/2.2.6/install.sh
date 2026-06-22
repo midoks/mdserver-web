358a021ba67e3c9f0e7200faf8dc06300a774fcb
dami <midoks@163.com>
2026-06-22 12:47:54 +0800

    Update install.sh

--- plugins/nezha/versions/2.2.6/install.sh
+++ plugins/nezha/versions/2.2.6/install.sh
@@ -117,7 +117,6 @@ Install_App()
 Uninstall_App()
 {
 	cd ${rootPath} && python3 ${rootPath}/plugins/nezha/index.py initd_uninstall
-	cd ${rootPath} && python3 ${rootPath}/plugins/nezha/index.py initd_uninstall_agent
 	cd ${rootPath} && python3 ${rootPath}/plugins/nezha/index.py stop
 	cd ${rootPath} && python3 ${rootPath}/plugins/nezha/index.py stop_agent
 	rm -rf $serverPath/nezha
